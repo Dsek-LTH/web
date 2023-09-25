f74db4e3eb2139941b1885475aaed31cd36c616d
Daniel Adu-Gyan <danny_adu@hotmail.com>
2023-09-25 23:30:20 +0200

    Add more article info to news feed

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -12,13 +12,24 @@
           </figure>
         {/if}
         
-        <div class="flex items-center gap-3">
-          <div class="avatar">
-            <div class="w-8 rounded-full">
-              <img src={article.author.member.picture_path} alt={article.author.member.first_name} />
+        <div class="flex flex-row justify-between">
+          <div class="flex items-center gap-3">
+            <div class="avatar">
+              <div class="w-10 rounded-full">
+                <img src={article.author.member.picture_path} alt={article.author.member.first_name} />
+              </div>
+            </div>
+            
+            <div>
+              <h3 class="text-sm font-semibold">{article.author.member.first_name + " " + article.author.member.last_name}</h3>
+              {#if  article.author.mandate?.position.name}
+                <h3 class="text-sm font-thin text-primary">{article.author.mandate?.position.name}</h3>
+              {/if}
             </div>
           </div>
-          <h3 class="text-sm font-semibold">{article.author.member.first_name + " " + article.author.member.last_name}</h3>
+          <p class="text-xs text-gray-500">
+            {new Date(article.publishedDatetime).toLocaleDateString()}
+          </p>
         </div>
         
         <h2 class="text-2xl font-bold my-3">{article.header}</h2>
@@ -29,9 +40,16 @@
           </p>
         </div>
 
-        <p class="text-xs text-gray-500">
-          {new Date(article.publishedDatetime).toLocaleDateString()}
-        </p>
+        
+        <div class="flex flex-col gap-2">
+          <div class="flex flex-row flex-wrap gap-2">
+            {#each article.tags as tag}
+              <span class="text-xs text-gray-500 bg-base-200 rounded-full p-1">
+                {tag.name}
+              </span>
+            {/each}
+          </div>
+        </div>
       </article>
     </a>
 	{/each}
