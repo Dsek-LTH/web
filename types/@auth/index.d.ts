3eecbf1566c6f417033c6c3ab49d02a624c8773c
Ludvig <ludvig.svedberg@mac.com>
2023-10-03 23:03:12 +0200

    Add keycloak login

---
+++ types/@auth/index.d.ts
@@ -0,0 +1,19 @@
+
+import type { DefaultJWT } from '@auth/core/jwt';
+import type {User as BaseUser } from '@auth/core/types';
+
+type ExtendedUserFields = {
+    student_id: string;
+    group_list: string[];
+}
+
+declare module '@auth/core/jwt' {
+    interface JWT extends ExtendedUserFields, DefaultJWT {}
+}
+
+declare module '@auth/core/types' {
+    interface User extends ExtendedUserFields, BaseUser {}
+    interface Session extends DefaultSession {
+        user: ExtendedUserFields & DefaultSession['user']; 
+    }
+}
