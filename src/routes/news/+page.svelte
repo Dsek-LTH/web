0afc2e500e41c93d614db76a4ae1e22197669b37
Ludvig <ludvig.svedberg@mac.com>
2023-10-03 15:04:44 +0200

    Move image on article card and add timestamp

--- src/routes/news/+page.svelte
+++ src/routes/news/+page.svelte
@@ -13,16 +13,8 @@
   </form>
   {#each data.articles as article (article.id)}
     <article
-      class="m-4 rounded-2xl p-6 shadow-2xl ring-neutral-700 transition ease-in-out md:my-8 md:p-8 md:ring-1 md:hover:scale-105"
+      class="ease my-4 rounded-2xl p-6 shadow-2xl ring-neutral-700 transition md:my-8 md:p-8 md:ring-1 md:hover:scale-105"
     >
-      {#if article.imageUrl}
-        <a href="/news/{article.id}">
-          <figure class="my-4">
-            <img class="rounded-2xl" src={article.imageUrl} alt={article.header} />
-          </figure>
-        </a>
-      {/if}
-
       <div class="flex flex-row justify-between">
         <div class="flex items-center gap-3">
           <div class="avatar">
@@ -45,31 +37,40 @@
           </div>
         </div>
 
-        <p class="text-xs text-gray-500">
-          {new Date(article.publishedDatetime).toLocaleDateString(["sv"])}
+        <p class="text-right text-xs text-gray-500">
+          {new Date(article.publishedDatetime).toLocaleDateString(["sv"])} <br />
+          {new Date(article.publishedDatetime).toLocaleTimeString(["sv"], {
+            hour: "2-digit",
+            minute: "2-digit"
+          })}
         </p>
       </div>
 
       <a href="/news/{article.id}">
         <h2 class="my-3 text-2xl font-bold">{article.header}</h2>
 
-        <div class="my-3">
+        <div class="my-3 flex flex-row items-start gap-2">
           <p class="line-clamp-4 text-ellipsis">
             <!-- The article body is sanitized server-side. -->
             <!-- eslint-disable-next-line svelte/no-at-html-tags -->
             {@html article.body}
           </p>
+          {#if article.imageUrl}
+            <a href="/news/{article.id}">
+              <figure>
+                <img class="rounded-2xl" src={article.imageUrl} alt={article.header} />
+              </figure>
+            </a>
+          {/if}
         </div>
       </a>
 
-      <div class="flex flex-col gap-2">
-        <div class="flex flex-row flex-wrap gap-2">
-          {#each article.tags as tag}
-            <span class="rounded-full bg-base-200 px-2 py-1 text-xs text-gray-500">
-              {tag.name}
-            </span>
-          {/each}
-        </div>
+      <div class="flex flex-row flex-wrap gap-2">
+        {#each article.tags as tag}
+          <span class="badge badge-md" style="border-color: {tag.color}">
+            {tag.name}
+          </span>
+        {/each}
       </div>
     </article>
   {/each}
