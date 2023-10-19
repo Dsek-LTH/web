1b24e2dedb2d2f359dd796fa7c0bc103f20c68f6
Daniel Adu-Gyan <danny_adu@hotmail.com>
2023-10-19 11:28:59 +0200

    Refactor dev-setup script

--- dev/setup.ts
+++ dev/setup.ts
@@ -1,20 +1,20 @@
-import { intro, outro, isCancel, text, confirm, select, group } from "@clack/prompts";
-import crypto from "crypto";
-import { writeFile } from "fs/promises";
+import { confirm, group, intro, outro, select, text } from "@clack/prompts";
 import {
-  ENV_FILE_PATH,
-  POSTGRES_DOCKER_COMMAND,
   POSTGRES_DOCKER_URL,
-  POSTGRES_URL_REGEX,
-  spin,
-  fileExists,
+  envFileExists,
+  generateSecret,
   handleCancellation,
   onCancel,
   runWithSpinner,
+  setupDatabase,
+  spin,
+  validateDbUrl,
+  writeEnvFile,
 } from "./utils";
 
 intro("Let's get you set up!");
 
+let databaseUrl: string | symbol = POSTGRES_DOCKER_URL;
 const database = await select({
   message: "What should be used to setup a local PostgreSQL database?",
   options: [
@@ -26,28 +26,20 @@ const database = await select({
 });
 handleCancellation(database);
 
-let databaseUrl: string | symbol = POSTGRES_DOCKER_URL;
 switch (database) {
   case "docker":
-    await runWithSpinner(
-      POSTGRES_DOCKER_COMMAND,
-      "Setting up PostgreSQL",
-      "PostgreSQL setup complete!"
-    );
+    await setupDatabase();
     break;
   case "install":
     console.log(
-      "\nPlease install PostgreSQL yourself.\nThere is a guide over at https://www.prisma.io/dataguide/postgresql/setting-up-a-local-postgresql-database"
+      "\nPlease install PostgreSQL yourself before continuing.\nThere is a guide over at https://www.prisma.io/dataguide/postgresql/setting-up-a-local-postgresql-database"
     );
-  // Fall through - prompt for database URL unless Docker is used
+  // Fall through - prompt for database URL if user choose to install or skip
   case "skip":
     databaseUrl = await text({
       message: "What is the URL to your PostgreSQL database?",
       placeholder: "postgresql://USER:PASSWORD@HOST:PORT/DATABASE?schema=SCHEMA",
-      validate: (value) => {
-        if (POSTGRES_URL_REGEX.test(value)) return;
-        return "Please provide a valid PostgreSQL URL";
-      },
+      validate: validateDbUrl,
     });
     handleCancellation(databaseUrl);
 }
@@ -65,17 +57,15 @@ const setup = await group(
     authSecret: () =>
       text({
         message: "Choose a secret value (minimum 32 characters)",
-        initialValue: crypto.randomBytes(32 / 2).toString("hex"),
+        initialValue: generateSecret(),
       }),
     keycloakClientSecret: () =>
       text({
-        message: "Set the keycloak client secret",
+        message: "Set the keycloak client secret (can be left as 'secret')",
         initialValue: "secret",
       }),
   },
-  {
-    onCancel,
-  }
+  { onCancel }
 );
 
 if (setup.migrate) {
@@ -87,21 +77,16 @@ if (setup.seed) {
 }
 
 let writeEnv: boolean | symbol = true;
-if (await fileExists(ENV_FILE_PATH)) {
+if (await envFileExists()) {
   writeEnv = await confirm({
     message: "Do you want to overwrite the existing .env.local file?",
   });
-  if (isCancel(writeEnv)) onCancel();
+  handleCancellation(writeEnv);
 }
 
 if (writeEnv) {
   spin.start("Writing environment variables to .env.local");
-  await writeFile(
-    ENV_FILE_PATH,
-    `DATABASE_URL=${String(databaseUrl)}\nAUTH_SECRET=${setup.authSecret}\nKEYCLOAK_CLIENT_SECRET=${
-      setup.keycloakClientSecret
-    }`
-  );
+  await writeEnvFile(String(databaseUrl), setup.authSecret, setup.keycloakClientSecret);
   spin.stop("Environment variables written!");
 } else {
   console.log("\nIn that case, please set the following values manually in the .env.local file:");
--- dev/utils.ts
+++ dev/utils.ts
@@ -1,54 +1,8 @@
 import { cancel, isCancel, spinner } from "@clack/prompts";
 import { ExecException, exec } from "child_process";
-import { access } from "fs/promises";
+import { randomBytes } from "crypto";
+import { access, writeFile } from "fs/promises";
 import { resolve } from "path";
-
-async function fileExists(filePath: string): Promise<boolean> {
-  try {
-    await access(filePath);
-    return true;
-  } catch {
-    return false;
-  }
-}
-
-function onError(error: ExecException | null) {
-  // If the error is that the container already exists, we can ignore it
-  if (error && !error.message.includes("is already in use")) {
-    console.error(); // New line
-    console.error(error);
-    process.exit(1);
-  }
-}
-
-function handleCancellation<T>(value: T) {
-  if (isCancel(value)) {
-    onCancel();
-  }
-}
-
-function onCancel() {
-  cancel("Setup aborted.");
-  process.exit(0);
-}
-
-function run(command: string): Promise<void> {
-  return new Promise((resolve) => {
-    exec(command, {}, (error) => {
-      if (error) onError(error);
-      resolve();
-    });
-  });
-}
-
-function runWithSpinner(
-  command: string,
-  startMessage?: string,
-  stopMessage?: string
-): Promise<void> {
-  spin.start(startMessage);
-  return run(command).finally(() => spin.stop(stopMessage));
-}
 
 const POSTGRES_DOCKER_URL = "postgresql://postgres:postgres@localhost:5432/new_web?schema=public";
 const POSTGRES_DOCKER_COMMAND =
@@ -59,16 +13,136 @@ const ENV_FILE_PATH = resolve(__dirname, "..", ".env.local");
 
 const spin = spinner();
 
+async function fileExists(filePath: string): Promise<boolean> {
+  try {
+    await access(filePath);
+    return true;
+  } catch {
+    return false;
+  }
+}
+
+function envFileExists() {
+  return fileExists(ENV_FILE_PATH);
+}
+
+/**
+ * Write the environment variables to a .env file.
+ * @param url The PostgreSQL URL
+ * @param authSecret The auth secret
+ * @param keycloakClientSecret The Keycloak client secret
+ * @returns A promise that resolves when the file is written
+ */
+async function writeEnvFile(url: string, authSecret: string, keycloakClientSecret: string) {
+  writeFile(
+    ENV_FILE_PATH,
+    `DATABASE_URL=${String(
+      url
+    )}\nAUTH_SECRET=${authSecret}\nKEYCLOAK_CLIENT_SECRET=${keycloakClientSecret}`
+  );
+}
+
+/**
+ * Handle cancellation from CLI prompts, i.e CTRL+C.
+ * @param value The value returned from the prompt
+ */
+function handleCancellation<T>(value: T) {
+  if (isCancel(value)) {
+    onCancel();
+  }
+}
+
+/**
+ * Print a message and exit the process.
+ */
+function onCancel() {
+  cancel("Setup aborted.");
+  process.exit(0);
+}
+
+/**
+ * Run a shell command using `exec` and return a promise that resolves when the command is done.
+ * @param command The command to run
+ * @returns A promise that resolves when the command is done
+ */
+function run(command: string): Promise<void> {
+  return new Promise((resolve) => {
+    exec(command, {}, (error) => {
+      if (error) onError(error);
+      resolve();
+    });
+  });
+}
+
+/**
+ * Run a shell command using `exec` and show a spinner while it's running.
+ * @param command The command to run
+ * @param startMessage The message to show while the command is running
+ * @param stopMessage The message to show when the command is done
+ * @returns
+ */
+function runWithSpinner(
+  command: string,
+  startMessage?: string,
+  stopMessage?: string
+): Promise<void> {
+  spin.start(startMessage);
+  return run(command).finally(() => spin.stop(stopMessage));
+}
+
+/**
+ * Setup the PostgreSQL database using Docker.
+ * @returns A promise that resolves when the database is ready.
+ */
+function setupDatabase() {
+  return runWithSpinner(
+    POSTGRES_DOCKER_COMMAND,
+    "Setting up PostgreSQL",
+    "PostgreSQL setup complete!"
+  );
+}
+
+/**
+ * Validate a PostgreSQL URL.
+ * @param value
+ * @returns An error message if the URL is invalid
+ */
+function validateDbUrl(value: string) {
+  if (POSTGRES_URL_REGEX.test(value)) return;
+  return "Please provide a valid PostgreSQL URL";
+}
+
+/**
+ * Generate a random secret.
+ * @param N The length of the secret
+ * @returns A random secret in hex format
+ */
+function generateSecret(N = 32) {
+  return randomBytes(N / 2).toString("hex");
+}
+
+/**
+ * Handle errors from `exec` and exit the process.
+ * @param error The error from `exec`
+ */
+function onError(error: ExecException | null) {
+  // If the error is that the container already exists, we can ignore it
+  if (error && !error.message.includes("is already in use")) {
+    console.error(); // New line
+    console.error(error);
+    process.exit(1);
+  }
+}
+
 export {
-  ENV_FILE_PATH,
-  POSTGRES_DOCKER_COMMAND,
   POSTGRES_DOCKER_URL,
-  POSTGRES_URL_REGEX,
-  spin,
-  fileExists,
+  envFileExists,
+  generateSecret,
   handleCancellation,
   onCancel,
-  onError,
-  run,
   runWithSpinner,
+  setupDatabase,
+  spin,
+  validateDbUrl,
+  writeEnvFile,
 };
