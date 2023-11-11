efb8f1c371f828df787565f53fffa68254f919ce
Alfred Andersson <alfred@rygno.net>
2023-11-11 11:45:47 +0100

    songbook progress

--- src/routes/songbook/+page.server.ts
+++ src/routes/songbook/+page.server.ts
@@ -1,8 +1,66 @@
 import prisma from "$lib/utils/prisma";
 import type { PageServerLoad } from "./$types";
+import type { Prisma } from "@prisma/client";
 
-export const load: PageServerLoad = async () => {
-  const songs = await prisma.song.findMany({});
+const nPerPage = 10;
 
-  return { songs };
+export const load: PageServerLoad = async ({ url }) => {
+  const page = url.searchParams.get("page");
+  const search = url.searchParams.get("search");
+
+  const where: Prisma.SongWhereInput = search
+    ? {
+        OR: [
+          {
+            title: {
+              contains: search,
+              mode: "insensitive",
+            },
+          },
+          {
+            lyrics: {
+              contains: search,
+              mode: "insensitive",
+            },
+          },
+          {
+            category: {
+              contains: search,
+              mode: "insensitive",
+            },
+          },
+          {
+            melody: {
+              contains: search,
+              mode: "insensitive",
+            },
+          },
+        ],
+      }
+    : {};
+
+  const [songs, pageCount, categories] = await Promise.all([
+    prisma.song.findMany({
+      take: nPerPage,
+      skip: page ? Math.max((Number.parseInt(page) - 1) * nPerPage, 0) : 0,
+      orderBy: [{ category: "asc" }, { title: "asc" }],
+      where,
+    }),
+    prisma.song.count({ where }),
+    prisma.song.findMany({
+      select: {
+        category: true,
+      },
+      distinct: ["category"],
+      orderBy: {
+        category: "asc",
+      },
+    }),
+  ]);
+
+  return {
+    songs,
+    pageCount: Math.max(Math.ceil(pageCount / nPerPage), 1),
+    categories,
+  };
 };
--- src/routes/songbook/+page.svelte
+++ src/routes/songbook/+page.svelte
@@ -1,13 +1,26 @@
 <script lang="ts">
+  import Pagination from "$lib/components/Pagination.svelte";
+  import SearchBar from "$lib/components/SearchBar.svelte";
+  import { sanitize } from "isomorphic-dompurify";
+
   export let data;
 </script>
 
-{#each data.songs as song}
-  <article class="my-4 p-6 rounded-lg ring-neutral-700 shadow-2xl md:ring-1">
+{#each data.categories as cat}
+  <div>{cat.category}</div>
+{/each}
 
+<section>
+  <form method="get" id="filter-form">
+    <SearchBar />
+  </form>
+</section>
+
+{#each data.songs as song}
+  <article class="my-4 rounded-lg p-6 shadow-2xl ring-neutral-700 md:ring-1">
     <div class="flex justify-between">
       <h2 class="my-3 text-2xl font-bold">
-        {song.title}
+        {@html sanitize(song.title)}
       </h2>
 
       <p class="text-right text-xs text-gray-500">
@@ -19,11 +32,13 @@
       </p>
     </div>
 
-    <h3 class="text-gray-500 text-lg mb-4 italic">
+    <h3 class="mb-4 text-lg italic text-gray-500">
       {song.category}
     </h3>
 
     <p class="mb-4">Melodi: {song.melody}</p>
-    <p class="whitespace-pre-line">{song.lyrics}</p>
+    <p class="whitespace-pre-line">{@html sanitize(song.lyrics)}</p>
   </article>
 {/each}
+
+<Pagination pages={data.pageCount} />
