8f16c282bc2afc4506d5540ef7191a22b317043f
Alfred Andersson <alfred@rygno.net>
2023-11-11 15:36:11 +0100

    reset page when changing category

--- src/routes/songbook/+page.svelte
+++ src/routes/songbook/+page.svelte
@@ -24,6 +24,7 @@
     } else {
       urlParams.delete(param);
     }
+    urlParams.delete("page");
 
     return "?" + urlParams.toString();
   }
@@ -33,10 +34,7 @@
 
 <div class="my-4 flex flex-wrap justify-between">
   {#each Object.keys(data.categories) as catId}
-    <a
-      class="flex-grow"
-      href={hrefWithParam("category", isCatSelected(catId) ? "" : catId)}
-    >
+    <a class="flex-grow" href={hrefWithParam("category", isCatSelected(catId) ? "" : catId)}>
       <div
         class={(isCatSelected(catId) ? "bg-neutral-600" : "hover:bg-neutral-700") +
           " m-1 rounded-lg px-2 py-1 text-center ring-neutral-700 transition hover:scale-105 md:ring-1"}
