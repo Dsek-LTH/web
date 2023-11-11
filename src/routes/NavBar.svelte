753245c8e6f757bf4b52a5f6358d4de004e024cc
Alfred Grip <64659799+alfredgrip@users.noreply.github.com>
2023-11-11 21:39:10 +0100

    Navbar styling (#60)

--- src/routes/NavBar.svelte
+++ src/routes/NavBar.svelte
@@ -4,6 +4,7 @@
   import "../app.css";
   import apiNames from "$lib/utils/apiNames";
   import DarkLightToggle from "./DarkLightToggle.svelte";
+  import DsekLogo from "$lib/components/DsekLogo.svelte";
   $: accessPolicies = $page.data.accessPolicies;
   $: user = $page.data.session?.user;
 
@@ -25,22 +26,51 @@
       </div>
       <div class="hidden flex-none lg:block">
         <!-- Navbar menu content here -->
-        <a class="btn" href="/">Hem</a>
-        <a class="btn" href="/news">Nyheter</a>
-        <a class="btn" href="/events">Evenemang</a>
-        <a class="btn" href="/documents">Filer</a>
+        <a class="btn btn-ghost" href="/"
+          ><span
+            class="i-mdi-home h-6 w-6 text-primary-focus
+          "
+          ></span>Hem</a
+        >
+        <a class="btn btn-ghost" href="/news"
+          ><span class="i-mdi-newspaper h-6 w-6 text-primary-focus"></span> Nyheter</a
+        >
+        <a class="btn btn-ghost" href="/events"
+          ><span class="i-mdi-calendar h-6 w-6 text-primary-focus"></span>
+          Evenemang</a
+        >
+        <a class="btn btn-ghost" href="/documents"
+          ><span class="i-mdi-text-box-multiple h-6 w-6 text-primary-focus"></span>
+          Dokument</a
+        >
         <!-- https://bugs.webkit.org/show_bug.cgi?id=22261 -->
         <div class="dropdown dropdown-hover">
           <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
           <!-- svelte-ignore a11y-label-has-associated-control -->
-          <label tabindex="0" class="btn">Sektionen</label>
+          <label tabindex="0" class="btn btn-ghost">
+            <DsekLogo className="h-6 w-6 text-primary-focus" />
+            Sektionen</label
+          >
           <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
           <ul
             tabindex="0"
             class="menu dropdown-content rounded-box z-[1] w-52 bg-base-100 p-2 shadow"
           >
-            <li><a href="/committees">Utskott</a></li>
-            <li><a href="/songbook">Sjungbok</a></li>
+            <li>
+              <a href="/committees" class="btn-ghost"
+                ><span class="i-mdi-account-group h-6 w-6 text-primary-focus"></span>
+                Utskott</a
+              >
+            </li>
+            <li>
+              <a href="/songbook" class="btn-ghost">
+                <span
+                  class="i-mdi-library-music
+                 h-6 w-6 text-primary-focus"
+                ></span>
+                Sjungbok</a
+              >
+            </li>
           </ul>
         </div>
         {#if accessPolicies.includes(apiNames.ACCESS_POLICY.READ)}
@@ -48,13 +78,21 @@
           <div class="dropdown-hover dropdown">
             <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
             <!-- svelte-ignore a11y-label-has-associated-control -->
-            <label tabindex="0" class="btn">Admin</label>
+            <label tabindex="0" class="btn btn-ghost"
+              ><span class="i-mdi-security h-6 w-6 text-primary-focus"></span>
+              Admin</label
+            >
             <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
             <ul
               tabindex="0"
               class="menu dropdown-content rounded-box z-[1] w-52 bg-base-100 p-2 shadow"
             >
-              <li><a href="/admin/access">Access</a></li>
+              <li>
+                <a href="/admin/access" class="btn-ghost"
+                  ><span class="i-mdi-key h-6 w-6 text-primary-focus"> </span>
+                  Access</a
+                >
+              </li>
             </ul>
           </div>
         {/if}
@@ -62,10 +100,16 @@
       <div class="flex-1" />
       <DarkLightToggle />
       {#if $page.data.session}
-        <button class="btn" on:click={() => signOut()}>Logga ut</button>
-        <a href="/profile">Profil ({user?.student_id})</a>
+        <button class="btn btn-ghost" on:click={() => signOut()}
+          ><span class="i-mdi-logout h-6 w-6"> </span>
+          Logga ut</button
+        >
+        <a href={`/members/${user?.student_id}`} class="btn btn-ghost">
+          <span class="i-mdi-account-circle h-6 w-6"> </span>
+          Profil ({user?.student_id})</a
+        >
       {:else}
-        <button class="btn" on:click={() => signIn("keycloak")}>Logga in</button>
+        <button class="btn btn-ghost" on:click={() => signIn("keycloak")}>Logga in</button>
       {/if}
     </div>
     <!-- Page content here -->
@@ -75,21 +119,49 @@
     <label for="my-drawer-3" aria-label="close sidebar" class="drawer-overlay"></label>
     <div class="menu min-h-full w-80 bg-base-200">
       <!-- Sidebar content here -->
-      <div><a on:click={close} class="btn" href="/">Hem</a></div>
-      <div><a on:click={close} class="btn" href="/news">Nyheter</a></div>
-      <div><a on:click={close} class="btn" href="/events">Evenemang</a></div>
-      <div><a on:click={close} class="btn" href="/documents">Filer</a></div>
+      <div>
+        <a on:click={close} href="/" class="btn btn-ghost"
+          ><span class="i-mdi-home h-6 w-6 text-primary-focus"> </span>
+          Hem</a
+        >
+      </div>
+      <div>
+        <a on:click={close} href="/news" class="btn btn-ghost"
+          ><span class="i-mdi-newspaper h-6 w-6 text-primary-focus"> </span>
+          Nyheter</a
+        >
+      </div>
+      <div>
+        <a on:click={close} href="/events" class="btn btn-ghost"
+          ><span class="i-mdi-calendar h-6 w-6 text-primary-focus"> </span>
+          Evenemang</a
+        >
+      </div>
+      <div>
+        <a on:click={close} href="/documents" class="btn btn-ghost"
+          ><span class="i-mdi-text-box-multiple h-6 w-6 text-primary-focus"> </span>
+          Dokument</a
+        >
+      </div>
       <!-- https://bugs.webkit.org/show_bug.cgi?id=22261 -->
       <div class="dropdown-hover dropdown">
         <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
         <!-- svelte-ignore a11y-label-has-associated-control -->
-        <label tabindex="0" class="btn">Sektionen</label>
+        <label tabindex="0" class="btn btn-ghost"
+          ><DsekLogo className="h-6 w-6 text-primary-focus" />
+          Sektionen</label
+        >
         <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
         <ul
           tabindex="0"
           class="menu dropdown-content rounded-box z-[1] w-52 bg-base-100 p-2 shadow"
         >
-          <li><a on:click={close} href="/committees">Utskott</a></li>
+          <li>
+            <a on:click={close} href="/committees"
+              ><span class="i-mdi-account-group h-6 w-6 text-primary-focus"> </span>
+              Utskott</a
+            >
+          </li>
         </ul>
       </div>
       {#if accessPolicies.includes(apiNames.ACCESS_POLICY.READ)}
@@ -97,13 +169,21 @@
         <div class="dropdown-hover dropdown">
           <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
           <!-- svelte-ignore a11y-label-has-associated-control -->
-          <label tabindex="0" class="btn">Admin</label>
+          <label tabindex="0" class="btn btn-ghost"
+            ><span class="i-mdi-security h-6 w-6 text-primary-focus"> </span>
+            Admin</label
+          >
           <!-- svelte-ignore a11y-no-noninteractive-tabindex -->
           <ul
             tabindex="0"
             class="menu dropdown-content rounded-box z-[1] w-52 bg-base-100 p-2 shadow"
           >
-            <li><a on:click={close} href="/admin/access">Access</a></li>
+            <li>
+              <a on:click={close} href="/admin/access"
+                ><span class="i-mdi-key h-6 w-6 text-primary-focus"> </span>
+                Access</a
+              >
+            </li>
           </ul>
         </div>
       {/if}
