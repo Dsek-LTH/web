786aa55a5970f65371ff68979a6570213e1aef40
Ludvig <ludvig.svedberg@mac.com>
2023-11-16 01:14:01 +0100

    Add superforms for doors

--- src/routes/admin/doors/edit/[slug]/+page.server.ts
+++ src/routes/admin/doors/edit/[slug]/+page.server.ts
@@ -1,7 +1,10 @@
 import { policyAccessGuard, withAccess } from "$lib/utils/access";
 import apiNames from "$lib/utils/apiNames";
 import prisma from "$lib/utils/prisma";
+import { z } from "zod";
 import type { PageServerLoad } from "./$types";
+import { message, setError, superValidate } from "sveltekit-superforms/server";
+import { fail } from "@sveltejs/kit";
 
 export const load: PageServerLoad = async ({ parent, params }) => {
   const doorAccessPolicies = await prisma.doorAccessPolicy.findMany({
@@ -37,66 +40,76 @@ export const load: PageServerLoad = async ({ parent, params }) => {
   policyAccessGuard(apiNames.DOOR.READ, accessPolicies);
   return {
     doorAccessPolicies,
+    createForm: await superValidate(createSchema),
+    deleteForm: await superValidate(deleteSchema),
   };
 };
 
+const createSchema = z
+  .object({
+    studentId: z.string().min(1).optional(),
+    role: z.string().min(1).optional(),
+    startDatetime: z.date().optional(),
+    endDatetime: z.date().optional(),
+  })
+  .refine((data) => data.studentId != null || data.role != null, {
+    message: "Du måste ange roll och studentid",
+  });
+const deleteSchema = z.object({
+  id: z.string(),
+});
+
 export const actions = {
   create: async ({ request, locals, params }) => {
+    const form = await superValidate(request, createSchema);
+    if (!form.valid) return fail(400, { form });
     const session = await locals.getSession();
-    return withAccess(apiNames.DOOR.UPDATE, session?.user, async () => {
-      const data = await request.formData();
-      const doorName = params.slug;
-      const memberId = data.get("member");
-      const role = data.get("role");
-      const start = data.get("startDatetime");
-      const end = data.get("endDatetime");
-      console.log(start, end);
-
-      try {
+    return withAccess(
+      apiNames.DOOR.UPDATE,
+      session?.user,
+      async () => {
+        const doorName = params.slug;
+        const { studentId } = form.data;
+        if (
+          studentId &&
+          (await prisma.member.count({
+            where: { studentId },
+          })) <= 0
+        ) {
+          return setError(form, "studentId", "Medlemmen finns inte");
+        }
         await prisma.doorAccessPolicy.create({
           data: {
             doorName,
-            studentId: memberId && typeof memberId === "string" ? memberId : undefined,
-            role: role && typeof role === "string" ? role : undefined,
-            startDatetime: start && typeof start === "string" ? new Date(start) : undefined,
-            endDatetime: end && typeof end === "string" ? new Date(end) : undefined,
+            ...form.data,
           },
         });
-        return {
-          success: true,
-        };
-      } catch (e) {
-        return {
-          success: false,
-          error: "message" in (e as Error) ? (e as Error).message : "Unknown error",
-        };
-      }
-    });
+        return message(form, {
+          message: "Dörrpolicy skapad",
+          type: "success",
+        });
+      },
+      form
+    );
   },
   delete: async ({ request, locals }) => {
+    const form = await superValidate(request, deleteSchema);
+    if (!form.valid) return fail(400, { form });
     const session = await locals.getSession();
-    return withAccess(apiNames.DOOR.DELETE, session?.user, async () => {
-      const data = await request.formData();
-      const id = data.get("id");
-      if (typeof id !== "string") {
-        return {
-          success: false,
-          missing: true,
-        };
-      }
-      try {
+    return withAccess(
+      apiNames.DOOR.DELETE,
+      session?.user,
+      async () => {
+        const { id } = form.data;
         await prisma.doorAccessPolicy.delete({
           where: { id },
         });
-        return {
-          success: true,
-        };
-      } catch (e) {
-        return {
-          success: false,
-          error: "message" in (e as Error) ? (e as Error).message : "Unknown error",
-        };
-      }
-    });
+        return message(form, {
+          message: "Dörrpolicy raderad",
+          type: "success",
+        });
+      },
+      form
+    );
   },
 };
