a049f92cb192df63dcfdd6ff8928e135aa337bcb
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2023-12-24 00:54:27 +0100

    Change FullCalendar styling to avoid warnings

--- src/routes/bookings/Calendar.svelte
+++ src/routes/bookings/Calendar.svelte
@@ -4,6 +4,7 @@
   import daygridPlugin from "@fullcalendar/daygrid";
   import type { Bookable, BookingRequest } from "@prisma/client";
   import { goto } from "$app/navigation";
+  import "$lib/FullCalendar.css";
 
   export let bookingRequests: (BookingRequest & { bookables: Bookable[] })[] = [];
 
@@ -52,48 +53,3 @@
   class="min-w-0 flex-col break-words rounded-2xl border-0 bg-clip-border p-4 shadow-xl"
   bind:this={calendarEl}
 ></div>
-
-<style lang="postcss">
-  /* FullCalendar style overrides */
-  /* They need to be global since the classes are defined in the library. */
-
-  :global(.fc-button.fc-button-primary) {
-    @apply btn;
-  }
-
-  :global(.fc-button.fc-button.fc-button-primary.fc-button-active) {
-    @apply btn-primary;
-  }
-
-  :global(.fc-toolbar-title) {
-    @apply text-2xl font-bold;
-  }
-
-  :global(.fc-scrollgrid) {
-    @apply table;
-  }
-
-  :global(.fc-theme-standard td, .fc-theme-standard th) {
-    @apply border border-neutral-700;
-  }
-
-  :global(.fc-day-today.fc-day-today.fc-day-today) {
-    @apply bg-primary bg-opacity-20;
-  }
-
-  :global(.fc-scrollgrid.fc-scrollgrid) {
-    @apply border-0;
-  }
-
-  :global(.fc-daygrid tr > th:last-child, .fc-daygrid tr > td:last-child) {
-    @apply border-r-0;
-  }
-
-  :global(.fc-daygrid tr:last-child > td) {
-    @apply border-b-0;
-  }
-
-  :global(.fc-toolbar) {
-    @apply flex-wrap gap-2;
-  }
-</style>
