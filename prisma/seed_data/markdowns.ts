69ac3b0ae586573181f4362e006d5ece08668513
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-01-04 17:21:19 +0100

    Fix broken markdown seeding

--- prisma/seed_data/markdowns.ts
+++ prisma/seed_data/markdowns.ts
@@ -1,11 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
 export const insertMarkdowns = async (prisma: PrismaClient) => {
-  await prisma.markdown.delete({ where: { name: "cafe:hours" } });
-  await prisma.markdown.create({
-    data: {
+  await prisma.markdown.upsert({
+    where: { name: "cafe:open" },
+    create: {
       name: "cafe:open",
       markdown: "11:30 - 13:00",
     },
+    update: {},
   });
 };
