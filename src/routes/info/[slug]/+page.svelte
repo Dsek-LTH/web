9bd65631e8f1e34363b1db3f0473df9795ecfa66
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-01-05 18:47:52 +0100

    Fix aligment of edit button markdown pages

--- src/routes/info/[slug]/+page.svelte
+++ src/routes/info/[slug]/+page.svelte
@@ -3,10 +3,10 @@
   import MarkdownBody from "$lib/components/MarkdownBody.svelte";
 </script>
 
-{#if data && data.accessPolicies.includes(`markdowns:${data.slug}:update`)}
-  <a type="button" class="btn" href="{data.slug}/edit">Edit</a>
-{/if}
+<div class="flex flex-col items-center p-2 text-neutral-content">
+  {#if data && data.accessPolicies.includes(`markdowns:${data.slug}:update`)}
+    <a type="button" class="btn my-4" href="{data.slug}/edit">Edit</a>
+  {/if}
 
-<div class="p-2 text-neutral-content">
-  <MarkdownBody body={data.markdown?.markdown} class="mx-auto" />
+  <MarkdownBody body={data.markdown?.markdown} />
 </div>
