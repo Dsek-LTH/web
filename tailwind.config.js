3dffacb153b78d0fd5d5ffd6a2eb5aea3cf3b846
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-01-06 22:33:50 +0100

    Fix Tailwind dark variant usage with themeChange

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@ import { iconsPlugin, getIconCollections } from "@egoist/tailwindcss-icons";
 
 /** @type {import('tailwindcss').Config} */
 export default {
+  darkMode: ["class", '[data-theme="dark"]'], // dark mode set by class="dark" or data-theme="dark" in DOM
   mode: "jit",
   content: ["./src/**/*.{html,js,svelte,ts}"],
   safelist: [
