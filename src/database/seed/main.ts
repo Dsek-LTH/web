d52f19d3e0be18d8036268fb0ae5bbc6f15d6a96
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-01-09 18:52:25 +0100

    Add guideline comment for writing prisma seeds

--- src/database/seed/main.ts
+++ src/database/seed/main.ts
@@ -7,6 +7,12 @@ import { insertMarkdowns } from "./markdowns";
 
 const prisma = new PrismaClient();
 
+/**
+ * Calls all seed functions.
+ *
+ * Should be idempotent, meaning that it can be called multiple times,
+ * without changing the result, or at the very least without crashing.
+ */
 async function main() {
   await Promise.all([
     insertCommitteeLogos(prisma),
