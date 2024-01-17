e6c92890b0ff13e424a292dae043e377128f9763
Alfred Grip <alfred.grip@gmail.com>
2024-01-17 11:57:22 +0100

    Render Song as deleted if access

--- src/routes/[[lang=lang]]/songbook/SongElement.svelte
+++ src/routes/[[lang=lang]]/songbook/SongElement.svelte
@@ -1,10 +1,19 @@
 <script lang="ts">
+  import apiNames from "$lib/utils/apiNames";
   import type { Song } from "@prisma/client";
 
   export let song: Song;
+  export let accessPolicies: string[];
 </script>
 
 <article class="my-4 rounded-lg p-6 shadow-2xl ring-neutral-700 md:ring-1">
+  {#if accessPolicies.includes(apiNames.SONG.DELETE) && song.deletedAt != null}
+    <p class="text-xl font-bold text-red-500">Borttagen</p>
+    <p class="text-sm text-red-300">
+      Men du har access att se och återställa den
+    </p>
+  {/if}
+
   <div class="flex justify-between">
     <h2 class="my-3 text-2xl font-bold">
       <!-- Sanitized server side -->
