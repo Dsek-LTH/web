cd9d07cd13cf2c268efab6f27d1cabdc4cc1e338
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-01-31 21:51:27 +0100

    Remove automatic writes to i18n fields

--- src/database/prisma/translationExtension.ts
+++ src/database/prisma/translationExtension.ts
@@ -29,31 +29,6 @@ const translatedModelFields = {
 export default (lang: AvailableLanguageTag) =>
   Prisma.defineExtension({
     name: "translations",
-    query: {
-      $allModels: {
-        // Redirect all write operations to the translated fields
-        $allOperations({ model, args, query }) {
-          if (model in translatedModelFields && "data" in args && args.data) {
-            const fields =
-              translatedModelFields[
-                model as keyof typeof translatedModelFields
-              ];
-            const data = Object.entries(args.data)
-              .map(([key, value]) => {
-                if ((fields as string[]).includes(key) && lang === "en") {
-                  return {
-                    [`${key}En`]: value,
-                  };
-                }
-                return { [key]: value };
-              })
-              .reduce((acc, curr) => ({ ...acc, ...curr }), {});
-            args.data = data;
-          }
-          return query(args);
-        },
-      },
-    },
     // Redirect all read operations to the translated fields
     result: Object.entries(translatedModelFields).reduce(
       (acc, [model, fields]) => ({
