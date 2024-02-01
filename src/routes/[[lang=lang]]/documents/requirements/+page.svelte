c32f2ce39fbfb5fc89c3860a90199b5c3ff50b5c
Isak Kallini <isak.kallini@live.com>
2024-02-01 23:44:02 +0100

    Added requirements page (dummy text for now)

---
+++ src/routes/[[lang=lang]]/documents/requirements/+page.svelte
@@ -0,0 +1,6 @@
+<svelte:head>
+  <title>Kravprofiler | D-sektionen</title>
+</svelte:head>
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris id scelerisque
+metus, id condimentum lectus. Cras sed maximus est.
