082027bec7a4a7746c4493c8fa82196044b958b9
tair <tairstenen@gmail.com>
2024-02-05 16:57:33 +0100

    changing layout and

--- src/lib/components/Notification.svelte
+++ src/lib/components/Notification.svelte
@@ -1,4 +1,8 @@
 <script lang="ts">
+  export let notificationTitle: string;
+  export let notificationText: string;
+  export let notificationLink: string;
+
   const handleDelete = () => {
     console.log("Deleting notification");
   };
@@ -8,11 +12,19 @@
   };
 </script>
 
-<div>
-  <button class="btn btn-ghost" on:click={handleClick}>
-    Notification text
-  </button>
-  <button class="btn btn-ghost *:text-2xl" on:click={handleDelete}>
+<div class="relative rounded-none">
+  <a
+    href={notificationLink}
+    class="flex h-12 max-w-full flex-col"
+    on:click={handleClick}
+  >
+    <span class="truncate text-lg">{notificationTitle}</span>
+    <span class=" truncate text-xs">{notificationText}</span>
+  </a>
+  <button
+    class="btn btn-ghost absolute right-0 h-full w-auto rounded-none p-2 *:text-2xl"
+    on:click={handleDelete}
+  >
     <span class="i-mdi-delete-outline"></span>
   </button>
 </div>
--- src/routes/NotificationBell.svelte
+++ src/routes/NotificationBell.svelte
@@ -1,20 +1,11 @@
 <script lang="ts">
   import Notification from "$lib/components/Notification.svelte";
-  import type { FocusEventHandler } from "svelte/elements";
 
   let open = false;
 
   const handleClick = () => {
-    open = !open;
+    open = true;
   };
-
-  // const handleFocusloss = (
-  //   relatedTarget: FocusEventHandler<HTMLDivElement> | undefined | null,
-  //   currentTarget: any,
-  // ) => {
-  //   if (currentTarget.contains(relatedTarget)) return;
-  //   open = false;
-  // };
 </script>
 
 <div
@@ -27,9 +18,26 @@
     ><span class="i-mdi-bell-outline" /></button
   >
   <ul
-    class="menu dropdown-content right-0 w-72 rounded-box bg-base-100 p-2 shadow"
+    class="menu dropdown-content right-0 w-96 overflow-clip rounded-box bg-base-100 p-0 shadow"
   >
-    <li class="m-0 p-0"><Notification /></li>
-    <li class="m-0 p-0"><Notification /></li>
+    <li class="m-0 p-0">
+      <Notification
+        notificationText={"test"}
+        notificationTitle={"Title test"}
+        notificationLink={""}
+      />
+    </li>
+    <li class="m-0 p-0">
+      <Notification
+        notificationText={"longer text longer text longer text longer rtext tea wasdawdasdadawdasdaw yeeeee buddyyyyyyyyyyy"}
+        notificationTitle={"Title test 2 longer text buddyyyy light weeeeeeeeeight"}
+        notificationLink={""}
+      />
+    </li>
+    <button
+      class="btn btn-ghost rounded-none border-0 border-t border-gray-700 *:text-2xl"
+    >
+      Radera alla <span class="i-mdi-delete-outline" />
+    </button>
   </ul>
 </div>
