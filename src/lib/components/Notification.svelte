0c58ac785ceaf891b09546789a1afe0bcd29f655
tair <tairstenen@gmail.com>
2024-02-06 18:00:04 +0100

    design change

--- src/lib/components/Notification.svelte
+++ src/lib/components/Notification.svelte
@@ -18,10 +18,10 @@
     class="h-18 pointer-events-auto flex w-80 max-w-80 flex-col justify-center"
     on:click={handleClick}
   >
-    <span class="my-1 w-4/5 truncate text-lg font-bold"
+    <span class="my-1 w-11/12 truncate text-lg font-bold"
       >{notificationTitle}</span
     >
-    <span class="w-4/5 truncate text-wrap text-xs">{notificationText}</span>
+    <span class="w-11/12 truncate text-wrap text-xs">{notificationText}</span>
   </a>
   <button
     class="btn btn-ghost pointer-events-auto absolute right-0 z-10 h-full w-auto rounded-none p-2 *:text-2xl"
