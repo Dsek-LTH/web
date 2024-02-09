9986411740ece8527886c81c21d48f8bdea72b0b
Isak Kallini <isak.kallini@live.com>
2024-02-09 18:07:50 +0100

    Initial Onboarding design

---
+++ src/lib/components/Select.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import Labeled from "$lib/components/Labeled.svelte";
+  import { twMerge } from "tailwind-merge";
+
+  export let name: string;
+  export let label: string | null = null;
+  export let placeholder: string | null = label;
+  export let values: string[] | number[];
+  export let required: boolean | null = null;
+  export let error: string | string[] | undefined = undefined;
+  export let explanation: string | null = null;
+  let clazz: string = "";
+  export { clazz as class };
+</script>
+
+<Labeled {label} id={name} {error} {explanation}>
+  <select
+    id={name}
+    {name}
+    class={twMerge(" select select-bordered hover:border-base-content ", clazz)}
+    {placeholder}
+    {required}
+    {...$$restProps}
+  >
+    {#each values as value}
+      <option>{value}</option>
+    {/each}
+  </select>
+</Labeled>
---
+++ src/routes/onboarding/+page.svelte
@@ -0,0 +1,44 @@
+<script lang="ts">
+  import Input from "$lib/components/Input.svelte";
+  import Select from "$lib/components/Select.svelte";
+
+  const currentYear = new Date().getFullYear();
+  const years: number[] = Array.from(
+    { length: currentYear - 1961 + 1 },
+    (value, index) => 2024 - index,
+  );
+</script>
+
+<svelte:head>
+  <title>Onboarding | D-sektionen</title>
+</svelte:head>
+
+<div class="rounded-lg bg-base-200 p-10">
+  <div class="max-w-sm">
+    <div class="text-5xl">Welcome</div>
+    <div class="text-lg">Fill in your information below</div>
+
+    <div class="flex flex-col">
+      <Input name="Förnamn" label="Förnamn" placeholder="" />
+      <Input name="" label="Efternamn" placeholder="" />
+      <Input
+        name="pref"
+        label="Food preference"
+        placeholder="Ex. lactos, gluten, mushrooms"
+      />
+      <Select
+        name="program"
+        placeholder="program"
+        values={["D", "C"]}
+        label="Program"
+      />
+      <Select name="year" label="Year" values={years} />
+      <div class="pt-6">
+        <button name="save" class="btn bg-base-300 text-primary">
+          <span class="i-mdi-floppy-disc size-5 bg-primary"></span>
+          Save
+        </button>
+      </div>
+    </div>
+  </div>
+</div>
