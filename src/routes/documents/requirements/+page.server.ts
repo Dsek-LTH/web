361aa09d2ca1738c7a4bb4c52e3a4ebd9f55cafe
Isak <65332998+Isak-Kallini@users.noreply.github.com>
2024-02-15 21:37:12 +0100

    144 add kravprofiler page (#149)
    * Added requirements page (dummy text for now)
    
    * Adding kravprofiler page WIP
    
    * Started cleaning up a little
    
    * Made Kravprofiler page look a bit nicer
    
    * Added upload button, editing needs fixing
    
    * Fixed editing kravprofiler
    
    * Upload button links to correct document upload
    
    * Fixed folder sorting
    
    * Folder opens when clicked
    
    * Added comments, removed console.log
    
    * Moved requirements page to new folder structure
    
    * Update src/routes/documents/requirements/+page.svelte
    
    Co-authored-by: Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
    
    * Update src/routes/documents/requirements/+page.server.ts
    
    Co-authored-by: Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
    
    * Fixed review comments
    
    Update src/routes/documents/requirements/+page.svelte
    
    Co-authored-by: Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
    
    Update src/routes/documents/requirements/+page.server.ts
    
    Co-authored-by: Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>

---
+++ src/routes/documents/requirements/+page.server.ts
@@ -0,0 +1,62 @@
+import { PUBLIC_BUCKETS_DOCUMENTS } from "$env/static/public";
+import { fileHandler } from "$lib/files";
+import type { PageServerLoad, Actions } from "./$types";
+import { message, superValidate } from "sveltekit-superforms/server";
+import { z } from "zod";
+import type { FileData } from "$lib/files/fileHandler";
+import { fail } from "@sveltejs/kit";
+
+export type FolderType = {
+  id: string;
+  name: string;
+  isFolder: boolean;
+  url: string;
+  files: FolderType[];
+};
+
+export const load: PageServerLoad = async ({ locals, url }) => {
+  const { user } = locals;
+  const year = url.searchParams.get("year") || new Date().getFullYear();
+  const files = await fileHandler.getInBucket(
+    user,
+    PUBLIC_BUCKETS_DOCUMENTS,
+    "requirement/" + year,
+    true,
+  );
+  console.log(files);
+  const filesGroupedByFolder = files.reduce<Record<string, FileData[]>>(
+    (acc, file) => {
+      const fileParts = file.id.split("/");
+      const folder = //get the folder structure starting 2 "steps" in, we don't want the requirements/[year] part of the filepath
+        fileParts.slice(2, fileParts.length - 1).join("/") ?? "unknown";
+      if (!acc[folder]) acc[folder] = [];
+      acc[folder]?.push(file);
+      return acc;
+    },
+    {},
+  );
+  return {
+    files,
+    folders: filesGroupedByFolder,
+    deleteForm: await superValidate(deleteSchema),
+  };
+};
+
+const deleteSchema = z.object({
+  id: z.string(),
+});
+export type DeleteSchema = typeof deleteSchema;
+
+export const actions: Actions = {
+  deleteFile: async ({ request, locals }) => {
+    const { user } = locals;
+    const form = await superValidate(request, deleteSchema);
+    if (!form.valid) return fail(400, { form });
+    const { id } = form.data;
+    await fileHandler.remove(user, PUBLIC_BUCKETS_DOCUMENTS, [id]);
+    return message(form, {
+      message: "Fil borttagen",
+      type: "success",
+    });
+  },
+};
