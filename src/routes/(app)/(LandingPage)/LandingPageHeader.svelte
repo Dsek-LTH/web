d330c864578c9082d94c1c715a2f64c884579fe2
Ludvig <ludvig.svedberg@mac.com>
2024-03-01 19:26:36 +0100

    Refactor landing page

---
+++ src/routes/(app)/(LandingPage)/LandingPageHeader.svelte
@@ -0,0 +1,26 @@
+<header class="contents">
+  <img
+    class="h-full w-full object-cover md:col-span-2"
+    src="./hero-image.jpg"
+    alt="Cover"
+  />
+  <h1 class="text-balance text-5xl font-bold lg:text-6xl">
+    Det <span class="text-primary">roliga</span> med plugget
+  </h1>
+  <div>
+    <p class="mb-8">
+      <strong>D-sektionen inom TLTH</strong> är en ideell organisation för
+      studenter och alumner vid programmen
+      <span class="text-primary">Datateknik</span> och
+      <span class="text-secondary">InfoCom</span>. Sektionen har sociala
+      arrangemang, näringslivskontakter, studiebevakning, och allt annat som
+      hjälper studenter och alumner.
+    </p>
+    <a
+      class="btn btn-primary justify-self-start rounded-none px-10 font-bold uppercase"
+      href="/info/for-foretag"
+    >
+      För företag
+    </a>
+  </div>
+</header>
