2ab29d1919304c357ead5e7712cc7cfd32238fb2
BlixtSamme <samuel.hogfeldt@gmail.com>
2024-03-03 02:00:19 +0100

    forgot to copy over some files

---
+++ src/routes/(app)/bookings/Calendar.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  import { Calendar } from "@fullcalendar/core";
+  import daygridPlugin from "@fullcalendar/daygrid";
+  import type { Bookable, BookingRequest } from "@prisma/client";
+  import { goto } from "$app/navigation";
+  import "$lib/FullCalendar.css";
+
+  export let bookingRequests: (BookingRequest & { bookables: Bookable[] })[] =
+    [];
+
+  let calendarEl: HTMLElement;
+  let calendar: Calendar;
+
+  onMount(() => {
+    calendar = new Calendar(calendarEl, {
+      initialView: "dayGridMonth",
+      plugins: [daygridPlugin],
+      events: bookingRequests.map((booking) => ({
+        title: booking.event ?? "",
+        start: booking.start ?? new Date(),
+        end: booking.end ?? new Date(),
+      })),
+      locale: "sv",
+      eventColor: "#f280a1",
+      firstDay: 1,
+      headerToolbar: {
+        left: "prev,next,addEvent",
+        center: "title",
+        right: "dayGridDay,dayGridWeek,dayGridMonth",
+      },
+      customButtons: {
+        subscribe: {
+          text: "Subscribe",
+          click: () => {
+            void goto("/events/subscribe");
+          },
+        },
+      },
+      buttonText: {
+        today: "Idag",
+        month: "Månad",
+        week: "Vecka",
+        day: "Dag",
+      },
+    });
+    calendar.render();
+
+    return () => calendar?.destroy();
+  });
+</script>
+
+<div
+  class="min-w-0 flex-col break-words rounded-2xl border-0 bg-clip-border p-4 shadow-xl"
+  bind:this={calendarEl}
+></div>
