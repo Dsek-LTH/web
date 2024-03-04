076fb33fd1fafe2ef608a7ecb2dbf77045dc39a5
Ludvig <ludvig.svedberg@mac.com>
2024-03-04 22:59:41 +0100

    Fix landing page articles and events

--- src/lib/components/MarkdownBody.svelte
+++ src/lib/components/MarkdownBody.svelte
@@ -3,17 +3,28 @@
   import { marked } from "marked";
   import { twMerge } from "tailwind-merge";
 
+  export let bare = false;
+  export let wrappedLink = false; // if body is wrapped in an anchor tag (has to be positioned absolute and inset-0)
   let clazz = "";
   export { clazz as class };
   export let body: string;
+  $: mergedClass = (() => {
+    if (bare) return clazz;
+    let merged = twMerge(
+      "prose prose-a:link-primary prose-a:no-underline",
+      clazz,
+    );
+    if (wrappedLink) {
+      merged = twMerge(
+        "pointer-events-none relative prose-a:pointer-events-auto",
+        merged,
+      );
+    }
+    return merged;
+  })();
 </script>
 
-<section
-  class={twMerge(
-    "prose-a prose prose-a:link prose-a:link-primary lg:prose-xl prose-a:no-underline",
-    clazz,
-  )}
->
+<section class={mergedClass}>
   <slot />
   <!-- eslint-disable-next-line svelte/no-at-html-tags -- Sanitized client-side -->
   {@html marked(DOMPurify.sanitize(body))}
--- src/routes/(app)/(LandingPage)/LandingPageEvents.svelte
+++ src/routes/(app)/(LandingPage)/LandingPageEvents.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
-  import { marked } from "marked";
-
+  import MarkdownBody from "$lib/components/MarkdownBody.svelte";
   import type { Event } from "@prisma/client";
   export let events: Array<
     Pick<Event, "slug" | "title" | "description" | "startDatetime">
@@ -9,31 +8,39 @@
 
 <ol class="contents" role="article">
   {#each events as event}
-    <li>
-      <a href="/events/{event.slug}" class="flex align-top">
-        <div class="mr-5 h-24 w-24 min-w-24 bg-base-300">
-          <div
-            class="flex h-5 items-center justify-center bg-primary text-sm font-bold capitalize text-black"
-          >
-            {event.startDatetime.toLocaleString("sv-SE", {
-              weekday: "long",
-            })}
-          </div>
-          <p class="text-center text-6xl font-semibold">
-            {event.startDatetime.getDay().toString().padStart(2, "0")}
-          </p>
+    <li class="relative flex align-top">
+      <!-- svelte-ignore a11y-missing-content -->
+      <a
+        href="/events/{event.slug}"
+        aria-hidden="true"
+        class="absolute inset-0 z-0"
+      />
+      <div class="mr-5 h-24 w-24 min-w-24 bg-base-300">
+        <div
+          class="flex h-5 items-center justify-center bg-primary text-sm font-bold capitalize text-base-100"
+        >
+          {event.startDatetime.toLocaleString("sv-SE", {
+            weekday: "long",
+          })}
         </div>
+        <div class="text-center text-6xl font-semibold">
+          {event.startDatetime.getDay().toString().padStart(2, "0")}
+        </div>
+      </div>
 
-        <div class="min-w-0">
-          <h2 class="mb-4 text-xl font-bold leading-none">
+      <div class="flex min-w-0 flex-col justify-between overflow-hidden">
+        <!-- for screen readers, we icnlude this anchor -->
+        <a href="/events/{event.slug}">
+          <h2 class="line-clamp-1 text-xl font-bold leading-none">
             {event.title}
           </h2>
-          <p class="line-clamp-3 break-words">
-            <!-- eslint-disable-next-line svelte/no-at-html-tags - Sanitized server-side -->
-            {@html marked(event.description)}
-          </p>
-        </div>
-      </a>
+        </a>
+        <MarkdownBody
+          wrappedLink
+          body={event.description}
+          class="line-clamp-3 text-ellipsis leading-snug"
+        />
+      </div>
     </li>
   {/each}
 </ol>
