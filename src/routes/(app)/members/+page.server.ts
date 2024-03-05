562e63d9eb694f1f3529e455f27a25ec8738a44a
Alfred Grip <alfred.grip@gmail.com>
2024-03-05 20:26:39 +0100

    reuse variable

--- src/routes/(app)/members/+page.server.ts
+++ src/routes/(app)/members/+page.server.ts
@@ -24,7 +24,7 @@ export const load: PageServerLoad = async (request) => {
           ? {
               // dont actually show ALL members in db, only those in the specified programmes
               // we have some members for other programmes, but they are not part of the guild
-              in: ["D", "C", "VR/AR"],
+              in: allowedProgrammes,
             }
           : {
               equals: classProgramme,
