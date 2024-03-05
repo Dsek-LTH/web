e4ea437b3973e6c66e09f4cbf544b964154439c8
Alfred Grip <64659799+alfredgrip@users.noreply.github.com>
2024-03-05 21:53:22 +0100

    Class year page (#171)
    * Add page for class year
    
    * make classBadge clickable
    
    * allow for VR/AR, don't expose members from other guillds
    
    * fix missing query name update
    
    * simplify prisma query
    
    * reuse variable
    
    * Change sort order
    
    * Show class badge when viewing all programmes
    
    * Make page title uppercase
    
    ---------
    
    Co-authored-by: Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>

---
+++ src/routes/(app)/members/+page.server.ts
@@ -0,0 +1,51 @@
+import { error } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+
+const allowedProgrammes = ["D", "C", "VR/AR"];
+
+export const load: PageServerLoad = async (request) => {
+  const { prisma, user } = request.locals;
+  if (!user) {
+    error(401, "Not logged in");
+  }
+  let classProgramme = request.url.searchParams.get("programme");
+  if (!classProgramme || !allowedProgrammes.includes(classProgramme)) {
+    classProgramme = "all";
+  }
+  let classYear = parseInt(request.url.searchParams.get("year") ?? "");
+  if (isNaN(classYear)) {
+    classYear = new Date().getFullYear();
+  }
+  const members = await prisma.member.findMany({
+    where: {
+      classYear,
+      classProgramme:
+        classProgramme === "all"
+          ? {
+              // dont actually show ALL members in db, only those in the specified programmes
+              // we have some members for other programmes, but they are not part of the guild
+              in: allowedProgrammes,
+            }
+          : {
+              equals: classProgramme,
+            },
+    },
+    orderBy: [
+      {
+        firstName: "asc",
+      },
+      {
+        lastName: "asc",
+      },
+      {
+        classProgramme: "asc",
+      },
+    ],
+  });
+
+  return {
+    members,
+    programme: classProgramme,
+    year: classYear,
+  };
+};
