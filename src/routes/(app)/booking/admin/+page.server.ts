8bf24912580a1bd23002268b66f63ae344f1bb5f
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-03-12 23:57:19 +0100

    Restrict access to booking admin page

--- src/routes/(app)/booking/admin/+page.server.ts
+++ src/routes/(app)/booking/admin/+page.server.ts
@@ -1,7 +1,11 @@
+import apiNames from "$lib/utils/apiNames";
+import { authorize } from "$lib/utils/authorization";
 import type { Actions, PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ locals }) => {
-  const { prisma } = locals;
+  const { prisma, user } = locals;
+  authorize(apiNames.BOOKINGS.UPDATE, user);
+
   const bookingRequests = await prisma.bookingRequest.findMany({
     where: {
       start: {
