8de7c29cc491f4aca35574e352c65ae57f83a7f5
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-03-13 15:48:44 +0100

    Avoid running migrations in Vercel on PR branches

---
+++ vercel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+ 
+if [[ $VERCEL_ENV == "production"  ]] ; then 
+  pnpm run build && pnpm prisma migrate deploy
+else 
+  # prod and preview use the same database.
+  # We don't want to risk breaking it with a work-in-progress migration in a PR.
+  pnpm run build
+fi
