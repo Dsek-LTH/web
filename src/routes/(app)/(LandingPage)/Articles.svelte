fb348e5de5100b93e502ed3f647c688bb8fa4398
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-03-14 20:11:29 +0100

    Change handling of landing page markdown text

--- src/routes/(app)/(LandingPage)/Articles.svelte
+++ src/routes/(app)/(LandingPage)/Articles.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import MarkdownBody from "$lib/components/MarkdownBody.svelte";
+  import { markdownToTxt } from "markdown-to-txt";
   import type { Article } from "@prisma/client";
 
   export let news: Array<Pick<Article, "header" | "body" | "slug">>;
@@ -9,31 +9,15 @@
   class="flex flex-col divide-y divide-dashed divide-base-100 divide-opacity-50"
 >
   {#each news as article (article.slug)}
-    <li class="group relative py-5 text-base-100">
-      <!-- svelte-ignore a11y-missing-content -->
-      <a
-        href="/news/{article.slug}"
-        aria-hidden="true"
-        class="absolute inset-0 z-0"
-      />
+    <li class="group py-5 text-black">
       <a href="/news/{article.slug}">
         <h2 class="mb-2 text-xl font-bold group-hover:underline">
           {article.header}
         </h2>
+        <p class="line-clamp-2 text-ellipsis leading-normal md:line-clamp-3">
+          {markdownToTxt(article.body)}
+        </p>
       </a>
-      <MarkdownBody
-        wrappedLink
-        body={article.body}
-        class="
-        line-clamp-2 text-ellipsis 
-        leading-normal text-base-100 
-        prose-headings:text-base prose-headings:text-base-100 
-        prose-p:my-0 
-        prose-a:z-10 prose-a:text-base-100 prose-a:underline hover:prose-a:text-base-100 hover:prose-a:no-underline 
-        prose-strong:text-base-100
-        md:line-clamp-3 lg:line-clamp-3 
-        "
-      />
     </li>
   {/each}
 </ol>
