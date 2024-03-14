d9f7abd3ac17da5ddcac839c239deb185c6ad68f
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-03-14 15:54:49 +0100

    Change PDF file modal button to link

--- src/routes/(app)/documents/FileButton.svelte
+++ src/routes/(app)/documents/FileButton.svelte
@@ -1,28 +1,26 @@
 <script lang="ts">
-  import { goto } from "$app/navigation";
-
   export let name: string;
   export let url: string;
   export let onClick: (url: string) => void;
 </script>
 
 <div class="flex items-center gap-2 py-2">
-  <button
+  <a
     class="link flex items-center gap-2 text-left no-underline hover:underline"
+    href={url}
     on:click={(e) => {
-      // don't open modal if opening a new tab, or we're on too small a screen
-      if (e.metaKey || window.innerWidth < 640) {
-        goto(url);
-      } else {
-        onClick(url);
-      }
+      // don't open modal if we're on too small a screen
+      if (window.innerWidth < 768) return;
+
+      e.preventDefault();
+      onClick(url);
     }}
   >
     <span
       class="i-mdi-file-document-outline align-text-top text-xl text-primary"
     />
     {name}
-  </button>
+  </a>
 
   <a
     href={url}
