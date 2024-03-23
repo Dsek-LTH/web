e7de0aa42b6608e41183aa851946fa431fc5de03
Isak Kallini <isak.kallini@live.com>
2024-03-23 14:33:22 +0100

    Refresh data on language switch

--- src/routes/LanguageSwitcher.svelte
+++ src/routes/LanguageSwitcher.svelte
@@ -2,14 +2,25 @@
   import { languageTag } from "$paraglide/runtime";
   import { page } from "$app/stores";
   import { i18n } from "$lib/utils/i18n";
+  import { invalidateAll } from "$app/navigation";
 </script>
 
 {#if languageTag() === "sv"}
-  <a href={i18n.route($page.url.pathname)} class="btn btn-ghost" hreflang="en">
+  <a
+    href={i18n.route($page.url.pathname)}
+    class="btn btn-ghost"
+    hreflang="en"
+    on:click={() => invalidateAll()}
+  >
     EN
   </a>
 {:else if languageTag() === "en"}
-  <a href={i18n.route($page.url.pathname)} class="btn btn-ghost" hreflang="sv">
+  <a
+    href={i18n.route($page.url.pathname)}
+    class="btn btn-ghost"
+    hreflang="sv"
+    on:click={() => invalidateAll()}
+  >
     SV
   </a>
 {/if}
