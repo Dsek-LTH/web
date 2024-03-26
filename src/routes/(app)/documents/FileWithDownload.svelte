ee01618ea3ea8f9f65729d46e200145b2776296b
Alfred Grip <64659799+alfredgrip@users.noreply.github.com>
2024-03-26 20:40:11 +0100

    PDF modal (#184)
    * add util func
    
    * add pdf endpoint
    
    * make gov docs modals
    
    * lint fix
    
    * Simplify PDF API route
    
    * Move dialog out of PDFModal component
    
    * Rename PDF modal to FileButton
    
    * Disable PDF modal on small screens
    
    * Change PDF file modal button to link
    
    * Rename FileButton to FileWithDownload
    
    * rename component usage
    
    * left align download button
    
    * Revert "left align download button"
    
    This reverts commit d901bf4d5ca37478570e2094ec77c47294718c2a.
    
    ---------
    
    Co-authored-by: Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>

---
+++ src/routes/(app)/documents/FileWithDownload.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  export let name: string;
+  export let url: string;
+  export let onClick: (url: string) => void;
+</script>
+
+<div class="flex items-center gap-2 py-2">
+  <a
+    class="link flex items-center gap-2 text-left no-underline hover:underline"
+    href={url}
+    on:click={(e) => {
+      // don't open modal if we're on too small a screen
+      if (window.innerWidth < 768) return;
+
+      e.preventDefault();
+      onClick(url);
+    }}
+  >
+    <span
+      class="i-mdi-file-document-outline align-text-top text-xl text-primary"
+    />
+    {name}
+  </a>
+
+  <a
+    href={url}
+    download={name}
+    class="link no-underline hover:link-primary hover:underline"
+  >
+    <span class="i-mdi-download align-text-top text-xl" />
+  </a>
+</div>
