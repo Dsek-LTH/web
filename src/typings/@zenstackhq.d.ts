afebd6103fa908e52b10a31f43f7ac220c4da3a6
Ludvig <ludvig.svedberg@mac.com>
2024-04-07 23:06:07 +0200

    Expose user externalCode to locals.user object

--- src/typings/@zenstackhq.d.ts
+++ src/typings/@zenstackhq.d.ts
@@ -2,8 +2,8 @@ import "@zenstackhq/runtime";
 
 declare module "@zenstackhq/runtime" {
   export interface AuthUser {
-    studentId: string;
-    memberId: string;
+    studentId?: string;
+    memberId?: string;
     policies: string[];
     externalCode?: string; // for non-authenticated users
   }
