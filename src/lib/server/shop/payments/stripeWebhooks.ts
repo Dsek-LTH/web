44021f29cd77dfc9ed9382c68cd6765f90fa3790
Ludvig <ludvig.svedberg@mac.com>
2024-04-10 13:17:33 +0200

    Try to save a webhook where we can't find the intent id

--- src/lib/server/shop/payments/stripeWebhooks.ts
+++ src/lib/server/shop/payments/stripeWebhooks.ts
@@ -13,10 +13,37 @@ export const onPaymentSuccess = async (intent: Stripe.PaymentIntent) => {
     },
   });
   if (updated.count === 0) {
-    console.error(`No consumables found for intent ${intent.id}`);
-    throw new Error("No consumables found for intent"); // this is a big issue
+    await tryToSavePaymentIntent(intent);
   }
   // TODO: Notify user of successful payment
+};
+
+export const tryToSavePaymentIntent = async (intent: Stripe.PaymentIntent) => {
+  const consumableIds = intent.metadata?.["consumableIds"]?.split(", ");
+  if (!consumableIds) {
+    console.error(
+      `No consumables found for intent ${intent.id}. Metadata was corrupt. Metadata: ${intent.metadata}`,
+    );
+    throw new Error("No consumables found for intent"); // this is a big issue
+  }
+  const updated = await authorizedPrismaClient.consumable.updateMany({
+    where: {
+      id: {
+        in: consumableIds,
+      },
+    },
+    data: {
+      stripeIntentId: intent.id,
+      purchasedAt: new Date(),
+    },
+  });
+  if (updated.count === 0) {
+    console.error(
+      `No consumables found for intent ${intent.id}. Not found in db.`,
+    );
+    throw new Error("No consumables found for intent"); // this is the biggest issue
+  }
+  // we saved it. Nice!
 };
 
 export const onPaymentFailure = async (intent: Stripe.PaymentIntent) => {
