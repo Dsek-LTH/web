f3baa9393b2a6a483fbcc11f7926d92b3fb7008a
Erik Heimdal <63123731+ekvanox@users.noreply.github.com>
2024-04-16 18:55:44 +0200

    Feat: Add delete button (#152)
    * fix: :bug: incorrect event creation redirect url
    
    * feat: :sparkles: add simple deletion mechanism
    
    * feat: :fire: use false deletion flag
    
    * refactor: :mute: remove console.log statement
    
    * feat: :lipstick: add icons to buttons
    
    * refactor: :recycle: rework functionality
    
    * refactor: :coffin: remove unused code

--- src/routes/(app)/events/[slug]/+page.server.ts
+++ src/routes/(app)/events/[slug]/+page.server.ts
@@ -9,9 +9,15 @@ import { error } from "@sveltejs/kit";
 import { superValidate } from "sveltekit-superforms/server";
 import { getEvent } from "../events";
 import type { Actions, PageServerLoad } from "./$types";
+import { isAuthorized } from "$lib/utils/authorization";
+import apiNames from "$lib/utils/apiNames";
+import {
+  removeEventAction,
+  removeEventSchema,
+} from "../../news/removeEventAction";
 
 export const load: PageServerLoad = async ({ locals, params }) => {
-  const { prisma } = locals;
+  const { prisma, user } = locals;
   const event = await getEvent(prisma, params.slug);
   if (event == undefined) {
     throw error(404, {
@@ -19,15 +25,21 @@ export const load: PageServerLoad = async ({ locals, params }) => {
     });
   }
   const allTaggedMembers = await getAllTaggedMembers(prisma, event.comments);
+  const canEdit = isAuthorized(apiNames.EVENT.UPDATE, user);
+  const canDelete = isAuthorized(apiNames.EVENT.DELETE, user);
   return {
     event,
     allTaggedMembers,
+    canEdit,
+    canDelete,
     commentForm: await superValidate(commentSchema),
     removeCommentForm: await superValidate(removeCommentSchema),
+    removeEventForm: await superValidate(removeEventSchema),
   };
 };
 
 export const actions: Actions = {
   comment: commentAction("EVENT"),
   removeComment: removeCommentAction("EVENT"),
+  removeEvent: removeEventAction(),
 };
---
+++ src/routes/(app)/news/removeEventAction.ts
@@ -0,0 +1,47 @@
+import apiNames from "$lib/utils/apiNames";
+import { authorize } from "$lib/utils/authorization";
+import { error, fail, type RequestEvent } from "@sveltejs/kit";
+import { redirect } from "sveltekit-flash-message/server";
+import { superValidate } from "sveltekit-superforms/server";
+import { z } from "zod";
+
+export const removeEventSchema = z.object({
+  eventId: z.string(),
+});
+export type RemoveEventSchema = typeof removeEventSchema;
+
+export const removeEventAction =
+  () => async (event: RequestEvent<Record<string, string>, string>) => {
+    const { request, locals } = event;
+    const { prisma, user } = locals;
+
+    const form = await superValidate(request, removeEventSchema);
+    if (!form.valid) return fail(400, { form });
+    authorize(apiNames.EVENT.DELETE, user);
+
+    const existingEvent = await prisma.event.findUnique({
+      where: {
+        id: form.data.eventId,
+      },
+    });
+
+    if (!existingEvent) return error(404, "Event not found");
+
+    await prisma.event.update({
+      where: {
+        id: existingEvent.id,
+      },
+      data: {
+        removedAt: new Date(),
+      },
+    });
+
+    throw redirect(
+      "/events",
+      {
+        message: "Event raderat",
+        type: "success",
+      },
+      event,
+    );
+  };
