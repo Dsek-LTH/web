fdce3fd9cff8509aa094f14db45ac79ef328fe65
Oliver Levay <oliver.levay@hotmail.com>
2024-04-20 21:28:34 +0200

    Align header, content and footer

--- src/routes/Footer.svelte
+++ src/routes/Footer.svelte
@@ -1,36 +1,39 @@
-<footer class="footer items-center gap-y-2 bg-base-300 p-2">
-  <nav
-    class="grid-flow-col place-self-center md:place-self-start"
-    aria-label="Contact and source code"
-  >
-    <a class="btn btn-ghost" href="/info/contact">Kontakt</a>
-    <a class="btn btn-ghost" href="https://github.com/Dsek-LTH/web">Källkod</a>
-  </nav>
+<footer class="bg-base-300">
+  <div class="container footer mx-auto items-center gap-y-2 p-2">
+    <nav
+      class="grid-flow-col place-self-center md:place-self-start"
+      aria-label="Contact and source code"
+    >
+      <a class="btn btn-ghost" href="/info/contact">Kontakt</a>
+      <a class="btn btn-ghost" href="https://github.com/Dsek-LTH/web">Källkod</a
+      >
+    </nav>
 
-  <nav
-    class="grid-flow-col place-self-center *:text-xl md:place-self-end"
-    aria-label="Social media"
-  >
-    <a class="btn btn-ghost" href="https://instagram.com/dseklth">
-      <span class="i-mdi-instagram" />
-    </a>
-    <a class="btn btn-ghost" href="https://facebook.com/Dsektionen">
-      <span class="i-mdi-facebook" />
-    </a>
-    <a
-      class="btn btn-ghost"
-      href="https://youtube.com/channel/UCqBtN7xlh4_VvywKaRiGfkw"
+    <nav
+      class="grid-flow-col place-self-center *:text-xl md:place-self-end"
+      aria-label="Social media"
     >
-      <span class="i-mdi-youtube" />
-    </a>
-    <a class="btn btn-ghost" href="https://github.com/Dsek-LTH">
-      <span class="i-mdi-github" />
-    </a>
-    <a
-      class="btn btn-ghost"
-      href="https://linkedin.com/company/datatekniksektionen-vid-tlth"
-    >
-      <span class="i-mdi-linkedin" />
-    </a>
-  </nav>
+      <a class="btn btn-ghost" href="https://instagram.com/dseklth">
+        <span class="i-mdi-instagram" />
+      </a>
+      <a class="btn btn-ghost" href="https://facebook.com/Dsektionen">
+        <span class="i-mdi-facebook" />
+      </a>
+      <a
+        class="btn btn-ghost"
+        href="https://youtube.com/channel/UCqBtN7xlh4_VvywKaRiGfkw"
+      >
+        <span class="i-mdi-youtube" />
+      </a>
+      <a class="btn btn-ghost" href="https://github.com/Dsek-LTH">
+        <span class="i-mdi-github" />
+      </a>
+      <a
+        class="btn btn-ghost"
+        href="https://linkedin.com/company/datatekniksektionen-vid-tlth"
+      >
+        <span class="i-mdi-linkedin" />
+      </a>
+    </nav>
+  </div>
 </footer>
