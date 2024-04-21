87ca1377de6d46290e77f034689500a035d9e6f1
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-04-21 02:07:42 +0200

    Add simple ticket page

--- src/routes/(app)/shop/tickets/TicketCard/TicketCard.svelte
+++ src/routes/(app)/shop/tickets/TicketCard/TicketCard.svelte
@@ -1,9 +1,5 @@
-<script context="module">
-</script>
-
 <script lang="ts">
   import type { SuperValidated } from "sveltekit-superforms";
-
   import type { AddToCartSchema } from "../+page.server";
   import type { TicketWithEvent } from "../types";
   import TicketActions from "./TicketActions.svelte";
@@ -13,21 +9,25 @@
   export let addToCartForm: SuperValidated<AddToCartSchema>;
 </script>
 
-<div class="group card card-compact overflow-hidden bg-base-300 shadow-xl">
-  <TicketEvent event={ticket.event} />
-  <div class="card-body">
-    <div class="flex items-start justify-between">
-      <h2 class="card-title">{ticket.shoppable.title}</h2>
-      <div class="flex flex-col">
-        <span class="card-title text-success">
-          {ticket.shoppable.price / 100} SEK
-        </span>
-        <span class="text-right">
-          {ticket.stock - ticket.shoppable._count.consumables} kvar
-        </span>
+<a href="/shop/tickets/{ticket.id}">
+  <div class="group card card-compact overflow-hidden bg-base-300 shadow-xl">
+    <TicketEvent event={ticket.event} />
+    <div class="card-body">
+      <div class="flex items-start justify-between">
+        <h2 class="card-title">
+          {ticket.shoppable.title}
+        </h2>
+        <div class="flex flex-col">
+          <span class="card-title text-success">
+            {ticket.shoppable.price / 100} SEK
+          </span>
+          <span class="text-right">
+            {ticket.stock - ticket.shoppable._count.consumables} kvar
+          </span>
+        </div>
       </div>
+      <p>{ticket.shoppable.description}</p>
+      <TicketActions {ticket} {addToCartForm} />
     </div>
-    <p>{ticket.shoppable.description}</p>
-    <TicketActions {ticket} {addToCartForm} />
   </div>
-</div>
+</a>
