ff7d13b258dc2101922eefa57582cc47ed792683
IsakKallini <isak.kallini@live.com>
2024-05-02 22:16:20 +0200

    Deploy done

---
+++ deploy-script.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+cd /server/dsek-web 
+
+# Remove everything but the node_modules folder, .env is also excluded by default
+shopt -s extglob
+rm -r !(node_modules|build.zip) 
+
+unzip build.zip -d .
+npm i --ignore-scripts
+
+sudo systemctl restart dsek-web.service 
