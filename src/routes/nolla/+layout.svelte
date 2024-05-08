e86c16f63c5c9015bcd908224db2753d43207328
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-05-08 11:27:14 +0200

    Add hover effects to nolla layout navbar

--- src/routes/nolla/+layout.svelte
+++ src/routes/nolla/+layout.svelte
@@ -3,10 +3,16 @@
   class="flex flex-1 flex-col font-nolla-sans"
 >
   <nav class="container mx-auto flex h-36 items-center justify-between py-10">
-    <div class="flex gap-24 text-2xl font-semibold">
-      <a href="/nolla">Start</a>
-      <a href="/nolla/nollning">Nollning?</a>
-      <a href="/nolla/sektionen">Sektionen</a>
+    <div
+      class="flex gap-24 text-2xl font-semibold *:bg-gradient-to-r *:from-primary *:to-primary *:bg-[length:0_2px] *:bg-bottom *:bg-no-repeat *:transition-all"
+    >
+      <a href="/nolla" class="hover:bg-[length:100%_2px]">Start</a>
+      <a href="/nolla/nollning" class="hover:bg-[length:100%_2px]">
+        Nollning
+      </a>
+      <a href="/nolla/sektionen" class="hover:bg-[length:100%_2px]">
+        Sektionen
+      </a>
     </div>
 
     <button
