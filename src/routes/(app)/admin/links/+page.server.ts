a3fbc8371a4947ff16a1274015caf3da8df07fbf
Esbjörn Stenberg <me@stagrim.com>
2024-05-08 17:08:30 +0200

    Adds orderBy to table

--- src/routes/(app)/admin/links/+page.server.ts
+++ src/routes/(app)/admin/links/+page.server.ts
@@ -1,17 +1,25 @@
 import type { PageServerLoad } from "./$types";
-import {
-  ShlinkApiClient,
-  type HttpClient,
-  type RequestOptions,
-} from "@shlinkio/shlink-js-sdk";
-import { SHLINK_ENDPOINT, SHLINK_API_KEY } from "$env/static/private";
+import { ShlinkApiClient } from "@shlinkio/shlink-js-sdk";
+import { type ProblemDetailsError } from "@shlinkio/shlink-js-sdk/api-contract";
+import { env } from "$env/dynamic/private";
 import { NodeHttpClient } from "@shlinkio/shlink-js-sdk/node";
-import { error } from "@sveltejs/kit";
+import { error, type NumericRange } from "@sveltejs/kit";
 
-export const load: PageServerLoad = async ({ locals, params, fetch, url }) => {
+const VALID_ORDER = [
+  "title",
+  "dateCreated",
+  "shortCode",
+  "longUrl",
+  "visits",
+  "nonBotVisits",
+];
+
+const VALID_DIR = ["ASC", "DESC"];
+
+export const load: PageServerLoad = async ({ url }) => {
   const apiClient = new ShlinkApiClient(new NodeHttpClient(), {
-    baseUrl: SHLINK_ENDPOINT,
-    apiKey: SHLINK_API_KEY,
+    baseUrl: env.SHLINK_ENDPOINT,
+    apiKey: env.SHLINK_API_KEY,
   });
 
   const page = url.searchParams.get("page") ?? "1";
@@ -19,10 +27,36 @@ export const load: PageServerLoad = async ({ locals, params, fetch, url }) => {
     error(422, `Invalid page "${page}"`);
   }
 
-  const domains = await apiClient.listShortUrls({
-    itemsPerPage: 20,
-    page: page,
-  });
+  const orderByField = url.searchParams.get("orderBy") ?? "dateCreated";
+  if (!VALID_ORDER.some((v) => v === orderByField)) {
+    error(422, "orderBy value not valid");
+  }
+
+  const dir = url.searchParams.get("dir") ?? "DESC";
+  if (!VALID_DIR.some((v) => v === dir)) {
+    error(422, "dir value not valid");
+  }
+
+  let domains;
+  try {
+    domains = await apiClient.listShortUrls({
+      itemsPerPage: 20,
+      page: page,
+      orderBy: {
+        field: orderByField as
+          | "dateCreated"
+          | "shortCode"
+          | "longUrl"
+          | "title"
+          | "visits"
+          | "nonBotVisits",
+        dir: dir as "ASC" | "DESC",
+      },
+    });
+  } catch (_e) {
+    const e = _e as ProblemDetailsError;
+    error(e.status as NumericRange<400, 599>, e.title);
+  }
   const tags = await apiClient.listTags();
 
   return {
--- src/routes/(app)/admin/links/+page.svelte
+++ src/routes/(app)/admin/links/+page.svelte
@@ -1,12 +1,40 @@
 <script lang="ts">
+  import { goto } from "$app/navigation";
   import PageHeader from "$lib/components/PageHeader.svelte";
   import Pagination from "$lib/components/Pagination.svelte";
   import TagSelector from "$lib/components/TagSelector.svelte";
+  import type { ShlinkShortUrlsOrder } from "@shlinkio/shlink-js-sdk/api-contract";
   import type { PageData } from "./$types";
+  import { page } from "$app/stores";
 
   export let data: PageData;
 
-  let selectedTags;
+  $: query = new URLSearchParams($page.url.searchParams.toString());
+
+  const set_url_params = (order: ShlinkShortUrlsOrder) => {
+    if (
+      query.get("orderBy") === order.field ||
+      (!query.has("orderBy") && order.field === "dateCreated")
+    ) {
+      if (query.has("dir")) {
+        query.delete("dir");
+      } else {
+        query.set("dir", "ASC");
+      }
+    } else {
+      query.delete("dir");
+    }
+    query.set("orderBy", order.field);
+    goto(`?${query.toString()}`);
+  };
+
+  const table_headers: [{ order: ShlinkShortUrlsOrder; title: string }] = [
+    { order: { field: "shortCode" }, title: "Slug" },
+    { order: { field: "longUrl" }, title: "Link" },
+    { order: { field: null }, title: "Tags" },
+    { order: { field: "dateCreated" }, title: "Created date" },
+    { order: { field: "visits" }, title: "Visits" },
+  ];
 </script>
 
 <PageHeader title="Link shortener" />
@@ -24,28 +52,63 @@
 
 <div class="overflow-x-auto">
   <h1 class="my-4 text-2xl font-bold">Links</h1>
-  <table class="table">
-    <!-- head -->
+  <table class="table mb-4">
     <thead>
-      <tr class="bg-base-200">
-        <th>Slug</th>
-        <th>Link</th>
-        <th>Tags</th>
+      <tr class="cursor-pointer bg-base-200">
+        {#each table_headers as th (th.title)}
+          {#if th.order.field}
+            <th on:click={() => set_url_params({ field: th.order.field })}>
+              {th.title}
+              {#if query.get("orderBy") === th.order.field || (!query.has("orderBy") && th.order.field === "dateCreated")}
+                {#if query.get("dir") === "ASC"}
+                  <span class="i-mdi-triangle-down"></span>
+                {:else}
+                  <span class="i-mdi-triangle"></span>
+                {/if}
+              {/if}
+            </th>
+          {:else}
+            <th class="cursor-text">
+              {th.title}
+            </th>
+          {/if}
+        {/each}
         <th />
       </tr>
     </thead>
     <tbody>
-      {#each data.domains as { shortCode, longUrl, tags } (shortCode)}
+      {#each data.domains as d (d.shortCode)}
+        {@const date = new Date(d.dateCreated)}
         <tr>
-          <td class="font-medium">{shortCode}</td>
-          <td>
-            {longUrl}
+          <td class="font-medium">
+            <a href={d.shortUrl}>
+              {d.shortCode}
+            </a>
           </td>
           <td>
-            {tags}
+            <a href={d.longUrl}>
+              {d.longUrl}
+            </a>
+          </td>
+          <td>
+            {d.tags.join(", ")}
+          </td>
+          <td>
+            {date.getFullYear()}-{String(date.getMonth()).padStart(
+              2,
+              "0",
+            )}-{String(date.getDate()).padStart(2, "0")}
+          </td>
+          <td>
+            <div
+              class="tooltip"
+              data-tip={`nonBots: ${d.visitsSummary?.nonBots}, bots: ${d.visitsSummary?.bots}`}
+            >
+              {d.visitsSummary?.total}
+            </div>
           </td>
           <td class="text-right">
-            <a class="btn btn-xs px-8" href={`links/${shortCode}`}>Ändra</a>
+            <a class="btn btn-xs px-8" href={`links/${d.shortCode}`}>Edit</a>
           </td>
         </tr>
       {/each}
