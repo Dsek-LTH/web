4235cefa3f1c3e1f587a52a59e5c2cd35cbcdded
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-05-08 11:59:44 +0200

    Add /nolla/sektionen page

---
+++ src/routes/nolla/sektionen/+page.svelte
@@ -0,0 +1,14 @@
+<header>
+  <h1 class="mx-auto text-center font-nolla-mono text-8xl font-bold">
+    D-sektionen
+  </h1>
+
+  <p class="max-w-prose">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod
+    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
+    non
+  </p>
+</header>
