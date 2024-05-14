425eed32715eedb48f38890931b04c28abeb3578
Tair <tairstenen@gmail.com>
2024-05-14 16:21:00 +0200

    Refactoring, not using superforms but works

--- src/routes/(app)/settings/SubscriptionTags.svelte
+++ src/routes/(app)/settings/SubscriptionTags.svelte
@@ -1,34 +1,15 @@
 <script lang="ts">
-  import type { SettingsSchema, TagSchema } from "$lib/zod/schemas";
   import { type Tag } from "@prisma/client";
-  import { superForm } from "sveltekit-superforms/client";
-  import type { SuperValidated } from "sveltekit-superforms";
 
   export let subscribedTags: Tag[];
   export let tags: Tag[];
 
-  // const removeTag = (tagToRemove: Tag) => {
-  //   nonSubscribedTags.push(tagToRemove);
-  //   nonSubscribedTags = nonSubscribedTags; // Required to rerender
-  //   subscribedTags.subscribedTags = subscribedTags.subscribedTags.filter(
-  //     (tag) => tag.id != tagToRemove.id,
-  //   );
-  // };
-
-  // const addTag = (tagToAdd: Tag) => {
-  //   subscribedTags.subscribedTags.push(tagToAdd);
-  //   subscribedTags.subscribedTags = subscribedTags.subscribedTags; // Required to rerender
-  //   nonSubscribedTags = nonSubscribedTags.filter(
-  //     (tag) => tag.id != tagToAdd.id,
-  //   );
-  // };
-
-  export let data: SuperValidated<SettingsSchema>;
-
-  const { form } = superForm(data);
-  let selected = tags.map(
-    (tag) => subscribedTags.find((subTag) => subTag.id == tag.id) != undefined,
-  );
+  let selectedTags: string[] = tags
+    .map((tag) => {
+      if (subscribedTags.find((subTag) => subTag.id == tag.id)) return tag.id;
+      else return "";
+    })
+    .filter((t) => t != "");
 </script>
 
 <div class="flex flex-col items-center xl:grid xl:grid-cols-2">
@@ -41,9 +22,9 @@
         <input
           type="checkbox"
           class="peer sr-only"
-          bind:group={}
-          bind:checked={selected[index]}
-          value={tag}
+          name="tag"
+          bind:group={selectedTags}
+          value={tag.id}
         />
         <span
           class="ms-3 pb-1 pt-1 text-sm font-medium text-gray-900 dark:text-gray-300"
@@ -56,31 +37,4 @@
       </label>
     </div>
   {/each}
-  <!-- <h3 class="text-xl font-bold">Aktiva</h3>
-  <div>
-    {#each subscribedTags.subscribedTags as tag}
-      <button
-        type="button"
-        class="button-color-darkmode rounded-2xl border border-gray-400"
-        style={"background-color: " +
-          (tag.color != null && tag.color ? tag.color : "white") +
-          "; padding: 2px 10px 2px 10px; margin: 2px; color: black;"}
-        on:click={() => removeTag(tag)}>{tag.name}</button
-      >
-    {/each}
-  </div>
-  <br />
-  <h3 class="text-xl font-bold">Inaktiva</h3>
-  <div>
-    {#each nonSubscribedTags as tag}
-      <button
-        type="button"
-        class="button-color-darkmode rounded-2xl border border-gray-400"
-        style={"background-color: " +
-          (tag.color != null && tag.color ? tag.color : "white") +
-          "; padding: 2px 10px 2px 10px; margin: 2px; color: black;"}
-        on:click={() => addTag(tag)}>{tag.name}</button
-      >
-    {/each}
-  </div> -->
 </div>
