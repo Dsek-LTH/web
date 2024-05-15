da6125301a1c5cc8eeb2937fd4f056b788babb8e
Tair <tairstenen@gmail.com>
2024-05-15 13:27:37 +0200

    Sends notification when accepted or denied a booking request

--- src/routes/(app)/booking/admin/+page.server.ts
+++ src/routes/(app)/booking/admin/+page.server.ts
@@ -1,12 +1,15 @@
+import authorizedPrismaClient from "$lib/server/shop/authorizedPrisma";
 import apiNames from "$lib/utils/apiNames";
 import { authorize } from "$lib/utils/authorization";
+import sendNotification from "$lib/utils/notifications";
+import { NotificationType } from "$lib/utils/notifications/types";
 import type { Actions, PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ locals }) => {
   const { prisma, user } = locals;
   authorize(apiNames.BOOKINGS.UPDATE, user);
 
-  const bookingRequests = await prisma.bookingRequest.findMany({
+  const bookingRequests = await authorizedPrismaClient.bookingRequest.findMany({
     where: {
       start: {
         gte: new Date(),
@@ -18,14 +21,13 @@ export const load: PageServerLoad = async ({ locals }) => {
       booker: true,
     },
   });
-
   return { bookingRequests };
 };
 
 export const actions: Actions = {
   accept: async (event) => {
     const { request, locals } = event;
-    const { prisma } = locals;
+    const { prisma, user } = locals;
     const formData = await request.formData();
     const id = formData.get("id");
     if (id && typeof id === "string") {
@@ -35,11 +37,30 @@ export const actions: Actions = {
           status: "ACCEPTED",
         },
       });
+      const event = await prisma.bookingRequest.findFirst({
+        where: {
+          id,
+        },
+        select: {
+          bookerId: true,
+          event: true,
+        },
+      });
+      if (event && event.bookerId != null && user && user.memberId) {
+        sendNotification({
+          title: "Booking request accepted",
+          message: `Your booking request for ${event.event} has been accepted`,
+          type: NotificationType.BOOKING_REQUEST,
+          link: "https://dsek.se/",
+          memberIds: [event.bookerId],
+          fromMemberId: user.memberId,
+        });
+      }
     }
   },
   reject: async (event) => {
     const { request, locals } = event;
-    const { prisma } = locals;
+    const { prisma, user } = locals;
     const formData = await request.formData();
     const id = formData.get("id");
     if (id && typeof id === "string") {
@@ -49,6 +70,25 @@ export const actions: Actions = {
           status: "DENIED",
         },
       });
+      const event = await prisma.bookingRequest.findFirst({
+        where: {
+          id,
+        },
+        select: {
+          bookerId: true,
+          event: true,
+        },
+      });
+      if (event && event.bookerId != null && user && user.memberId) {
+        sendNotification({
+          title: "Booking request denied",
+          message: `Your booking request for ${event.event} has been denied`,
+          type: NotificationType.BOOKING_REQUEST,
+          link: "https://dsek.se/",
+          memberIds: [event.bookerId],
+          fromMemberId: user.memberId,
+        });
+      }
     }
   },
 };
