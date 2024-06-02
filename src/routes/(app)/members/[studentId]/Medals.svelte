bab766de32140a0498dd7bd43a7b893e3264e964
fnurkla <fnurkla@fnurkla.se>
2024-06-02 14:59:29 +0200

    Add links to medals years in the medals on profile page

--- src/routes/(app)/members/[studentId]/Medals.svelte
+++ src/routes/(app)/members/[studentId]/Medals.svelte
@@ -3,24 +3,31 @@
   export let medals: Array<{ medal: string; after: Semester }>;
 
   let sortedMedals = medals.toSorted((a, b) => a.after - b.after);
+
+  const makeLink = (semester: Semester) => {
+    const sp = new URLSearchParams([["semester", toString(semester)]]);
+    return "/medals?" + sp;
+  };
 </script>
 
 <div class="my-2 text-xl font-bold">Medaljer</div>
 <ul class="flex flex-col gap-2">
   {#each sortedMedals as medal}
-    <li
-      class="flex items-center justify-between gap-4 rounded-lg bg-base-200 p-3"
-    >
-      <div class="flex flex-col">
-        <span class="font-semibold">{medal.medal}</span>
-      </div>
-      <div
-        class="flex flex-1 flex-col items-stretch overflow-hidden text-right"
+    <a class="hover:underline" href={makeLink(medal.after)}>
+      <li
+        class="flex items-center justify-between gap-4 rounded-lg bg-base-200 p-3"
       >
-        <div class="flex flex-col font-bold opacity-50">
-          efter {toString(medal.after)}
+        <div class="flex flex-col">
+          <span class="font-semibold">{medal.medal}</span>
         </div>
-      </div>
-    </li>
+        <div
+          class="flex flex-1 flex-col items-stretch overflow-hidden text-right"
+        >
+          <div class=" flex flex-col font-bold opacity-50">
+            efter {toString(medal.after)}
+          </div>
+        </div>
+      </li>
+    </a>
   {/each}
 </ul>
