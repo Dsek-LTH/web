0e0d847b092993a554cfb32b231190c88fb6262c
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-06-06 23:12:29 +0200

    nolla: begin implementing /todo page

--- src/routes/nolla/todo/+page.svelte
+++ src/routes/nolla/todo/+page.svelte
@@ -1,3 +1,86 @@
-<header class="flex justify-between">
-  <h1 class="font-nolla-mono text-8xl font-bold">Att göra</h1>
-</header>
+<script lang="ts">
+  import * as m from "$paraglide/messages";
+</script>
+
+<div class="my-16 space-y-32">
+  <header class="flex justify-between">
+    <h1 class="font-nolla-mono text-8xl font-bold">{m.nolla_todo_header()}</h1>
+  </header>
+
+  <article class="grid grid-cols-2 overflow-hidden rounded-3xl bg-secondary">
+    <img
+      src="https://www.afbostader.se/globalassets/bostadsomraden/pireus/pf_bsflb.jpg"
+      alt=""
+      class="h-full object-cover"
+    />
+    <div class="p-16">
+      <h1 class="text-xl font-bold">{m.nolla_todo_boende()}</h1>
+      <p>
+        {m.nolla_todo_boendeText()}
+      </p>
+      <a href="/nolla/boende" class="link">{m.nolla_todo_readMore()}</a>
+      <ul class="mt-8 flex gap-4">
+        <li>
+          <a href="https://www.afbostader.se/" class="link" target="_blank">
+            AF Bostäder
+            <span class="i-mdi-external-link" />
+          </a>
+        </li>
+        <li>
+          <a
+            href="https://www.facebook.com/groups/458545794323070"
+            class="link"
+            target="_blank"
+          >
+            Boende i Lund
+            <span class="i-mdi-external-link" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.mhklund.se/" class="link" target="_blank">
+            MHK
+            <span class="i-mdi-external-link" />
+          </a>
+        </li>
+        <li>
+          <a href="https://www.lkf.se/" class="link" target="_blank">
+            LKF
+            <span class="i-mdi-external-link" />
+          </a>
+        </li>
+      </ul>
+    </div>
+  </article>
+
+  <article class="grid grid-cols-2 overflow-hidden rounded-3xl bg-primary">
+    <div class="p-16">
+      <h1 class="text-xl font-bold">{m.nolla_todo_nationer()}</h1>
+      <p>
+        {m.nolla_todo_nationerText()}
+      </p>
+      <a href="/nolla/nationer" class="link">{m.nolla_todo_readMore()}</a>
+    </div>
+
+    <img
+      src="https://static-cdn.sr.se/images/2054/4ee08adc-68ca-45f7-bee9-579b09817ab0.jpg?preset=1024x576"
+      alt=""
+      class="h-full object-cover"
+    />
+  </article>
+
+  <article class="grid grid-cols-2 overflow-hidden rounded-3xl bg-secondary">
+    <img
+      src="https://media.cnn.com/api/v1/images/stellar/prod/underscored-how-to-pack-a-suitcase-lead-packing.jpg?q=h_1800,w_3200,x_0,y_0"
+      alt=""
+      class="h-full object-cover"
+    />
+
+    <div class="p-16">
+      <h1 class="text-xl font-bold">{m.nolla_todo_list()}</h1>
+      <p>
+        {m.nolla_todo_listText()}
+      </p>
+      <a href="/nolla/list" class="link">{m.nolla_todo_readMore()}</a>
+    </div>
+  </article>
+</div>
