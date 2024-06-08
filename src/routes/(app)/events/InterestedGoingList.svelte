32a0355bea56f0550ac5ce4cae0a8370073a93ea
fnurkla <fnurkla@fnurkla.se>
2024-06-08 21:20:36 +0200

    Translate interested/going lists

--- src/routes/(app)/events/InterestedGoingList.svelte
+++ src/routes/(app)/events/InterestedGoingList.svelte
@@ -2,6 +2,7 @@
   import { getFullName } from "$lib/utils/client/member";
   import type { Member } from "@prisma/client";
   import AuthorSignature from "$lib/components/AuthorSignature.svelte";
+  import * as m from "$paraglide/messages";
 
   let goingModal: HTMLDialogElement;
   let interestedModal: HTMLDialogElement;
@@ -9,18 +10,44 @@
   export let going: Member[];
 
   $: goingText =
-    going.length > 2
-      ? `${getFullName(going[0]!)}, ${getFullName(going[1]!)} och ${
-          going.length - 2
-        } andra`
-      : `${going.map((m) => getFullName(m)).join(" och ")}`;
+    going.length > 0
+      ? going.length === 1
+        ? m.events_interestedGoing_isGoing({ x: getFullName(going[0]!) })
+        : m.events_interestedGoing_areGoing({
+            x:
+              going.length > 2
+                ? m.events_interestedGoing_threeOrMore({
+                    name1: getFullName(going[0]!),
+                    name2: getFullName(going[1]!),
+                    others: going.length - 2,
+                  })
+                : m.events_interestedGoing_two({
+                    name1: getFullName(going[0]!),
+                    name2: getFullName(going[1]!),
+                  }),
+          })
+      : "";
 
   $: interestedText =
-    interested.length > 2
-      ? `${getFullName(interested[0]!)}, ${getFullName(interested[1]!)} och ${
-          interested.length - 2
-        } andra`
-      : `${interested.map((m) => getFullName(m)).join(" och ")}`;
+    interested.length > 0
+      ? interested.length === 1
+        ? m.events_interestedGoing_isInterested({
+            x: getFullName(interested[0]!),
+          })
+        : m.events_interestedGoing_areInterested({
+            x:
+              interested.length > 2
+                ? m.events_interestedGoing_threeOrMore({
+                    name1: getFullName(interested[0]!),
+                    name2: getFullName(interested[1]!),
+                    others: interested.length - 2,
+                  })
+                : m.events_interestedGoing_two({
+                    name1: getFullName(interested[0]!),
+                    name2: getFullName(interested[1]!),
+                  }),
+          })
+      : "";
 </script>
 
 {#if going.length > 0}
@@ -28,7 +55,7 @@
     on:click|preventDefault={() => goingModal.showModal()}
     class="link text-sm opacity-40 hover:opacity-60"
   >
-    {goingText} kommer
+    {goingText}
   </button>
   <dialog id="going_modal" class="modal" bind:this={goingModal}>
     <ul class="modal-box m-1 flex flex-col">
@@ -39,7 +66,7 @@
       {/each}
     </ul>
     <form method="dialog" class="modal-backdrop">
-      <button>close</button>
+      <button>{m.events_interestedGoing_close()}</button>
     </form>
   </dialog>
 {/if}
@@ -49,7 +76,7 @@
     on:click|preventDefault={() => interestedModal.showModal()}
     class="link text-sm opacity-40 hover:opacity-60"
   >
-    {interestedText} är intresserade
+    {interestedText}
   </button>
   <dialog id="interested_modal" class="modal" bind:this={interestedModal}>
     <ul class="modal-box m-1 flex flex-col">
@@ -60,7 +87,7 @@
       {/each}
     </ul>
     <form method="dialog" class="modal-backdrop">
-      <button>close</button>
+      <button>{m.events_interestedGoing_close()}</button>
     </form>
   </dialog>
 {/if}
