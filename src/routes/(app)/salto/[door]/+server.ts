678a21049cc82eecb6b5bbf1398bbe4baf8aa893
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-06-09 23:53:03 +0200

    Fix a bug where expired mandates granted door access

--- src/routes/(app)/salto/[door]/+server.ts
+++ src/routes/(app)/salto/[door]/+server.ts
@@ -1,5 +1,64 @@
+import type { DoorAccessPolicy, PrismaClient } from "@prisma/client";
 import type { RequestHandler } from "./$types";
 import { BACKUP_LIST_OF_STUDENT_IDS } from "./constants";
+
+/**
+ * Every door policy applies to either a studentId or a positionId.
+ * This function splits the policies into two respective arrays.
+ */
+function parseDoorPolicies(policies: DoorAccessPolicy[]) {
+  const studentIds = policies
+    .map((policy) => policy.studentId)
+    .filter((id): id is string => id !== null);
+  const positionIds = policies
+    .map((policy) => policy.role)
+    .filter((id): id is string => id !== null);
+  return { studentIds, positionIds };
+}
+
+/**
+ * Given an array of role strings such as "dsek", "dsek.km", etc,
+ * return an array of positions that match the input strings.
+ */
+function fetchMatchingPositions(positions: string[], prisma: PrismaClient) {
+  return prisma.position.findMany({
+    where: {
+      OR: positions.map((p) => ({
+        id: {
+          startsWith: `${p}%`,
+        },
+      })),
+    },
+  });
+}
+
+/**
+ * Given an array of position IDs, return an array of
+ * student IDs that are currently holding the positions.
+ */
+async function fetchStudentsWithPositions(
+  positions: string[],
+  prisma: PrismaClient,
+) {
+  const mandates = await prisma.mandate.findMany({
+    where: {
+      AND: [
+        { positionId: { in: positions } },
+        { startDate: { lte: new Date() } },
+        { endDate: { gte: new Date() } },
+      ],
+    },
+    select: {
+      member: {
+        select: {
+          studentId: true,
+        },
+      },
+    },
+  });
+  const studentIds = mandates.map((mandate) => mandate.member.studentId);
+  return studentIds.filter((id): id is string => id !== null);
+}
 
 export const GET: RequestHandler = async ({ locals, params }) => {
   const { prisma } = locals;
@@ -18,50 +77,19 @@ export const GET: RequestHandler = async ({ locals, params }) => {
     },
   });
 
-  const studentIds = policies
-    .map((policy) => policy.studentId)
-    .filter((id): id is string => id !== null);
-  const positionIds = policies
-    .map((policy) => policy.role)
-    .filter((id): id is string => id !== null);
-
-  // Find
-  const positions = await prisma.position.findMany({
-    where: {
-      OR: positionIds.map((p) => ({
-        id: {
-          startsWith: `${p}%`,
-        },
-      })),
-    },
-  });
-
-  // Take positions such as "dsek" and "dsek.km" and return all students with these roles
-  const studentsFromRoles = (
-    await prisma.mandate.findMany({
-      where: {
-        positionId: {
-          in: positions.map((p) => p.id),
-        },
-      },
-      select: {
-        member: {
-          select: {
-            studentId: true,
-          },
-        },
-      },
-    })
-  )
-    .map((mandate) => mandate.member.studentId)
-    .filter((id): id is string => id !== null);
+  const { studentIds, positionIds } = parseDoorPolicies(policies);
+  const positions = await fetchMatchingPositions(positionIds, prisma);
+  const studentsFromPositions = await fetchStudentsWithPositions(
+    positions.map((p) => p.id),
+    prisma,
+  );
 
   return new Response(
     Array.from([
       ...new Set([
         ...BACKUP_LIST_OF_STUDENT_IDS,
         ...studentIds,
-        ...studentsFromRoles,
+        ...studentsFromPositions,
       ]),
     ]).join("\n"),
   );
