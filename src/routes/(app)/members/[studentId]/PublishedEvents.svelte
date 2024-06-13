0705ea12f64c6a9b5aa1648bf00d8de756ff43d9
fnurkla <fnurkla@fnurkla.se>
2024-06-13 21:08:08 +0200

    Translate profile pages

--- src/routes/(app)/members/[studentId]/PublishedEvents.svelte
+++ src/routes/(app)/members/[studentId]/PublishedEvents.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
   import ActivityButton from "./ActivityButton.svelte";
+  import * as m from "$paraglide/messages";
+  import { languageTag } from "$paraglide/runtime";
 
   import type { Event } from "@prisma/client";
   export let events: Event[];
 </script>
 
-<h2 class="mb-2 text-lg">Publicerade evenemang</h2>
+<h2 class="mb-2 text-lg">{m.members_publishedEvents()}</h2>
 <div class="flex flex-col gap-2">
   {#each events as event (event.id)}
     <ActivityButton
       link="/events/{event.slug}"
       title={event.title}
-      rightAdornment={event.startDatetime?.toLocaleDateString("sv")}
+      rightAdornment={event.startDatetime?.toLocaleDateString(languageTag())}
     />
   {/each}
 </div>
