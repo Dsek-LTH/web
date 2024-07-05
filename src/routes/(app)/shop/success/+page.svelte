9d87c2a65f72d7cdd755c2366d3c4ee92ba7de2a
Ludvig Svedberg <ludvig.svedberg@mac.com>
2024-07-05 15:03:19 +0200

    Refresh automatically if success page's status is action_required

--- src/routes/(app)/shop/success/+page.svelte
+++ src/routes/(app)/shop/success/+page.svelte
@@ -1,7 +1,23 @@
 <script lang="ts">
+  import { invalidate } from "$app/navigation";
   import * as m from "$paraglide/messages";
+  import { onDestroy, onMount } from "svelte";
 
   export let data;
+
+  let interval: ReturnType<typeof setInterval>;
+  $: if (data.refreshPeriodically) {
+    onMount(() => {
+      interval = setInterval(() => {
+        invalidate("cart-success-page");
+      }, 1000);
+    });
+    onDestroy(() => {
+      if (interval) clearInterval(interval);
+    });
+  } else if (interval) {
+    clearInterval(interval);
+  }
 </script>
 
 <svelte:head>
@@ -9,3 +25,6 @@
 </svelte:head>
 
 <h1 class="text-2xl font-bold">{data.message}</h1>
+{#if interval}
+  <span class="loading" />
+{/if}
