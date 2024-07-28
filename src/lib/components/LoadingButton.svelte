e3f1a671fbd48b4fac9a645cd8e3b2a7ec879280
Ludvig Svedberg <ludvig.svedberg@mac.com>
2024-07-28 20:44:30 +0300

    Add FormSubmitButton and add delay to loading button

--- src/lib/components/LoadingButton.svelte
+++ src/lib/components/LoadingButton.svelte
@@ -1,21 +1,37 @@
 <script lang="ts">
-  export let onClick: (() => Promise<unknown> | void) | "default";
+  import { twMerge } from "tailwind-merge";
+
+  export let onClick: (() => Promise<unknown> | void) | "default" = "default";
   export let isLoading = false;
+  export let disabled = false;
+  // ms to wait before showing the loading spinner
+  // The reason behind this can be read up on here: https://www.nngroup.com/articles/response-times-3-important-limits/
+  // TL;DR: Under 100ms delay, the user feels it as instant. Over 1000ms delay, the user feels it as a separate task. 500ms is a sweet spot to show a loading spinner to tell the user something is happening.
+  export let delay = 500;
+  let clazz: string | undefined = undefined;
+  export { clazz as class };
 </script>
 
 <button
-  disabled={isLoading}
   {...$$props}
-  on:click={async () => {
-    if (onClick === "default") return;
-    isLoading = true;
-    await onClick();
-    isLoading = false;
-  }}
+  class={twMerge("relative", clazz)}
+  disabled={disabled || isLoading}
+  on:click={onClick !== "default"
+    ? async () => {
+        const timeout = setTimeout(() => {
+          isLoading = true;
+        }, delay);
+        await onClick();
+        clearTimeout(timeout);
+        isLoading = false;
+      }
+    : undefined}
 >
-  {#if isLoading}
+  <slot />
+  <div
+    class="absolute inset-0 flex items-center justify-center transition-all"
+    class:opacity-0={!isLoading}
+  >
     <span class="loading" />
-  {:else}
-    <slot />
-  {/if}
+  </div>
 </button>
