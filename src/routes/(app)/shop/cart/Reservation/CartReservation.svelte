081a515cf5ef521b4aef2d4ed3418d00c9fd6c11
Ludvig Svedberg <ludvig.svedberg@mac.com>
2024-07-30 08:39:32 +0200

    add missing webshop fixes (#412)

--- src/routes/(app)/shop/cart/Reservation/CartReservation.svelte
+++ src/routes/(app)/shop/cart/Reservation/CartReservation.svelte
@@ -3,8 +3,8 @@
   import Price from "$lib/components/Price.svelte";
   import ScrollingNumber from "$lib/components/Timer/ScrollingNumber.svelte";
   import Timer from "$lib/components/Timer/Timer.svelte";
-  import { now } from "$lib/stores/date";
   import type { CartReservation } from "$lib/utils/shop/types";
+  import { now } from "$lib/stores/date";
   import * as m from "$paraglide/messages";
 
   export let item: CartReservation;
