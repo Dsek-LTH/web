8ed31f37344931b227a8ac6c256921048fccde57
Ludvig Svedberg <ludvig.svedberg@mac.com>
2024-08-01 15:04:26 +0300

    Add debug page

---
+++ src/routes/(app)/admin/debug/+page.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  export let data;
+  $: user = data.user;
+  $: policies = user.policies.toSorted();
+</script>
+
+<section>
+  <h1 class="text-lg font-semibold">User</h1>
+  <span>{user.studentId}</span>
+  <ul class="ml-4 list-disc text-sm">
+    {#each policies as permission}
+      <li>{permission}</li>
+    {/each}
+  </ul>
+</section>
+
+<section class="mt-4">
+  <h1 class="text-lg font-semibold">Meta info</h1>
+  <div>
+    <b>Is nollning: {data.isNollning}</b>
+  </div>
+  <div>
+    <b>Is app: {data.isApp}</b>
+  </div>
+  <div>
+    <b>App info: {JSON.stringify(data.appInfo)}</b>
+  </div>
+</section>
