946d801820755de7ff397ca312c3ec83d36d28be
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-08-07 00:54:44 +0200

    Fix broken committee logos

--- src/lib/components/images/CommitteeIcon.svelte
+++ src/lib/components/images/CommitteeIcon.svelte
@@ -1,51 +1,59 @@
 <script lang="ts">
   import { getImageUrl } from "$lib/files/images";
   import type { Committee } from "@prisma/client";
+  import type { EventHandler } from "svelte/elements";
 
   export let committee: Pick<
     Committee,
     "darkImageUrl" | "lightImageUrl" | "monoImageUrl" | "name"
   >;
   export let useMono = false;
+
+  const FALLBACK = {
+    mono: "https://raw.githubusercontent.com/Dsek-LTH/grafik/main/guild/d_sektionen/full/bw.svg",
+    color:
+      "https://raw.githubusercontent.com/Dsek-LTH/grafik/main/guild/d_sektionen/full/color.svg",
+  };
+
+  /** Fallback to D-sektionen icon if the committee icon is not found */
+  const onError =
+    (imageUrl: string): EventHandler =>
+    (event) => {
+      if (event.target instanceof HTMLImageElement) {
+        event.target.src = imageUrl;
+      }
+    };
 </script>
 
+<!-- 
+  @component
+  This component renders an icon for a committee. It will render a colored 
+  icon by default (with respect to the user's theme), but can be set to
+  render a monochrome icon instead. It fallbacks to the guild logo if
+  the committee icon is not available.
+
+  Note that `darkImageUrl` corresponds to the icon used in dark theme, 
+  and `lightImageUrl` corresponds to the icon used in light theme.
+-->
+
 {#if useMono}
-  <object
-    data="https://raw.githubusercontent.com/Dsek-LTH/grafik/main/guild/d_sektionen/full/bw.svg"
-    type="“image/svg"
-    title="{committee.name} icon"
-    class="aspect-square"
-  >
-    <img
-      src={getImageUrl(committee.monoImageUrl)}
-      alt="{committee.name} icon"
-      class="h-full w-full object-contain"
-    />
-  </object>
+  <img
+    src={getImageUrl(committee.monoImageUrl) ?? FALLBACK.mono}
+    alt="{committee.name} icon"
+    on:error={onError(FALLBACK.mono)}
+  />
 {:else}
   <!-- dark/light support -->
-  <object
-    data="https://raw.githubusercontent.com/Dsek-LTH/grafik/main/guild/d_sektionen/full/color.svg"
-    type="“image/svg"
-    title="{committee.name} icon"
-    class="hidden aspect-square dark:block"
-  >
-    <img
-      src={getImageUrl(committee.darkImageUrl)}
-      alt="{committee.name} oicon"
-      class="h-full w-full object-contain"
-    />
-  </object>
-  <object
-    data="https://raw.githubusercontent.com/Dsek-LTH/grafik/main/guild/d_sektionen/full/color.svg"
-    type="“image/svg"
-    title="{committee.name} icon"
-    class="aspect-square dark:hidden"
-  >
-    <img
-      src={getImageUrl(committee.lightImageUrl)}
-      alt="{committee.name} icon"
-      class="h-full w-full object-contain"
-    />
-  </object>
+  <img
+    src={getImageUrl(committee.darkImageUrl) ?? FALLBACK.color}
+    alt="{committee.name} icon"
+    class="hidden dark:block"
+    on:error={onError(FALLBACK.color)}
+  />
+  <img
+    src={getImageUrl(committee.lightImageUrl) ?? FALLBACK.color}
+    alt="{committee.name} icon"
+    class="block dark:hidden"
+    on:error={onError(FALLBACK.color)}
+  />
 {/if}
