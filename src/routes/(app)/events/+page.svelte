25313456820ca964c5153cdd3728528b2b80e352
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-08-07 23:54:47 +0200

    Add buttons for subscribing to calendar

--- src/routes/(app)/events/+page.svelte
+++ src/routes/(app)/events/+page.svelte
@@ -11,30 +11,54 @@
 
   import type { PageData } from "./$types";
   import SetPageTitle from "$lib/components/nav/SetPageTitle.svelte";
+  import { toast } from "$lib/stores/toast";
   export let data: PageData;
   let filteredTags: Tag[] = data.allTags.filter((tag) =>
     $page.url.searchParams.getAll("tags").includes(tag.name),
   );
   let filterButton: HTMLButtonElement;
   $: isPast = $page.url.searchParams.get("past") == "on";
+  $: eventsSubscribeUrl = `${$page.url.origin}${$page.url.pathname}/subscribe`;
 </script>
 
 <SetPageTitle title={m.events()} />
 
 <section class="flex flex-col gap-2">
   <div class="flex items-center gap-2">
-    <a class="btn" href="/events/calendar">
-      <span class="i-mdi-calendar" />{m.events_calendar()}
-    </a>
     {#if isAuthorized(apiNames.EVENT.CREATE, data.user)}
       <a class="btn" href="/events/create">
         <span class="i-mdi-create" />{m.events_create()}
       </a>
     {/if}
+    <a class="btn" href="/events/calendar">
+      <span class="i-mdi-calendar" />{m.events_calendar()}
+    </a>
+
+    <details
+      class="dropdown"
+      on:toggle={(event) => {
+        if (event.target instanceof HTMLDetailsElement && event.target.open) {
+          navigator.clipboard.writeText(eventsSubscribeUrl);
+          toast(m.events_calendar_subscribe_copyToClipboard(), "success");
+        }
+      }}
+    >
+      <summary class="btn">
+        <span class="i-mdi-calendar-sync" />{m.events_calendar_subscribe()}
+      </summary>
+      <div class="dropdown-content z-[1] rounded-box bg-base-300 p-4 shadow">
+        <p>
+          {m.events_calendar_subscribe_details()}
+        </p>
+        <p class="my-2 rounded border p-2 font-mono text-sm">
+          {eventsSubscribeUrl}
+        </p>
+      </div>
+    </details>
     {#if isAuthorized(apiNames.TAGS.CREATE, data.user) || isAuthorized(apiNames.TAGS.UPDATE, data.user)}
-      <a class="btn" href="/news/tags"
-        ><span class="i-mdi-tag" />{m.events_tags()}</a
-      >
+      <a class="btn" href="/news/tags">
+        <span class="i-mdi-tag" />{m.events_tags()}
+      </a>
     {/if}
   </div>
   <form
