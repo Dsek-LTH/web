cc52d0ea9231c64d7818638ee1437ec61046a96f
Ludvig Svedberg <ludvig.svedberg@mac.com>
2024-08-19 09:46:59 +0200

    Hide nollning events and removed events from subscribed calendar

--- src/routes/(app)/events/subscribe/+server.ts
+++ src/routes/(app)/events/subscribe/+server.ts
@@ -1,3 +1,4 @@
+import { BASIC_EVENT_FILTER } from "$lib/events/events.js";
 import { generateICS } from "$lib/server/ics/calendar.js";
 import dayjs from "dayjs";
 
@@ -6,6 +7,7 @@ export const GET = async ({ locals, setHeaders }) => {
 
   const events = await prisma.event.findMany({
     where: {
+      ...BASIC_EVENT_FILTER(false),
       startDatetime: {
         gte: dayjs().subtract(1, "month").toDate(),
       },
