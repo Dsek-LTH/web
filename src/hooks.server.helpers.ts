543fc6e1e1224fbf627d8f167c678bf3f0ab9761
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-08-23 17:52:55 +0200

    Only cache logged out access policies

--- src/hooks.server.helpers.ts
+++ src/hooks.server.helpers.ts
@@ -4,6 +4,32 @@ import apiNames from "$lib/utils/apiNames";
 import { getDerivedRoles } from "$lib/utils/authorization";
 import type { PrismaClient } from "@prisma/client";
 
+const fetchAccessPolicies = async (
+  prisma: PrismaClient,
+  roles: string[],
+  studentId?: string,
+) => {
+  const isNollning = await isNollningPeriod();
+  return prisma.accessPolicy
+    .findMany({
+      where: {
+        OR: [{ role: { in: roles } }, { studentId }],
+      },
+    })
+    .then((policies) => policies.map((p) => p.apiName))
+    .then((policies) => {
+      /**
+       * Stab hide functionality: when outside of the nollning everyone has the
+       * access policy "SEE_STABEN", but during nollning only those who get it
+       * due to their roles can see staben.
+       */
+      if (!isNollning && !policies.includes(apiNames.MEMBER.SEE_STABEN)) {
+        policies.push(apiNames.MEMBER.SEE_STABEN);
+      }
+      return policies;
+    });
+};
+
 const CACHE_TTL = 10 * 60 * 1000; // 10 minutes
 const accessPoliciesCache: { policies: string[]; lastUpdated: number | null } =
   {
@@ -26,37 +52,25 @@ export const getAccessPolicies = async (
   if (!!studentId && dev) {
     return getAllAccessPolicies(prisma);
   }
-  const isNollning = await isNollningPeriod();
+  const roles = getDerivedRoles(groupList, !!studentId);
 
-  if (
-    accessPoliciesCache.lastUpdated &&
-    Date.now() - accessPoliciesCache.lastUpdated > CACHE_TTL
-  ) {
-    accessPoliciesCache.policies = await prisma.accessPolicy
-      .findMany({
-        where: {
-          OR: [
-            { role: { in: getDerivedRoles(groupList, !!studentId) } },
-            { studentId },
-          ],
-        },
-      })
-      .then((policies) => policies.map((p) => p.apiName))
-      .then((policies) => {
-        /**
-         * Stab hide functionality: when outside of the nollning everyone has the
-         * access policy "SEE_STABEN", but during nollning only those who get it
-         * due to their roles can see staben.
-         */
-        if (!isNollning && !policies.includes(apiNames.MEMBER.SEE_STABEN)) {
-          policies.push(apiNames.MEMBER.SEE_STABEN);
-        }
-        return policies;
-      });
-    accessPoliciesCache.lastUpdated = Date.now();
+  // only has *, i.e logged out user
+  if (roles.length === 1) {
+    if (
+      accessPoliciesCache.lastUpdated &&
+      Date.now() - accessPoliciesCache.lastUpdated > CACHE_TTL
+    ) {
+      accessPoliciesCache.policies = await fetchAccessPolicies(
+        prisma,
+        roles,
+        studentId,
+      );
+      accessPoliciesCache.lastUpdated = Date.now();
+    }
+    return accessPoliciesCache.policies;
   }
 
-  return accessPoliciesCache.policies;
+  return await fetchAccessPolicies(prisma, roles, studentId);
 };
 
 /** Should only be used in development mode. */
