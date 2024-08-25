fa072f76320666db4a11ccda7a83dceab1258085
Axel Hallefält <axelhallefalt@protonmail.com>
2024-08-25 17:39:10 +0200

    progress on reducing content width

--- src/routes/(nollning)/nollning/+page.svelte
+++ src/routes/(nollning)/nollning/+page.svelte
@@ -32,88 +32,97 @@
         </span>
       </figure>
     </div>
-    <section class="flex flex-col">
-      <h3 class="page-title text-secondary">Hejsan N0llan</h3>
-      <p class="nolla-prose">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum
-        venenatis faucibus. Praesent dictum iaculis iaculis. Aliquam nulla
-        magna, venenatis sit amet mauris sed, pharetra consectetur urna. Class
-        aptent taciti sociosqu ad litora torquent per conubia nostra, per
-        inceptos himenaeos. Phasellus non congue leo. Duis malesuada commodo
-        arcu eu consectetur. Morbi aliquam rhoncus tellus ac tempus. Suspendisse
-        blandit augue nec massa semper semper nec vitae erat.
-      </p>
-      <a href="#staben" class="btn-primary-dark btn self-center"
-        >Läs om Staben</a
-      >
-    </section>
-    <section>
-      <h3 class="page-title font-nolla-stab !text-2xl text-secondary">
-        Det var en gång...
-      </h3>
-      <p class="nolla-prose">
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum
-        venenatis faucibus. Praesent dictum iaculis iaculis. Aliquam nulla
-        magna, venenatis sit amet mauris sed, pharetra consectetur urna. Class
-        aptent taciti sociosqu ad litora torquent per conubia nostra, per
-        inceptos himenaeos. Phasellus non congue leo. Duis malesuada commodo
-        arcu eu consectetur. Morbi aliquam rhoncus tellus ac tempus. Suspendisse
-        blandit augue nec massa semper semper nec vitae erat.
-      </p>
-    </section>
+  {/if}
+  <!-- limit width and center -->
+  <div class="mx-auto max-w-screen-md">
+    {#if data.revealTheme}
+      <section class="flex flex-col">
+        <h3 class="page-title text-secondary">Hejsan N0llan</h3>
+        <p class="nolla-prose">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum
+          venenatis faucibus. Praesent dictum iaculis iaculis. Aliquam nulla
+          magna, venenatis sit amet mauris sed, pharetra consectetur urna. Class
+          aptent taciti sociosqu ad litora torquent per conubia nostra, per
+          inceptos himenaeos. Phasellus non congue leo. Duis malesuada commodo
+          arcu eu consectetur. Morbi aliquam rhoncus tellus ac tempus.
+          Suspendisse blandit augue nec massa semper semper nec vitae erat.
+        </p>
+        <a href="#staben" class="btn-primary-dark btn self-center"
+          >Läs om Staben</a
+        >
+      </section>
+      <section>
+        <h3 class="page-title font-nolla-stab !text-2xl text-secondary">
+          Det var en gång...
+        </h3>
+        <p class="nolla-prose">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In interdum
+          venenatis faucibus. Praesent dictum iaculis iaculis. Aliquam nulla
+          magna, venenatis sit amet mauris sed, pharetra consectetur urna. Class
+          aptent taciti sociosqu ad litora torquent per conubia nostra, per
+          inceptos himenaeos. Phasellus non congue leo. Duis malesuada commodo
+          arcu eu consectetur. Morbi aliquam rhoncus tellus ac tempus.
+          Suspendisse blandit augue nec massa semper semper nec vitae erat.
+        </p>
+      </section>
 
-    <section class="!my-32 !mb-48 flex flex-col items-center" id="staben">
+      <section class="!my-32 !mb-48 flex flex-col items-center" id="staben">
+        <div class="carousel w-full scroll-smooth">
+          <PersonCarouselItem
+            stab
+            name={`Øverphøs\nArux Tonitribus`}
+            index={0}
+          />
+          <PersonCarouselItem stab name="ImRe Daemon" index={1} />
+          <PersonCarouselItem stab name="Volto RefacTor" index={2} />
+          <PersonCarouselItem stab name="Lostrego MutatI/O" index={3} />
+          <PersonCarouselItem stab name="Macro Tempestas" index={4} />
+          <PersonCarouselItem stab name="Celesta VentUX" index={5} />
+        </div>
+      </section>
+    {/if}
+
+    <section class="!my-32 !mb-48 flex flex-col items-center" id="peppers">
       <div class="carousel w-full scroll-smooth">
-        <PersonCarouselItem stab name={`Øverphøs\nArux Tonitribus`} index={0} />
-        <PersonCarouselItem stab name="ImRe Daemon" index={1} />
-        <PersonCarouselItem stab name="Volto RefacTor" index={2} />
-        <PersonCarouselItem stab name="Lostrego MutatI/O" index={3} />
-        <PersonCarouselItem stab name="Macro Tempestas" index={4} />
-        <PersonCarouselItem stab name="Celesta VentUX" index={5} />
+        <PersonCarouselItem name="Alexander" index={0} />
+        <PersonCarouselItem name="Thyra" index={1} />
+        <PersonCarouselItem name="Axel" index={2} />
+        <PersonCarouselItem name="Klara" index={3} />
+        <PersonCarouselItem name="Casper" index={4} />
+        <PersonCarouselItem name="Wilma" index={5} />
+        <PersonCarouselItem name="Emil" index={6} />
+        <PersonCarouselItem name="Linn" index={7} />
+        <PersonCarouselItem name="Gustaf" index={8} />
+        <PersonCarouselItem name="Lola" index={9} />
+        <PersonCarouselItem name="Jacobi" index={10} />
       </div>
     </section>
-  {/if}
 
-  <section class="!my-32 !mb-48 flex flex-col items-center" id="peppers">
-    <div class="carousel w-full scroll-smooth">
-      <PersonCarouselItem name="Alexander" index={0} />
-      <PersonCarouselItem name="Thyra" index={1} />
-      <PersonCarouselItem name="Axel" index={2} />
-      <PersonCarouselItem name="Klara" index={3} />
-      <PersonCarouselItem name="Casper" index={4} />
-      <PersonCarouselItem name="Wilma" index={5} />
-      <PersonCarouselItem name="Emil" index={6} />
-      <PersonCarouselItem name="Linn" index={7} />
-      <PersonCarouselItem name="Gustaf" index={8} />
-      <PersonCarouselItem name="Lola" index={9} />
-      <PersonCarouselItem name="Jacobi" index={10} />
-    </div>
-  </section>
-
-  <section class="flex flex-col">
-    <h3 class="page-title mb-4 text-secondary">Nollningspolicy</h3>
-    <p class="nolla-prose">
-      Alla som deltar i Nollningen ska följa denna nollepolicy:
-    </p>
-    <ul class="list-disc leading-relaxed">
-      <li>Att delta i Nollningen är frivilligt.</li>
-      <li>Aktiviteterna/verksamheten får inte störa allmänheten.</li>
-      <li>Deltagare ska respektera eventansvarigas direktiv och varandra.</li>
-      <li>
-        Inga handlingar som kan leda till att någon känner sig utnyttjad eller
-        obekväm ska utföras.
-      </li>
-    </ul>
-    {#if data.revealTheme}
+    <section class="flex flex-col">
+      <h3 class="page-title mb-4 text-secondary">Nollningspolicy</h3>
       <p class="nolla-prose">
-        Läs mer om dina rättigheter under nollningen som ny student
+        Alla som deltar i Nollningen ska följa denna nollepolicy:
       </p>
-      <!-- TODO: Link -->
-      <a href="/" class="btn-primary-dark btn self-center"
-        >Läs Nollekontraktet</a
-      >
-    {/if}
-  </section>
+      <ul class="list-disc leading-relaxed">
+        <li>Att delta i Nollningen är frivilligt.</li>
+        <li>Aktiviteterna/verksamheten får inte störa allmänheten.</li>
+        <li>Deltagare ska respektera eventansvarigas direktiv och varandra.</li>
+        <li>
+          Inga handlingar som kan leda till att någon känner sig utnyttjad eller
+          obekväm ska utföras.
+        </li>
+      </ul>
+      {#if data.revealTheme}
+        <p class="nolla-prose">
+          Läs mer om dina rättigheter under nollningen som ny student
+        </p>
+        <!-- TODO: Link -->
+        <a href="/" class="btn-primary-dark btn self-center"
+          >Läs Nollekontraktet</a
+        >
+      {/if}
+    </section>
 
-  <!-- TODO: Phaddergrupper -->
+    <!-- TODO: Phaddergrupper -->
+  </div>
 </article>
