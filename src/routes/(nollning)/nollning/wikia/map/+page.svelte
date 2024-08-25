579da41b2abb251353ec1b47f2ce5161429e0245
Ludvig Svedberg <ludvig.svedberg@mac.com>
2024-08-26 00:30:17 +0200

    Invert map before revealing theme

--- src/routes/(nollning)/nollning/wikia/map/+page.svelte
+++ src/routes/(nollning)/nollning/wikia/map/+page.svelte
@@ -6,12 +6,19 @@
   import floor3 from "./floor3.webp";
   import floor4 from "./floor4.webp";
   import * as m from "$paraglide/messages";
+  import { page } from "$app/stores";
+  $: revealTheme = $page.data["revealTheme"];
 </script>
 
 <div class="flex flex-col md:flex-row">
-  <img class="w-full max-w-sm" alt="Map of campus" src={map} />
+  <img
+    class="w-full max-w-sm {revealTheme ? '' : 'rounded-btn bg-black/20 p-4'}"
+    alt="Map of campus"
+    src={map}
+  />
   <div
     class="grid grid-cols-1 gap-2 md:grid-cols-2 xl:grid-cols-3 [&_h3]:text-center"
+    class:invert={!revealTheme}
   >
     <div>
       <h3>{m.nollning_wikia_map_cellar()}</h3>
