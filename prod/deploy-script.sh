05d0aa329760ed4d7c373240088b830c58a14222
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-08-28 13:58:23 +0200

    Move deployments to /var/www

--- prod/deploy-script.sh
+++ prod/deploy-script.sh
@@ -1,13 +1,13 @@
 #! /bin/bash
-cd /server/dsek-web 
+cd /var/www
 
 # Remove everything but the node_modules folder, .env is also excluded by default
 shopt -s extglob
 rm -r !(node_modules|build.zip) 
 
-unzip build.zip -d .
+unzip build.zip -d . && rm build.zip
 pnpm i --ignore-scripts --prod=false
 pnpm generate
 pnpm prisma migrate deploy
 
-/usr/local/lib/node_modules/pnpm/pm2 reload prod/ecosystem.config.cjs
+pm2 reload prod/ecosystem.config.cjs
