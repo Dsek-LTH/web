b286838251ac8e69bf3915def93303f169ad23da
Simon Mechler <mechler.simon@gmail.com>
2024-10-17 20:38:13 +0200

    Refactored code for handling accept/reject actions into one function

--- src/routes/(app)/booking/admin/sharedActions.ts
+++ src/routes/(app)/booking/admin/sharedActions.ts
@@ -5,79 +5,49 @@ import type { Actions } from "./$types";
 
 export const actions: Actions = {
   accept: async (event: RequestEvent) => {
-    const { request, locals } = event;
-    const { prisma, user } = locals;
-    const formData = await request.formData();
-    const id = formData.get("id");
-
-    if (id && typeof id === "string") {
-      await prisma.bookingRequest.update({
-        where: {
-          id,
-        },
-        data: {
-          status: "ACCEPTED",
-        },
-      });
-
-      const request = await prisma.bookingRequest.findFirst({
-        where: {
-          id,
-        },
-        select: {
-          bookerId: true,
-          event: true,
-        },
-      });
-
-      if (request && request.bookerId != null && user && user.memberId) {
-        sendNotification({
-          title: "Booking request accepted",
-          message: `Your booking request for ${request.event} has been accepted`,
-          type: NotificationType.BOOKING_REQUEST,
-          link: `/booking`,
-          memberIds: [request.bookerId],
-          fromMemberId: user.memberId,
-        });
-      }
-    }
+    await performAction(event, true)
   },
   reject: async (event: RequestEvent) => {
-    const { request, locals } = event;
-    const { prisma, user } = locals;
-    const formData = await request.formData();
-    const id = formData.get("id");
-
-    if (id && typeof id === "string") {
-      await prisma.bookingRequest.update({
-        where: {
-          id,
-        },
-        data: {
-          status: "DENIED",
-        },
-      });
-
-      const request = await prisma.bookingRequest.findFirst({
-        where: {
-          id,
-        },
-        select: {
-          bookerId: true,
-          event: true,
-        },
-      });
-
-      if (request && request.bookerId != null && user && user.memberId) {
-        sendNotification({
-          title: "Booking request denied",
-          message: `Your booking request for ${request.event} has been denied`,
-          type: NotificationType.BOOKING_REQUEST,
-          link: `/booking`,
-          memberIds: [request.bookerId],
-          fromMemberId: user.memberId,
-        });
-      }
-    }
+    await performAction(event, false)
   },
 };
+
+async function performAction(event: RequestEvent, accepted: boolean) {
+  const { request, locals } = event;
+  const { prisma, user } = locals;
+  const formData = await request.formData();
+  const id = formData.get("id");
+  const status = accepted ? "ACCEPTED" : "DENIED"
+
+  if (id && typeof id === "string") {
+    await prisma.bookingRequest.update({
+      where: {
+        id,
+      },
+      data: {
+        status,
+      },
+    });
+
+    const request = await prisma.bookingRequest.findFirst({
+      where: {
+        id,
+      },
+      select: {
+        bookerId: true,
+        event: true,
+      },
+    });
+
+    if (request && request.bookerId != null && user && user.memberId) {
+      sendNotification({
+        title: `Booking request ${status.toLowerCase()}`,
+        message: `Your booking request for ${request.event} has been ${status.toLowerCase()}`,
+        type: NotificationType.BOOKING_REQUEST,
+        link: `/booking`,
+        memberIds: [request.bookerId],
+        fromMemberId: user.memberId,
+      });
+    }
+  }
+}
