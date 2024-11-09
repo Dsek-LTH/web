d817ae446b3ab3e4da000cb6ae7bffc92a197be8
Rufus Kogg Röjder <rufus@rojder.se>
2024-11-09 21:13:55 +0100

    fix: switching languages when writing a post clears all content (#556)
    * Fix 552: check if language changes before invalidating
    
    * Fix 551: by subscribing to onSetLanguageTag

--- src/routes/LanguageSwitcher.svelte
+++ src/routes/LanguageSwitcher.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { languageTag } from "$paraglide/runtime";
+  import { languageTag, onSetLanguageTag } from "$paraglide/runtime";
   import { page } from "$app/stores";
   import { i18n } from "$lib/utils/i18n";
   import { invalidateAll } from "$app/navigation";
@@ -7,13 +7,15 @@
 
   let clazz = "";
   export { clazz as class };
+  onSetLanguageTag(() => {
+    invalidateAll();
+  });
 </script>
 
 <a
   class={twMerge("btn btn-ghost", clazz)}
   href={i18n.route($page.url.pathname)}
   hreflang={languageTag() === "sv" ? "en" : "sv"}
-  on:click={() => invalidateAll()}
 >
   <slot>
     {languageTag() === "sv" ? "EN" : "SV"}
