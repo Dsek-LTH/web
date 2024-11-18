0b93d7a6c41f7be8439bd10228d28676030a28ff
Rufus Kogg Röjder <rufus@rojder.se>
2024-11-18 19:46:33 +0100

    Fix 538

--- src/routes/(app)/booking/BookingEditor.svelte
+++ src/routes/(app)/booking/BookingEditor.svelte
@@ -22,6 +22,9 @@
   let start = $form.start;
   let end = $form.end;
 
+  const councilRoomId = "99854837-fdb9-4dba-85fc-86a5c514253c"
+  $: showCouncilRooomWarning = $form.bookables.includes(councilRoomId);
+
   // Ensure that the start date is always before the end date
   function handleStartChange() {
     if (start && end) {
@@ -102,6 +105,25 @@
       </label>
     {/each}
   </fieldset>
+
+  {#if showCouncilRooomWarning}
+    <div role="alert" class="alert alert-warning">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-6 w-6 shrink-0 stroke-current"
+        fill="none"
+        viewBox="0 0 24 24"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+        />
+      </svg>
+      <span>{m.booking_councilRoomWarning()}</span>
+    </div>
+  {/if}
 
   <label>
     <span class="label-text ml-2 font-bold">{m.booking_from()}</span>
