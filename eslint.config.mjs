0e566090b70ae0f3c3d57927fd0abf0852bd0ffa
Esbjörn Stenberg <me@stagrim.com>
2024-11-26 22:02:35 +0100

    Prettier fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,28 +10,30 @@ import { FlatCompat } from "@eslint/eslintrc";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
 });
 
-export default [{
+export default [
+  {
     ignores: [
-        "**/.DS_Store",
-        "**/node_modules",
-        "build",
-        ".svelte-kit",
-        "package",
-        "**/.env",
-        "**/.env.*",
-        "!**/.env.example",
-        ".vscode",
-        "src/translations/paraglide",
-        "**/pnpm-lock.yaml",
-        "**/package-lock.json",
-        "**/yarn.lock",
+      "**/.DS_Store",
+      "**/node_modules",
+      "build",
+      ".svelte-kit",
+      "package",
+      "**/.env",
+      "**/.env.*",
+      "!**/.env.example",
+      ".vscode",
+      "src/translations/paraglide",
+      "**/pnpm-lock.yaml",
+      "**/package-lock.json",
+      "**/yarn.lock",
     ],
-}, ...compat.extends(
+  },
+  ...compat.extends(
     "eslint:recommended",
     "plugin:eslint-comments/recommended",
     "plugin:@typescript-eslint/recommended",
@@ -39,64 +41,84 @@ export default [{
     "plugin:svelte/recommended",
     "plugin:svelte/prettier",
     "prettier",
-), {
+  ),
+  {
     plugins: {
-        "@typescript-eslint": typescriptEslint,
+      "@typescript-eslint": typescriptEslint,
     },
 
     linterOptions: {
-        reportUnusedDisableDirectives: true,
+      reportUnusedDisableDirectives: true,
     },
 
     languageOptions: {
-        globals: {
-            ...globals.browser,
-            ...globals.node,
-        },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
 
-        parser: tsParser,
-        ecmaVersion: 2020,
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 2020,
+      sourceType: "module",
 
-        parserOptions: {
-            extraFileExtensions: [".svelte"],
-        },
+      parserOptions: {
+        extraFileExtensions: [".svelte"],
+      },
     },
 
     rules: {
-        "eslint-comments/require-description": "warn",
+      "eslint-comments/require-description": "warn",
 
-        "no-restricted-imports": ["warn", {
-            patterns: [{
-                group: ["../**/*[a-zA-Z0-9_-]*/*"],
-                message: "\nIt looks like you're importing from a different subtree. Consider whether the imported code should really be shared. Suggestions:\n1) Write new code specific to your usage.\n2) Move the imported code to a shared location, e.g. a parent folder.\n3) Verify that you're using the correct path alias, e.g. $lib.",
-            }, {
-                importNamePattern: "^(goto|redirect)",
-                group: ["$app/navigation", "sveltekit-flash-message/server", "@sveltejs/kit"],
-                message: "Use the goto and redirect wrappers from $lib/utils/redirect instead",
-            }, {
-                importNamePattern: "^(superForm)",
-                group: ["sveltekit-superforms", "sveltekit-superforms/client"],
-                message: "Use the superForm from $lib/utils/client/superForms instead",
-            }],
-        }],
+      "no-restricted-imports": [
+        "warn",
+        {
+          patterns: [
+            {
+              group: ["../**/*[a-zA-Z0-9_-]*/*"],
+              message:
+                "\nIt looks like you're importing from a different subtree. Consider whether the imported code should really be shared. Suggestions:\n1) Write new code specific to your usage.\n2) Move the imported code to a shared location, e.g. a parent folder.\n3) Verify that you're using the correct path alias, e.g. $lib.",
+            },
+            {
+              importNamePattern: "^(goto|redirect)",
+              group: [
+                "$app/navigation",
+                "sveltekit-flash-message/server",
+                "@sveltejs/kit",
+              ],
+              message:
+                "Use the goto and redirect wrappers from $lib/utils/redirect instead",
+            },
+            {
+              importNamePattern: "^(superForm)",
+              group: ["sveltekit-superforms", "sveltekit-superforms/client"],
+              message:
+                "Use the superForm from $lib/utils/client/superForms instead",
+            },
+          ],
+        },
+      ],
 
-        "@typescript-eslint/consistent-type-definitions": "off",
+      "@typescript-eslint/consistent-type-definitions": "off",
 
-        "@typescript-eslint/array-type": ["error", {
-            default: "array-simple",
-        }],
+      "@typescript-eslint/array-type": [
+        "error",
+        {
+          default: "array-simple",
+        },
+      ],
     },
-}, {
+  },
+  {
     files: ["**/*.svelte"],
 
     languageOptions: {
-        parser: parser,
-        ecmaVersion: 5,
-        sourceType: "script",
+      parser: parser,
+      ecmaVersion: 5,
+      sourceType: "script",
 
-        parserOptions: {
-            parser: "@typescript-eslint/parser",
-        },
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
     },
-}];
+  },
+];
