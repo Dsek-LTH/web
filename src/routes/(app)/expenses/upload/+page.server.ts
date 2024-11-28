6fc6c11e97c6e1b7a4673dccc3b2e09b83e1a070
Ludvig Svedberg <ludvig.svedberg@mac.com>
2024-11-28 14:52:39 +0100

    Fix signer and send notification when creating expense

--- src/routes/(app)/expenses/upload/+page.server.ts
+++ src/routes/(app)/expenses/upload/+page.server.ts
@@ -2,15 +2,18 @@ import { PUBLIC_BUCKETS_FILES } from "$env/static/public";
 import { removeFilesWithoutAccessCheck } from "$lib/files/fileHandler";
 import { uploadFile } from "$lib/files/uploadFiles";
 import authorizedPrismaClient from "$lib/server/shop/authorizedPrisma";
+import sendNotification from "$lib/utils/notifications";
+import { NotificationType } from "$lib/utils/notifications/types";
 import { redirect } from "$lib/utils/redirect";
-import type { Prisma } from "@prisma/client";
+import type { Member, Prisma } from "@prisma/client";
 import type { AuthUser } from "@zenstackhq/runtime";
 import { fail } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
 import { message, superValidate } from "sveltekit-superforms/server";
-import { getCostCenter } from "../config";
+import { COST_CENTERS, getCostCenter } from "../config";
 import createBasicReceipt from "./baseItem";
 import { expenseSchema } from "./types";
+import { getFullName } from "$lib/utils/client/member";
 
 export const load = async () => {
   return {
@@ -71,6 +74,76 @@ const removeReceiptImages = (user: AuthUser, date: Date, id: string) =>
     expensePhotoUrl(date, id),
   ]);
 
+const CACHE_TTL = 1000 * 60 * 60 * 24; // 1 week
+const CACHED_SIGNERS: Record<string, Member["id"] | undefined> = {};
+let CACHE_UPDATED_AT = 0;
+
+const TREASURER = "dsek.skattm.mastare";
+const PRESIDENT = "dsek.skattm.funk";
+
+const updateSignersCache = async () => {
+  const allSigners = new Set([
+    ...COST_CENTERS.map((center) => center.signer),
+    TREASURER,
+    PRESIDENT,
+  ]);
+  const signers = await authorizedPrismaClient.mandate.findMany({
+    where: {
+      positionId: {
+        in: [...allSigners],
+      },
+      startDate: {
+        lte: new Date(),
+        gte: new Date(),
+      },
+    },
+    select: {
+      positionId: true,
+      memberId: true,
+    },
+  });
+  CACHE_UPDATED_AT = Date.now();
+  signers.forEach((signer) => {
+    CACHED_SIGNERS[signer.positionId] = signer.memberId;
+  });
+  return signers;
+};
+
+/**
+ * Assumes cache is updated, gets the member id for a signer positionId
+ */
+const getSigner = (signer: string) => {
+  if (signer in CACHED_SIGNERS) {
+    return CACHED_SIGNERS[signer]!;
+  }
+  return undefined; // might be a vacant position
+};
+
+/**
+ * In our policy we have a logic which desides how to handle edge cases where the signer is vacant, or the signer is also the user creating the expense.
+ * This method resolves said logic (or throws if impossible, which would be really rare).
+ */
+const resolveSignerLogic = (
+  signer: string | undefined,
+  userMemberId: string,
+  costCenterName: string,
+) => {
+  if (signer !== userMemberId && signer !== undefined) return signer;
+
+  // user is signer, or signer is vacant
+  signer = getSigner(TREASURER);
+  if (signer !== userMemberId && signer !== undefined) return signer;
+
+  // user is TREASURER, or treasurer is vacant
+  signer = getSigner(PRESIDENT);
+  if (signer !== userMemberId && signer !== undefined) return signer;
+
+  // user is PRESIDENT AND TREASURER (OR: President is vacant, user is treasurer, OR: treasurer is vacant and user is president)
+  throw new Error(
+    `Signer logic could not be resolved for cost center ${costCenterName}. Treasurer: ${getSigner(TREASURER)}, President: ${getSigner(PRESIDENT)}, User: ${userMemberId}`,
+  );
+};
+
 export const actions = {
   default: async (event) => {
     const { locals, request } = event;
@@ -86,24 +159,9 @@ export const actions = {
         memberId: member?.id,
       },
     });
-    const allSigners = form.data.receipts.flatMap((receipt) =>
-      receipt.rows.map((row) => getCostCenter(row.costCenter).signer),
-    );
-    const signers = await prisma.mandate.findMany({
-      where: {
-        positionId: {
-          in: allSigners,
-        },
-        startDate: {
-          lte: expense.date,
-          gte: expense.date,
-        },
-      },
-      select: {
-        positionId: true,
-        memberId: true,
-      },
-    });
+    if (CACHE_UPDATED_AT < Date.now() - CACHE_TTL) {
+      await updateSignersCache();
+    }
     const itemPromiseResults = await Promise.allSettled(
       form.data.receipts.map(
         async (receipt): Promise<Prisma.ExpenseItemCreateManyInput[]> => {
@@ -116,14 +174,11 @@ export const actions = {
 
           return receipt.rows.map((row) => {
             const costCenter = getCostCenter(row.costCenter);
-            const signer = signers.find(
-              (signer) => signer.positionId === costCenter.signer,
+            const signer = resolveSignerLogic(
+              getSigner(costCenter.signer),
+              member.id,
+              costCenter.name,
             );
-            if (!signer) {
-              throw new Error(
-                `Signer not found for cost center ${costCenter.name}`,
-              );
-            }
             return {
               expenseId: expense.id,
               costCenter: row.costCenter,
@@ -131,7 +186,7 @@ export const actions = {
               receiptUrl: uploadedReceipt,
               comment: row.comment,
               committeeShortName: costCenter.committee,
-              signerMemberId: signer.memberId,
+              signerMemberId: signer,
             };
           });
         },
@@ -159,11 +214,12 @@ export const actions = {
         });
       }
     }
+    const items = itemPromiseResults
+      .filter((promiseResults) => promiseResults.status === "fulfilled")
+      .flatMap(({ value: item }) => item);
     try {
       await prisma.expenseItem.createMany({
-        data: itemPromiseResults
-          .filter((promiseResults) => promiseResults.status === "fulfilled")
-          .flatMap(({ value: item }) => item),
+        data: items,
       });
     } catch (e) {
       await authorizedPrismaClient.expense.delete({
@@ -183,7 +239,21 @@ export const actions = {
       });
     }
 
-    // TODO: Send notification to certifier
+    const signerMemberIds = new Set(items.map((item) => item.signerMemberId));
+    try {
+      await sendNotification({
+        title: "Skriv under utlägg",
+        message: `${getFullName(member, {
+          hideNickname: true,
+        })} har skapat ett utlägg som du behöver signera.`,
+        link: `/expenses`,
+        type: NotificationType.EXPENSES,
+        memberIds: [...signerMemberIds],
+        fromMemberId: member.id, // send notification from the creator of the expense
+      });
+    } catch (e) {
+      console.error(`Could not send notification when creating expense`, e);
+    }
 
     throw redirect(
       `/expenses`,
