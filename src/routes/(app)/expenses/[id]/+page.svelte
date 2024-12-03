fd138552c04c19bab254417f3ba54476dffd21f7
Ludvig Svedberg <ludvig.svedberg@mac.com>
2024-12-03 18:07:48 +0100

    comment out expense specific page, to be built in a future pr

--- src/routes/(app)/expenses/[id]/+page.svelte
+++ src/routes/(app)/expenses/[id]/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-  import apiNames from "$lib/utils/apiNames";
-  import { isAuthorized } from "$lib/utils/authorization";
+  // TODO: Build this
+  // import apiNames from "$lib/utils/apiNames";
+  // import { isAuthorized } from "$lib/utils/authorization";
 
-  export let data;
-  $: expense = data.expense;
-  $: canSign =
-    expense.items.some((item) => item.signerMemberId === data.user.memberId) ||
-    isAuthorized(apiNames.EXPENSES.CERTIFICATION, data.user);
+  // export let data;
+  // $: expense = data.expense;
+  // $: canSign =
+  //   expense.items.some((item) => item.signerMemberId === data.user.memberId) ||
+  //   isAuthorized(apiNames.EXPENSES.CERTIFICATION, data.user);
 </script>
