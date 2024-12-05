6158c0ec42e73e42eec68a3d87c8140eeb4fbaf5
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2024-12-05 22:14:49 +0100

    Add missing translation and remove commented code

--- src/routes/(app)/elections/+page.svelte
+++ src/routes/(app)/elections/+page.svelte
@@ -35,75 +35,13 @@
           {election.expiresAt.toLocaleDateString(languageTag())}
         </p>
         <div class="card-actions self-center">
-          <a href={election.link}
-            ><button class=" btn btn-primary w-full px-9">Ansök</button></a
-          >
+          <a href={election.link}>
+            <button class=" btn btn-primary w-full px-9">
+              {m.elections_apply()}
+            </button>
+          </a>
         </div>
       </div>
     </div>
   {/each}
 </section>
-
-<!--
-<section class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-  {#each data.boardPositions as boardMember (boardMember.position.id)}
-    <div class="card bg-base-200 shadow-xl">
-      {#if boardMember.studentId}
-        <div class="group/photo card-body p-4 text-center">
-          <a href="/members/{boardMember.studentId}" class="group/link">
-            <figure class="transition-transform group-hover/photo:scale-90">
-              <MemberAvatar member={boardMember} class="size-24 rounded-full" />
-            </figure>
-            <h2
-              class="card-title mt-8 block text-pretty text-primary group-hover/link:underline"
-            >
-              {getFullName(boardMember)}
-            </h2>
-          </a>
-          <div class="flex flex-col gap-1 px-2 text-base-content/90">
-            <a
-              class="hover:underline"
-              href="/positions/{boardMember.position.id}"
-            >
-              {boardMember.position.name}
-            </a>
-            <a
-              class="flex items-center justify-center gap-1 text-sm text-base-content/90 hover:underline"
-              href="mailto:{boardMember.position.email}"
-            >
-              <span class="i-mdi-email" />
-              {boardMember.position.email}
-            </a>
-          </div>
-        </div>
-      {:else}
-
-        <div class="group/photo">
-          <figure class="mt-4 transition-transform group-hover/photo:scale-90">
-            MemberAvatar renders well even with null-values
-            <MemberAvatar member={boardMember} class="size-24 rounded-full" />
-          </figure>
-          <div class="card-body px-0 text-center">
-            <h2 class="card-title mx-auto text-primary">V.A. Kant</h2>
-            <div class="flex flex-col gap-1 px-2 text-base-content/90">
-              <a
-                class="hover:underline"
-                href="/positions/{boardMember.position.id}"
-              >
-                {boardMember.position.name}
-              </a>
-              <a
-                class="flex items-center justify-center gap-1 text-sm text-base-content/70 hover:underline"
-                href="mailto:{boardMember.position.email}"
-              >
-                <span class="i-mdi-email" />
-                {boardMember.position.email}
-              </a>
-            </div>
-          </div>
-        </div>
-      {/if}
-    </div>
-  {/each}
-</section>
--->
