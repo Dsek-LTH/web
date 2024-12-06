86a4039b24d7a94e4a62d96cd19d1b18445387c5
fgren <179166750+fgren@users.noreply.github.com>
2024-12-06 20:38:40 +0100

    Sort elections by date

--- src/routes/(app)/elections/+page.server.ts
+++ src/routes/(app)/elections/+page.server.ts
@@ -3,6 +3,11 @@ import type { PageServerLoad } from "./$types";
 export const load: PageServerLoad = async ({ locals }) => {
   const { prisma } = locals;
   const openElections = await prisma.election.findMany({
+    orderBy: [
+      {
+        expiresAt: "asc",
+      },
+    ],
     where: {
       expiresAt: {
         gte: new Date(),
