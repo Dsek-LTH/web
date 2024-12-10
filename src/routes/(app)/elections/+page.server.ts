1a7018534cc301749d4638dc83ff074e23105190
fgren <179166750+fgren@users.noreply.github.com>
2024-12-10 18:45:24 +0100

    Use day after expires at

--- src/routes/(app)/elections/+page.server.ts
+++ src/routes/(app)/elections/+page.server.ts
@@ -2,6 +2,8 @@ import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ locals }) => {
   const { prisma } = locals;
+  let date = new Date();
+  date.setDate(date.getDate() - 1);
   const openElections = await prisma.election.findMany({
     orderBy: [
       {
@@ -10,7 +12,7 @@ export const load: PageServerLoad = async ({ locals }) => {
     ],
     where: {
       expiresAt: {
-        gte: new Date(),
+        gte: date,
       },
     },
     select: {
