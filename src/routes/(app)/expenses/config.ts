0c985549b90b6293a09bb89b6353a1604c51b0aa
Ludvig Svedberg <ludvig.svedberg@mac.com>
2024-12-11 10:51:12 +0100

    Add all cost centers

--- src/routes/(app)/expenses/config.ts
+++ src/routes/(app)/expenses/config.ts
@@ -1,5 +1,233 @@
 export const COST_CENTERS = [
   {
+    name: "AKTU01",
+    description: "Funktionärer",
+    example: "Jobbarmat/Mötesfika, Arbetskläder och skyddsmaterial, Tack",
+    signer: "dsek.aktu.mastare",
+    committee: "aktu",
+  },
+  {
+    name: "AKTU02",
+    description: "D-shopen",
+    example: "Inköp av varor från Sverige, Programvaror",
+    signer: "dsek.aktu.mastare",
+    committee: "aktu",
+  },
+  {
+    name: "AKTU03",
+    description: "UtEDischot",
+    example:
+      "Inköp av varor från Sverige, Arrangemangskostnader, Trycksaker, Övriga externa tjänster, Vakter, Tillståndsavgifter, Tack, Mötesdricka, Vinstdelning",
+    signer: "dsek.aktu.mastare",
+    committee: "aktu",
+  },
+  {
+    name: "AKTU04",
+    description: "Externa evenemang",
+    example: "Inköp av varor från Sverige, Arrangemangskostnader",
+    signer: "dsek.aktu.mastare",
+    committee: "aktu",
+  },
+  {
+    name: "AKTU07",
+    description: "Evenemang",
+    example:
+      "Arrangemangskostnader, Kostnader för frilufts- och idrottsarrangemang, Subventioner",
+    signer: "dsek.aktu.mastare",
+    committee: "aktu",
+  },
+  {
+    name: "AKTU08",
+    description: "Inventarie",
+    example: "Förbrukningsinventarie",
+    signer: "dsek.aktu.mastare",
+    committee: "aktu",
+  },
+  {
+    name: "CAFE01",
+    description: "Funktionärer",
+    example: "Jobbarmat/Mötesfika, Arbetskläder och skyddsmaterial, Tack",
+    signer: "dsek.cafe.mastare",
+    committee: "cafe",
+  },
+  {
+    name: "CAFE02",
+    description: "D-café",
+    example: "Livsmedel till försäljning",
+    signer: "dsek.cafe.mastare",
+    committee: "cafe",
+  },
+  {
+    name: "CAFE04",
+    description: "Överiga evenemang",
+    example: "Arrangemangskostnader",
+    signer: "dsek.cafe.mastare",
+    committee: "cafe",
+  },
+  {
+    name: "FRA01",
+    description: "Allmänt",
+    example:
+      "Mötesfika/Jobbarmat, Arrangemangskostnader, Arbetskläder och skyddsmaterial, Tack",
+    signer: "dsek.fram.ordf",
+    committee: "fram",
+  },
+  {
+    name: "INFU01",
+    description: "Allmänt",
+    example:
+      "Jobbarmat/Mötesfika, Programvaror, Arbetskläder och skyddsmaterial, Tack",
+    signer: "dsek.infu.mastare",
+    committee: "infu",
+  },
+  {
+    name: "INFU02",
+    description: "DWWW (kommer nog flyttas)",
+    example:
+      "Jobbarmat/Mötesfika, Domäner / ProgramvaraArbetskläder och skyddsmaterial, Tack",
+    signer: "dsek.infu.mastare",
+    committee: "infu",
+  },
+  {
+    name: "KAL01",
+    description: "Funktionärer",
+    example:
+      "Jobbarmat/Mötesfika, Arrangemangskostnader, Arbetskläder och skyddsmaterial, Tack",
+    signer: "dsek.km.mastare",
+    committee: "km",
+  },
+
+  {
+    name: "KAL02",
+    description: "rootmästeriet (kommer nog flyttas)",
+    example:
+      "Jobbarmat/Mötesfika, Arrangemangskostnader, Arbetskläder och skyddsmaterial, Tack",
+    signer: "dsek.km.mastare",
+    committee: "km",
+  },
+
+  {
+    name: "KAL04",
+    description: "Inventarie",
+    example: "Inköp av varor från Sverige",
+    signer: "dsek.km.mastare",
+    committee: "km",
+  },
+  {
+    name: "MED01",
+    description: "Allmänt",
+    example:
+      "Inköp av varor från Sverige, Jobbarmat/Mötesfika, Arbetskläder och skyddsmaterial, Tack",
+    signer: "dsek.overmarskalk",
+    committee: "medalj",
+  },
+  {
+    name: "NARU01",
+    description: "Funktionärer",
+    example:
+      "Jobbarmat/Mötesfika, Arbetskläder och skyddsmaterial, Redovisningstjänster, Tack",
+    signer: "dsek.nari.mastare",
+    committee: "naru",
+  },
+  {
+    name: "NARU02",
+    description: "Spons",
+    example:
+      "Arrangemangskostnader, Sponsmaterial, Erhållna rabatter, Lokalhyra",
+    signer: "dsek.nari.mastare",
+    committee: "naru",
+  },
+  {
+    name: "NARU03",
+    description: "Alumnigruppen",
+    example: "Jobbarmat/Mötesfika, Arrangemangskostnader",
+    signer: "dsek.nari.mastare",
+    committee: "naru",
+  },
+  {
+    name: "NOLL01",
+    description: "Funktionärer",
+    example:
+      "Jobbarmat/Mötesfika, Arrangemangskostnader, Arbetskläder och skyddsmaterial, Tack",
+    signer: "dsek.noll.stab.oph",
+    committee: "nollu",
+  },
+  {
+    name: "NOLL02",
+    description: "Material",
+    example:
+      "Uppdragsmaterial, Sponsmaterial, Arbetskläder och skyddsmaterial, Trycksaker",
+    signer: "dsek.noll.stab.oph",
+    committee: "nollu",
+  },
+  {
+    name: "NOLL03",
+    description: "Pepparna",
+    example: "Arbetskläder och skyddsmaterial, Arrangemangskostnader",
+    signer: "dsek.noll.stab.oph",
+    committee: "nollu",
+  },
+  {
+    name: "NOLL04",
+    description: "Evenemang",
+    example: "Arrangemangskostnader, Erhållna rabatter, Biljetter",
+    signer: "dsek.noll.stab.oph",
+    committee: "nollu",
+  },
+  {
+    name: "SEKT01",
+    description: "Funktionärer",
+    example:
+      "Utlottningar och tävlingar, Reklam och PR, Utbildning, Rekrytering, Subventionering",
+    signer: "dsek.skattm.mastare",
+    committee: "other",
+  },
+  {
+    name: "SEKT02",
+    description: "Sektionsmöte",
+    example:
+      "Inköp av varor från Sverige, Bolagsstämma/års- eller föreningsstämma",
+    signer: "dsek.skattm.mastare",
+    committee: "other",
+  },
+  {
+    name: "SEKT03",
+    description: "Tack",
+    example:
+      "Utlottningar och tävlingar, Reklam och PR, Utbildning, Rekrytering, Subventionering",
+    signer: "dsek.skattm.mastare",
+    committee: "other",
+  },
+  {
+    name: "SEKT05",
+    description: "Gemensamma kostnader",
+    example:
+      "Förbrukningsmaterial, Underhåll och tvätt av arbetskläder, Drivmedel för personbilar, Försäkring och skatt för personbilar, Övriga personbilskostnader, Representation, Trycksaker",
+    signer: "dsek.skattm.mastare",
+    committee: "other",
+  },
+  {
+    name: "SEKT06",
+    description: "Administration",
+    example: "Myndighetsavgifter",
+    signer: "dsek.skattm.mastare",
+    committee: "other",
+  },
+  {
+    name: "SEKT07",
+    description: "Jobbardricka",
+    example: "Kaffe, te, mjölk och choklad, Mötesdricka",
+    signer: "dsek.skattm.mastare",
+    committee: "other",
+  },
+  {
+    name: "SEKT08",
+    description: "Fonder",
+    example: "Avsättning t. fonder",
+    signer: "dsek.skattm.mastare",
+    committee: "other",
+  },
+  {
     name: "SEX01",
     description: "Funktionärer",
     example: "Mötesmat, arbetskläder, tack",
@@ -12,6 +240,168 @@ export const COST_CENTERS = [
     example: "Mat, dekorationer, alkoholfria ingredienser",
     signer: "dsek.sex.mastare",
     committee: "sexm",
+  },
+  {
+    name: "SEX03",
+    description: "Pubar",
+    example:
+      "Alkoholkostnader, Livsmedel till försäljning, Arrangemangskostnader, Myndighetsavgifter",
+    signer: "dsek.sex.mastare",
+    committee: "sexm",
+  },
+  {
+    name: "SEX04",
+    description: "Nollegasque",
+    example:
+      "Alkoholkostnader, Livsmedel till försäljning, Arrangemangskostnader, Övriga externa tjänster, Myndighetsavgifter",
+    signer: "dsek.sex.mastare",
+    committee: "sexm",
+  },
+  {
+    name: "SEX05",
+    description: "Skiptesgasque",
+    example:
+      "Alkoholkostnader, Livsmedel till försäljning, Arrangemangskostnader, Myndighetsavgifter",
+    signer: "dsek.sex.mastare",
+    committee: "sexm",
+  },
+  {
+    name: "SEX06",
+    description: "Företagsevenemang",
+    example:
+      "Alkoholkostnader, Livsmedel till försäljning, Arrangemangskostnader, Myndighetsavgifter",
+    signer: "dsek.sex.mastare",
+    committee: "sexm",
+  },
+  {
+    name: "SEX09",
+    description: "UtEDischot",
+    example: "Alkoholkostnader, Livsmedel till försäljning, Vinstdelning",
+    signer: "dsek.sex.mastare",
+    committee: "sexm",
+  },
+  {
+    name: "SKT01",
+    description: "Funktionärer",
+    example: "Jobbarmat/Mötesfika, Arbetskläder och skyddsmaterial, Tack",
+    signer: "dsek.skattm.mastare",
+    committee: "skattm",
+  },
+  {
+    name: "SKT02",
+    description: "Avgifter",
+    example:
+      "Övriga kostnader, Post, IT-tjänster, Bankkostnader, Redovisningstjänster, Dröjsmålsräntor för leverantörsskulder",
+    signer: "dsek.skattm.mastare",
+    committee: "skattm",
+  },
+  {
+    name: "SRD01",
+    description: "Funktionärer",
+    example:
+      "Jobbarmat/Mötesfika, Utlottningar och tävlingar, Arbetskläder och skyddsmaterial, Tack",
+    signer: "dsek.srd.ordf",
+    committee: "srd",
+  },
+  {
+    name: "SRD02",
+    description: "Aktiviteter",
+    example: "Arrangemangskostnader, Utlottningar, priser och tävlingar",
+    signer: "dsek.srd.ordf",
+    committee: "srd",
+  },
+  {
+    name: "STYR01",
+    description: "Funktionärer",
+    example:
+      "Jobbarmat/Mötesfika, Arbetskläder och skyddsmaterial , Representation, Tack",
+    signer: "dsek.skattm.mastare",
+    committee: "other",
+  },
+  {
+    name: "TACK01",
+    description: "Sommar",
+    example:
+      "Jobbarmat/Mötesfika, Arbetskläder och skyddsmaterial, Tack, Förenat tack",
+    signer: "dsek.tackm.mastare",
+    committee: "tackm",
+  },
+  {
+    name: "TACK02",
+    description: "Vinter",
+    example:
+      "Jobbarmat/Mötesfika, Arbetskläder och skyddsmaterial, Tack, Förenat tack",
+    signer: "dsek.tackm.mastare",
+    committee: "tackm",
+  },
+  {
+    name: "TEK01",
+    description: "Funktionärer",
+    example: "Inköp av varor från Sverige, Mötesfika/Jobbarmat",
+    // signer: "dsek.teknik.projgr.ansv", // we need to fix such that only Teknikfokusansvarig at the D-guild can sign
+    signer: "dsek.nari.mastare",
+    committee: "naru",
+  },
+  {
+    name: "TEK02",
+    description: "Evenemang",
+    example:
+      "Inköp av varor från Sverige, Mötesfika/Jobbarmat, Arrangemangskostnader",
+    // signer: "dsek.teknik.projgr.ansv", // we need to fix such that only Teknikfokusansvarig at the D-guild can sign
+    signer: "dsek.nari.mastare",
+    committee: "naru",
+  },
+  {
+    name: "TEK03",
+    description: "Marknadsföring",
+    example:
+      "Saker till funktionärer, Utlottningar och tävlingar, Reklam och PR",
+    // signer: "dsek.teknik.projgr.ansv", // we need to fix such that only Teknikfokusansvarig at the D-guild can sign
+    signer: "dsek.nari.mastare",
+    committee: "naru",
+  },
+  {
+    name: "TEK04",
+    description: "Logistik",
+    example: "Arrangemangskostnader",
+    // signer: "dsek.teknik.projgr.ansv", // we need to fix such that only Teknikfokusansvarig at the D-guild can sign
+    signer: "dsek.nari.mastare",
+    committee: "naru",
+  },
+
+  {
+    name: "TEK05",
+    description: "Försäljning",
+    example: "Erhållna rabatter, Hyra av inventarier och verktyg, Vinstdelning",
+    // signer: "dsek.teknik.projgr.ansv", // we need to fix such that only Teknikfokusansvarig at the D-guild can sign
+    signer: "dsek.nari.mastare",
+    committee: "naru",
+  },
+
+  {
+    name: "TEK06",
+    description: "Höst",
+    example:
+      "Saker till funktionärer, Mötesfika/Jobbarmat, Arrangemangskostnader, Programvaror, Arbetskläder och skyddsmaterial, Reklam och PR, Redovisningstjänster, Tack, Mötesdricka",
+    // signer: "dsek.teknik.projgr.ansv", // we need to fix such that only Teknikfokusansvarig at the D-guild can sign
+    signer: "dsek.nari.mastare",
+    committee: "naru",
+  },
+
+  {
+    name: "TRI01",
+    description: "Allmänt",
+    example:
+      "Mötesfika/Jobbarmat, Arrangemangskostnader, Arbetskläder och skyddsmaterial, Tack",
+    signer: "dsek.triv.mastare",
+    committee: "trivsel",
+  },
+  {
+    name: "VAL01",
+    description: "Allmänt",
+    example: "Mötesfika/Jobbarmat, Arbetskläder och skyddsmaterial, Tack",
+    signer: "dsek.val.ordf",
+    committee: "valb",
   },
 ] as const;
 
