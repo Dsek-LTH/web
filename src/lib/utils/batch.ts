34f2692b748c63bcac40136c6132d092337d72c4
Alfred Grip <alfred.grip@gmail.com>
2025-01-08 16:31:14 +0100

    fix types in promiseAllInBatches

--- src/lib/utils/batch.ts
+++ src/lib/utils/batch.ts
@@ -3,22 +3,22 @@
  * the first {batchSize} promises to finish before starting the next batch.
  *
  * @param items Arguments to pass to the task for each call.
- * @param task The task to run for each item.
+ * @param task The asynchronous task to run for each item.
  * @param batchSize
  */
 export async function promiseAllInBatches<A, B>(
   items: A[],
-  task: (item: A) => B,
+  task: (item: A) => Promise<B>,
   batchSize: number,
 ): Promise<B[]> {
   let position = 0;
   let results: B[] = [];
   while (position < items.length) {
-    const itemsForBatch = items.slice(position, position + batchSize);
-    results = [
-      ...results,
-      ...(await Promise.all(itemsForBatch.map((item: A) => task(item)))),
-    ];
+    const itemsForBatch: A[] = items.slice(position, position + batchSize);
+    const promises: Array<Promise<B>> = itemsForBatch.map((item: A) =>
+      task(item),
+    );
+    results = [...results, ...(await Promise.all(promises))];
     position += batchSize;
   }
   return results;
