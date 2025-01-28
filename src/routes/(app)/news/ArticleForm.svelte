ea8025c60023c4db1142ec80618377954babd9ba
Axel Hallefält <axelhallefalt@protonmail.com>
2025-01-28 21:06:54 +0100

    lint

--- src/routes/(app)/news/ArticleForm.svelte
+++ src/routes/(app)/news/ArticleForm.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import FormFileInput from "$lib/components/forms/FormFileInput.svelte";
   import FormFilesInput from "$lib/components/forms/FormFilesInput.svelte";
   import FormInput from "$lib/components/forms/FormInput.svelte";
   import FormMarkdown from "$lib/components/forms/FormMarkdown.svelte";
