cf9fbe097102cef5981e60846f3035f991e896a4
Felix Ohrgren <179166750+fgren@users.noreply.github.com>
2025-01-29 16:12:08 +0100

    Modified layout of election creation page (#689)
    * Modified layout of election creation page
    
    * translation tweaks

--- src/routes/(app)/elections/ElectionEditor.svelte
+++ src/routes/(app)/elections/ElectionEditor.svelte
@@ -28,39 +28,6 @@
   <h1 class="text-2xl font-bold">
     {isCreating ? m.elections_create() : m.elections_edit()}
   </h1>
-  <Input
-    name="markdown"
-    label={m.elections_content_sv()}
-    textarea
-    bind:value={$form.markdown}
-    error={$errors.markdown}
-    {...$constraints.markdown}
-  />
-  <Input
-    name="markdownEn"
-    label={m.elections_content_en()}
-    textarea
-    bind:value={$form.markdownEn}
-    error={$errors.markdownEn}
-    {...$constraints.markdownEn}
-  />
-  <Input
-    name="link"
-    label={m.elections_link()}
-    bind:value={$form.link}
-    error={$errors.link}
-    {...$constraints.link}
-  />
-  <Input
-    name="expiresAt"
-    label={m.elections_expiryDate()}
-    type="date"
-    required
-    bind:value={$form.expiresAt}
-    error={$errors.expiresAt}
-    {...$constraints.expiresAt}
-  />
-
   <label class="label-text" for="committee">{m.elections_committee()}*</label>
   <select
     id="committee"
@@ -76,6 +43,45 @@
   {#if $errors.committeeId}
     <p class="text-error">{$errors.committeeId}</p>
   {/if}
+  <div class="flex flex-col gap-0 *:w-full md:h-64 md:flex-row md:gap-8">
+    <Input
+      name="markdown"
+      label={m.elections_content_sv()}
+      textarea
+      bind:value={$form.markdown}
+      error={$errors.markdown}
+      class="h-full"
+      placeholder="- Post 1&#13;- Post 2&#13;- Post 3"
+      {...$constraints.markdown}
+    />
+    <Input
+      name="markdownEn"
+      label={m.elections_content_en()}
+      textarea
+      bind:value={$form.markdownEn}
+      error={$errors.markdownEn}
+      class="h-full"
+      placeholder="- Position 1&#13;- Position 2&#13;- Position 3"
+      {...$constraints.markdownEn}
+    />
+  </div>
+  <Input
+    name="link"
+    label={m.elections_link()}
+    bind:value={$form.link}
+    error={$errors.link}
+    placeholder={m.elections_link_placeholder()}
+    {...$constraints.link}
+  />
+  <Input
+    name="expiresAt"
+    label={m.elections_expiryDate()}
+    type="date"
+    required
+    bind:value={$form.expiresAt}
+    error={$errors.expiresAt}
+    {...$constraints.expiresAt}
+  />
   <button type="submit" class="btn btn-primary mx-auto mt-4 w-4/12">
     {m.elections_save()}
   </button>
