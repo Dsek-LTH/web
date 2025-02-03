ff374ea77ecb2e81d7de348f05abc4ea5ee67f7b
Oliver Levay <oliver.levay@hotmail.com>
2025-02-03 22:06:28 +0100

    ADD SENTRY

---
+++ src/hooks.client.ts
@@ -0,0 +1,22 @@
+import { handleErrorWithSentry, replayIntegration } from "@sentry/sveltekit";
+import * as Sentry from "@sentry/sveltekit";
+
+Sentry.init({
+  dsn: "https://763ca90d83f725f70b31f0fd40e7ffda@o4508757277212672.ingest.de.sentry.io/4508757278523472",
+
+  tracesSampleRate: 1.0,
+
+  // This sets the sample rate to be 10%. You may want this to be 100% while
+  // in development and sample at a lower rate in production
+  replaysSessionSampleRate: 0.1,
+
+  // If the entire session is not sampled, use the below sample rate to sample
+  // sessions when an error occurs.
+  replaysOnErrorSampleRate: 1.0,
+
+  // If you don't want to use Session Replay, just remove the line below:
+  integrations: [replayIntegration()],
+});
+
+// If you have a custom error handler, pass it to `handleErrorWithSentry`
+export const handleError = handleErrorWithSentry();
