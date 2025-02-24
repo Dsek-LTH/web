6d7b99648dd0ad57d3079a1171aa7801879fa4e4
Isak Kallini <isak.kallini@live.com>
2025-02-24 22:24:57 +0100

    directus working example

--- src/routes/(nollning)/nollning/+page.server.ts
+++ src/routes/(nollning)/nollning/+page.server.ts
@@ -1,4 +1,12 @@
-export const load = async ({ locals }) => {
+import getDirectusInstance from "$lib/server/directus";
+import { readItem, readItems } from "@directus/sdk";
+import { error } from "@sveltejs/kit";
+
+export const load = async ({ locals, fetch, url }) => {
+  const preview = url.searchParams.get("secret") === "secret";
+  const id = parseInt(url.searchParams.get("id") ?? '1');
+
+  const directus = getDirectusInstance(fetch);
   const { prisma } = locals;
   const phadderGroups = await prisma.phadderGroup.findMany({
     where: {
@@ -16,7 +24,15 @@ export const load = async ({ locals }) => {
       createdAt: "asc",
     },
   });
+  let res;
+  try {
+    res = await directus.request(readItem("Test", preview ? id : 1));
+    console.log(res);
+  } catch (e) {
+    console.log(e);
+  }
   return {
     phadderGroups,
+    thing: res,
   };
 };
