5d855dfdc269c446a22658518eef10b1866f952d
Isak Kallini <isak.kallini@live.com>
2025-05-05 21:31:09 +0200

    remove old docs

--- docs/.vitepress/theme/index.ts
+++
@@ -1,8 +0,0 @@
-// https://vitepress.dev/guide/custom-theme
-import type { Theme } from "vitepress";
-import DefaultTheme from "vitepress/theme";
-import "./style.css";
-
-export default {
-  extends: DefaultTheme,
-} satisfies Theme;
