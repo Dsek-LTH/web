3f5d2b1eb805136127c41d9b60e34a386d1e5447
Zeb Hakansta <hakanstazeb@gmail.com>
2025-05-10 21:09:11 +0200

    pgk-skada

--- src/lib/components/home/WellbeingCTA.svelte
+++ src/lib/components/home/WellbeingCTA.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import * as m from "$paraglide/messages";
 
-  let { wellbeing }: { wellbeing: String } = $props();
+  let { wellbeing }: { wellbeing: string } = $props();
 </script>
 
 <a
