5f9e097c20f259885f6cdf3f23cbb1661fc97043
Isak Kallini <isak.kallini@live.com>
2025-05-12 22:08:36 +0200

    update nollning reveal dates

--- src/lib/components/postReveal/types.ts
+++ src/lib/components/postReveal/types.ts
@@ -1,7 +1,7 @@
 export const NOLLNING_TAG_PREFIX = "[NOLLNING]";
 export const POST_REVEAL_PREFIX = "/nollning";
 
-export const REVEAL_LAUNCH_DATE = new Date("2024-08-28T16:00:00"); // NEEDS TO BE UPDATED EVERY YEAR
+export const REVEAL_LAUNCH_DATE = new Date("2025-08-27T16:00:00"); // NEEDS TO BE UPDATED EVERY YEAR
 export const APP_PREFERRED_PAGE_COOKIE = "APP-preferred-page";
 
 // they will check if a link starts with one of these and then redirect to nollning copy
