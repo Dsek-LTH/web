5b7c73542d2d1bf3e8536f2d285397518c6ddc89
fgren <179166750+fgren@users.noreply.github.com>
2025-06-10 17:12:23 +0200

    add api endpoint for nollning period

---
+++ src/routes/(app)/api/nollning/+server.ts
@@ -0,0 +1,6 @@
+import type { RequestHandler } from "./$types";
+import { isNollningPeriod } from "$lib/utils/adminSettings/nollning";
+
+export const GET: RequestHandler = async () => {
+  return new Response(JSON.stringify(await isNollningPeriod()));
+};
