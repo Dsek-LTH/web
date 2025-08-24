152a033f4ecde58b6487d823681d79281935aa3c
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2025-08-25 00:42:14 +0200

    Defer rendering of members in phadder groups

--- src/lib/components/socials/MembersList.svelte
+++ src/lib/components/socials/MembersList.svelte
@@ -2,27 +2,47 @@
   import AuthorSignature from "$lib/components/socials/AuthorSignature.svelte";
   import type { Member } from "@prisma/client";
 
-  let modal: HTMLDialogElement;
+  let modal: HTMLDialogElement | null;
   export let members: Member[];
 
   let clazz: string | undefined = undefined;
   export { clazz as class };
+
+  let open = false;
+  $: {
+    if (open) modal?.showModal();
+  }
 </script>
 
 {#if members.length > 0}
-  <button on:click|preventDefault={() => modal.showModal()} class={clazz}>
+  <button
+    on:click|preventDefault={() => {
+      open = true;
+    }}
+    class={clazz}
+  >
     <slot />
   </button>
-  <dialog id="members_modal" class="modal" bind:this={modal}>
-    <ul class="modal-box m-1 flex flex-col">
-      {#each members as liker (liker.id)}
-        <li>
-          <AuthorSignature type="member" lazy member={liker} />
-        </li>
-      {/each}
-    </ul>
-    <form method="dialog" class="modal-backdrop">
-      <button>close</button>
-    </form>
+  <dialog
+    id="members_modal"
+    class="modal"
+    bind:this={modal}
+    on:close={() => (open = false)}
+    on:click={(e) => {
+      if (e.target === modal) modal?.close();
+    }}
+  >
+    {#if open}
+      <ul class="modal-box m-1 flex flex-col">
+        {#each members as liker (liker.id)}
+          <li>
+            <AuthorSignature type="member" lazy member={liker} />
+          </li>
+        {/each}
+      </ul>
+      <form method="dialog" class="modal-backdrop">
+        <button>close</button>
+      </form>
+    {/if}
   </dialog>
 {/if}
