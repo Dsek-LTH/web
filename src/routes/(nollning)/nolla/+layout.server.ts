990ae4e7792270eeeb29035f38bb7ed5f4286ce0
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2025-08-25 14:11:09 +0200

    Adjust date for /nollning

--- src/routes/(nollning)/nolla/+layout.server.ts
+++ src/routes/(nollning)/nolla/+layout.server.ts
@@ -1,6 +1,6 @@
 import { redirect } from "$lib/utils/redirect";
 
-const CUTOFF_DATE = Date.parse("2025-08-25T14:00:00"); // this will be in prod: 2025-08-25
+const CUTOFF_DATE = Date.parse("2025-08-24T12:00:00"); // this will be in prod: 2025-08-25
 export const load = () => {
   if (Date.now() > CUTOFF_DATE) {
     redirect(302, "/nollning");
