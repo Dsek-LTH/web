c9d7d32b29577f6d41b2be61938413c7e1a5427e
fgren <179166750+fgren@users.noreply.github.com>
2025-08-28 18:35:56 +0200

    fix types and remove unused dependencies

--- src/routes/(app)/booking/BookingCalendar.svelte
+++ src/routes/(app)/booking/BookingCalendar.svelte
@@ -13,11 +13,9 @@
   let {
     bookingRequests,
     bookables,
-    class: clazz,
   }: {
     bookingRequests: Array<BookingRequest & { bookables: Bookable[] }>;
     bookables: Bookable[];
-    class: string;
   } = $props();
 
   let options: Calendar.Options = $state({
@@ -91,7 +89,7 @@
   });
 </script>
 
-<div class={clazz} style="--slot-width: {slotWidth}px">
+<div style="--slot-width: {slotWidth}px">
   <Calendar plugins={[ResourceTimeline]} {options} />
 </div>
 
