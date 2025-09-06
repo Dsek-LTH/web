5026bc93ae31c1228f154e69b4b266e3ff725960
Isak Kallini <isak.kallini@live.com>
2025-09-06 16:09:50 +0200

    wip

---
+++ src/lib/components/ui/lang-tabs/lang-tabs.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import * as Tabs from "$lib/components/ui/tabs/index.js";
+  let { sv, en } = $props();
+</script>
+
+<Tabs.Root value="sv">
+  <Tabs.List>
+    <Tabs.Trigger value="sv">SV</Tabs.Trigger>
+    <Tabs.Trigger value="en">EN</Tabs.Trigger>
+  </Tabs.List>
+  <Tabs.Content value="sv">{@render sv()}</Tabs.Content>
+  <Tabs.Content value="en">{@render en()}</Tabs.Content>
+</Tabs.Root>
