94171bae02308849292e0918d7e289b9cfc47b6f
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2025-09-13 00:33:53 +0200

    Remove old email alias pages

--- src/routes/(app)/admin/email-alias/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import apiNames from "$lib/utils/apiNames";
-import { authorize } from "$lib/utils/authorization";
-import type { LayoutServerLoad } from "./$types";
-
-export const load: LayoutServerLoad = async (event) => {
-  const { user } = event.locals;
-  authorize(apiNames.EMAIL_ALIAS.READ, user);
-};
