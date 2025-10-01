a2470fbc78978b4e5d5f4cdea48f25c837803408
Esbjörn Stenberg <me@stagrim.com>
2025-10-02 01:06:31 +0200

    Happy 1000th commit!!!! (#882)
    * fix spelling
    
    * very cool 🎉
    
    move readme component
    
    * fix typos in messages
    
    * remove unused comment
    
    * Fix typo in messages
    
    * Fix merge conflict
    
    ---------
    
    Co-authored-by: Isak Kallini <isak.kallini@live.com>
    Co-authored-by: fgren <179166750+fgren@users.noreply.github.com>
    Co-authored-by: Dag Hemberg <dag.hemberg@gmail.com>
    Co-authored-by: David Agardh <agardh.david@gmail.com>

--- src/routes/(app)/readme/[year]/[number]/+page.svelte
+++ src/routes/(app)/readme/[year]/[number]/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import type { PageData } from "./$types";
-  import Readme from "$lib/components/home/Readme.svelte";
+  import Readme from "./Readme.svelte";
   export let data: PageData;
 </script>
 
