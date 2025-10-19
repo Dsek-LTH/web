538bbb66712d90f478f7523348b4b7f87121c99f
EndyMc <82646131+EndyMc@users.noreply.github.com>
2025-10-19 20:07:03 +0200

    Removed discord button from Footer when not logged in (#912)
    * Removed discord button from Footer when not logged in
    
    * Centered icons on the logged out home page on phone
    
    * Update padding to a standard size
    
    Co-authored-by: Isak <65332998+Isak-Kallini@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Isak <65332998+Isak-Kallini@users.noreply.github.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -96,11 +96,6 @@
       icon: "i-mdi-instagram",
     },
     {
-      title: "Discord",
-      href: "https://discord.com/invite/wxHQcvZ38p",
-      icon: "i-mdi-discord",
-    },
-    {
       title: "YouTube",
       href: "https://youtube.com/channel/UCqBtN7xlh4_VvywKaRiGfkw",
       icon: "i-mdi-youtube",
@@ -331,9 +326,9 @@
           {/each}
         </ul>
 
-        <ul class="grid grid-cols-3 grid-rows-2 gap-12 lg:flex">
+        <ul class="flex w-[50vw] flex-wrap justify-center lg:w-auto">
           {#each SOCIALS as social}
-            <li>
+            <li class="p-4">
               <a href={social.href} target="_blank">
                 <span class="{social.icon} size-8"></span>
               </a>
