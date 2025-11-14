54a3e5ce2cdbad4216d6af01c874a522002dcbce
Isak Kallini <isak@kallini.se>
2025-11-14 13:07:12 +0100

    Add placeholder nav-bar

---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import DarkmodeToggle from "./DarkmodeToggle.svelte";
+  import Navbar from "./Navbar.svelte";
+
+  const { children } = $props();
+</script>
+
+<nav class="contents">
+  <Navbar />
+</nav>
+<div class="p-2">
+  <DarkmodeToggle />
+</div>
+{@render children?.()}
