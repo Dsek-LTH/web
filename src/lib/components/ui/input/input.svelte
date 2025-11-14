b2723fc85ee30953143c4033136d376cac8a9b2f
fgren <179166750+fgren@users.noreply.github.com>
2025-11-14 22:40:27 +0100

    fix file upload input

--- src/lib/components/ui/input/input.svelte
+++ src/lib/components/ui/input/input.svelte
@@ -32,9 +32,9 @@
     bind:this={ref}
     data-slot={dataSlot}
     class={cn(
-      "selection:bg-primary dark:bg-input/30 selection:text-primary-foreground border-input ring-offset-background placeholder:text-muted-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 pt-1.5 text-sm font-medium shadow-xs transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50",
+      "selection:bg-primary dark:bg-input/30 selection:text-primary-foreground border-input ring-offset-background placeholder:text-muted-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 pt-1.5 text-right text-sm font-medium shadow-xs transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50",
       "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-      "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+      " aria-invalid:border-rosa-500 dark:aria-invalid:bg-rosa-950 aria-invalid:bg-rosa-50 aria-invalid:text-rosa-500 dark:aria-invalid:text-rosa-500 aria-invalid:border-[1px]",
       className,
     )}
     type="file"
@@ -58,8 +58,8 @@
   />
 {/if}
 {#if restProps["aria-errormessage"]}
-  <p class="text-rosa-500 text-sm font-semibold">
-    <CircleAlert class="inline h-[1.2rem] w-[1.2rem]" />
+  <p class="text-rosa-500 mt-0 text-xs font-semibold">
+    <CircleAlert class="mb-[2px] inline h-[1rem] w-[1rem]" />
     {restProps["aria-errormessage"]}
   </p>
 {/if}
