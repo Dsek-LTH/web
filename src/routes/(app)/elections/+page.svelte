9d25d012d1065579fa20190af453cb7504bd0d3a
Isak <65332998+Isak-Kallini@users.noreply.github.com>
2025-11-14 21:18:42 +0100

    Add buttongroup (#983)
    * Add buttongroup
    
    * fix lint

--- src/routes/(app)/elections/+page.svelte
+++ src/routes/(app)/elections/+page.svelte
@@ -1,13 +1,5 @@
 <script lang="ts">
-  import PageHeader from "$lib/components/nav/PageHeader.svelte";
-  import type { PageData } from "./$types";
-  import * as m from "$paraglide/messages";
-  import MarkdownBody from "$lib/components/MarkdownBody.svelte";
-  import { isAuthorized } from "$lib/utils/authorization";
-  import apiNames from "$lib/utils/apiNames";
-  import CommitteeIcon from "$lib/components/images/CommitteeIcon.svelte";
-  import dayjs from "dayjs";
-  export let data: PageData;
+  import NotImplemented from "$lib/components/NotImplemented.svelte";
 </script>
 
 <NotImplemented />
