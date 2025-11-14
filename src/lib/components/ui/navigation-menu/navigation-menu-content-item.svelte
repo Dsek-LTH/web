36e299d23c802daa091d696629d4051354cd9c85
Isak Kallini <isak@kallini.se>
2025-11-14 13:07:12 +0100

    Add placeholder nav-bar

---
+++ src/lib/components/ui/navigation-menu/navigation-menu-content-item.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  const { title, description }: { title: string; description: string } =
+    $props();
+</script>
+
+<div class="text-secondary-foreground font-medium">{title}</div>
+<div class="text-muted-foreground">{description}</div>
