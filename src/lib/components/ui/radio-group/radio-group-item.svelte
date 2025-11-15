e0d14525cbface564b07bd65a7bcbdb3977b4b54
fgren <179166750+fgren@users.noreply.github.com>
2025-11-15 20:05:15 +0100

    add radiogroup component

---
+++ src/lib/components/ui/radio-group/radio-group-item.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { RadioGroup as RadioGroupPrimitive } from "bits-ui";
+  import CircleIcon from "@lucide/svelte/icons/circle";
+  import { cn, type WithoutChildrenOrChild } from "$lib/utils.js";
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    ...restProps
+  }: WithoutChildrenOrChild<RadioGroupPrimitive.ItemProps> = $props();
+</script>
+
+<RadioGroupPrimitive.Item
+  bind:ref
+  data-slot="radio-group-item"
+  class={cn(
+    "border-input  aria-checked:border-rosa-background text-primary focus-visible:border-rosa-background focus-visible:ring-ring/50 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 aspect-square size-4 shrink-0 rounded-full border shadow-xs transition-[color,box-shadow] outline-none focus-visible:ring-[3px] disabled:cursor-not-allowed disabled:opacity-50",
+    className,
+  )}
+  {...restProps}
+>
+  {#snippet children({ checked })}
+    <div
+      data-slot="radio-group-indicator"
+      class="relative flex items-center justify-center"
+    >
+      {#if checked}
+        <CircleIcon
+          class="fill-rosa-background text-rosa-background absolute top-1/2 left-1/2 size-[10px] -translate-x-1/2 -translate-y-1/2"
+        />
+      {/if}
+    </div>
+  {/snippet}
+</RadioGroupPrimitive.Item>
