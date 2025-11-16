2975685bdd1c975c6ea871c6438332e53ae558aa
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2025-11-16 11:36:41 +0100

    Implement /admin/doors layout

--- src/routes/(app)/admin/doors/+layout.server.ts
+++ src/routes/(app)/admin/doors/+layout.server.ts
@@ -2,12 +2,13 @@ import apiNames from "$lib/utils/apiNames";
 import { authorize } from "$lib/utils/authorization";
 import type { LayoutServerLoad } from "./$types";
 
-export const load: LayoutServerLoad = async ({ locals }) => {
+export const load: LayoutServerLoad = async ({ locals, params }) => {
   const { prisma, user } = locals;
   authorize(apiNames.DOOR.READ, user);
 
   const doors = await prisma.door.findMany();
   return {
     doors,
+    slug: params.slug,
   };
 };
