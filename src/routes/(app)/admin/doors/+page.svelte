22b4e2c1e60cb0a177b38aa1a24ed53246030bad
Daniel Adu-Gyan <26229521+danieladugyan@users.noreply.github.com>
2025-11-16 11:37:55 +0100

    Add /admin/doors empty state page

--- src/routes/(app)/admin/doors/+page.svelte
+++ src/routes/(app)/admin/doors/+page.svelte
@@ -1,5 +1,15 @@
-<script>
-  import NotImplemented from "$lib/components/NotImplemented.svelte";
+<script lang="ts">
+  import { Card, CardContent } from "$lib/components/ui/card";
+  import DoorClosed from "@lucide/svelte/icons/door-closed";
 </script>
 
-<NotImplemented />
+<Card>
+  <CardContent>
+    <div class="flex flex-col items-center gap-8 py-8">
+      <DoorClosed size={96} class="text-muted-foreground" />
+      <p class="text-muted-foreground">
+        Välj en dörr för att hantera åtkomstregler
+      </p>
+    </div>
+  </CardContent>
+</Card>
