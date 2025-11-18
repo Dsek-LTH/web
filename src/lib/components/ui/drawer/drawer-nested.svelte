d66be5f22b384ffc6855a9f8c5a385e9ab3e80e0
fgren <179166750+fgren@users.noreply.github.com>
2025-11-19 00:56:45 +0100

    fix drawer formatting

--- src/lib/components/ui/drawer/drawer-nested.svelte
+++ src/lib/components/ui/drawer/drawer-nested.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
-	import { Drawer as DrawerPrimitive } from "vaul-svelte";
+  import { Drawer as DrawerPrimitive } from "vaul-svelte";
 
-	let {
-		shouldScaleBackground = true,
-		open = $bindable(false),
-		activeSnapPoint = $bindable(null),
-		...restProps
-	}: DrawerPrimitive.RootProps = $props();
+  let {
+    shouldScaleBackground = true,
+    open = $bindable(false),
+    activeSnapPoint = $bindable(null),
+    ...restProps
+  }: DrawerPrimitive.RootProps = $props();
 </script>
 
-<DrawerPrimitive.NestedRoot {shouldScaleBackground} bind:open bind:activeSnapPoint {...restProps} />
+<DrawerPrimitive.NestedRoot
+  {shouldScaleBackground}
+  bind:open
+  bind:activeSnapPoint
+  {...restProps}
+/>
