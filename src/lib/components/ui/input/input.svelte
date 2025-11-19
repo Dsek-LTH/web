26ea651295955be084ced97534773ef2e15f62a3
Manne <74592735+ManneG@users.noreply.github.com>
2025-11-19 23:59:54 +0100

    Added icon support for input component and added dependencies required for datetime selector

--- src/lib/components/ui/input/input.svelte
+++ src/lib/components/ui/input/input.svelte
@@ -23,43 +23,61 @@
     files = $bindable(),
     class: className,
     "data-slot": dataSlot = "input",
+    children: childProps,
     ...restProps
   }: Props = $props();
 </script>
 
-{#if type === "file"}
-  <input
-    bind:this={ref}
-    data-slot={dataSlot}
-    class={cn(
-      "selection:bg-primary dark:bg-input/30 selection:text-primary-foreground border-input ring-offset-background placeholder:text-muted-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 pt-1.5 text-right text-sm font-medium shadow-xs transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50",
-      "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-      " aria-invalid:border-rosa-500 dark:aria-invalid:bg-rosa-950 aria-invalid:bg-rosa-50 aria-invalid:text-rosa-500 dark:aria-invalid:text-rosa-500 aria-invalid:border-[1px]",
-      className,
-    )}
-    type="file"
-    bind:files
-    bind:value
-    {...restProps}
-  />
-{:else}
-  <input
-    bind:this={ref}
-    data-slot={dataSlot}
-    class={cn(
-      "border-input bg-background selection:bg-primary dark:bg-input/30 selection:text-primary-foreground ring-offset-background placeholder:text-muted-foreground flex h-9 w-full min-w-0 rounded-md border px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-      "focus-visible:border-muted-foreground focus-visible:ring-muted-foreground/20 focus-visible:ring-[3px]",
-      " aria-invalid:border-rosa-500 dark:aria-invalid:bg-rosa-950 aria-invalid:bg-rosa-50 aria-invalid:text-rosa-500 dark:aria-invalid:text-rosa-500 aria-invalid:border-[1px]",
-      className,
-    )}
-    {type}
-    bind:value
-    {...restProps}
-  />
-{/if}
+<div class="relative">
+  {#if childProps}<span
+      aria-hidden="true"
+      class={cn(
+        "absolute left-3 flex h-full w-4 items-center overflow-hidden",
+        restProps["aria-errormessage"] ? "text-rosa-500" : "",
+      )}>{@render childProps()}</span
+    >{/if}
+  {#if type === "file"}
+    <input
+      bind:this={ref}
+      data-slot={dataSlot}
+      class={cn(
+        "selection:bg-primary dark:bg-input/30 selection:text-primary-foreground border-input ring-offset-background placeholder:text-muted-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 pt-1.5 text-right text-sm font-medium shadow-xs transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50",
+        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+        " aria-invalid:border-rosa-500 dark:aria-invalid:bg-rosa-950 aria-invalid:bg-rosa-50 aria-invalid:text-rosa-500 dark:aria-invalid:text-rosa-500 aria-invalid:border-[1px]",
+        className,
+      )}
+      type="file"
+      bind:files
+      bind:value
+      {...restProps}
+    />
+  {:else}
+    <input
+      bind:this={ref}
+      data-slot={dataSlot}
+      class={cn(
+        "border-input bg-background selection:bg-primary dark:bg-input/30 selection:text-primary-foreground ring-offset-background placeholder:text-muted-foreground flex h-8 w-full min-w-0 rounded-md border px-3 py-1 shadow-xs transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "focus-visible:border-muted-foreground focus-visible:ring-muted-foreground/20 focus-visible:ring-[3px]",
+        "aria-invalid:border-rosa-500 dark:aria-invalid:bg-rosa-950 aria-invalid:bg-rosa-50 aria-invalid:text-rosa-500 dark:aria-invalid:text-rosa-500 aria-invalid:border-[1px]",
+        className,
+      )}
+      {type}
+      bind:value
+      {...restProps}
+    />
+  {/if}
+</div>
 {#if restProps["aria-errormessage"]}
-  <p class="text-rosa-500 mt-0 text-xs font-semibold">
+  <p class="text-rosa-500 mt-1 text-xs font-semibold">
     <CircleAlert class="mb-[2px] inline h-[1rem] w-[1rem]" />
     {restProps["aria-errormessage"]}
   </p>
+{/if}
+
+{#if childProps}
+  <style>
+    input {
+      text-indent: calc(var(--spacing) * 6);
+    }
+  </style>
 {/if}
