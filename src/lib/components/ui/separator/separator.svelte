fdbaf51e2562abbf5628a1e36f98d95082a6a694
Felix Ohrgren <179166750+fgren@users.noreply.github.com>
2025-11-24 18:47:23 +0100

    add file upload component (#1007)
    * style alert dialog
    
    * add dialog component
    
    * add separator component
    
    * fix alert dialog styling
    
    * update styles
    
    * add fileupload translations
    
    * add file upload component
    
    * fix formatting
    
    * make fileupload not have fixed width
    
    * rename variables, use nicer alt

--- src/lib/components/ui/separator/separator.svelte
+++ src/lib/components/ui/separator/separator.svelte
@@ -5,17 +5,44 @@
   let {
     ref = $bindable(null),
     class: className,
+    textClass,
     "data-slot": dataSlot = "separator",
+    text,
     ...restProps
-  }: SeparatorPrimitive.RootProps = $props();
+  }: SeparatorPrimitive.RootProps & {
+    text?: string;
+    textClass?: string;
+  } = $props();
 </script>
 
-<SeparatorPrimitive.Root
-  bind:ref
-  data-slot={dataSlot}
-  class={cn(
-    "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
-    className,
-  )}
-  {...restProps}
-/>
+{#if text}
+  <div class="flex flex-row items-center justify-center gap-4">
+    <SeparatorPrimitive.Root
+      bind:ref
+      data-slot={dataSlot}
+      class={cn(
+        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-1/2 data-[orientation=vertical]:h-1/2 data-[orientation=vertical]:w-px",
+        className,
+      )}
+      {...restProps}
+    />
+    <span class={textClass}>{text}</span>
+    <SeparatorPrimitive.Root
+      bind:ref
+      data-slot={dataSlot}
+      class={cn(
+        "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-1/2 data-[orientation=vertical]:h-1/2 data-[orientation=vertical]:w-px",
+        className,
+      )}
+      {...restProps}
+    />
+  </div>
+{:else}<SeparatorPrimitive.Root
+    bind:ref
+    data-slot={dataSlot}
+    class={cn(
+      "bg-border shrink-0 data-[orientation=horizontal]:h-px data-[orientation=horizontal]:w-full data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px",
+      className,
+    )}
+    {...restProps}
+  />{/if}
