1e49a53fd67410d044dfee712cba93913e9e032c
Simon Mechler <170467894+Fiery-132@users.noreply.github.com>
2025-12-01 22:34:19 +0100

    add cleanup logic to scheduler rate limiters

--- scheduler-service/rateLimitMiddleware.go
+++ scheduler-service/rateLimitMiddleware.go
@@ -1,29 +1,53 @@
 package main
 
 import (
+	"log"
 	"net"
 	"net/http"
 	"sync"
+	"time"
 
 	"golang.org/x/time/rate"
 )
 
 var (
-	limiters = make(map[string]*rate.Limiter)
+	limiters = make(map[string]*trackedLimiter)
 	mu       sync.Mutex
 )
+
+type trackedLimiter struct {
+	*rate.Limiter
+	lastSeen time.Time
+}
+
+func cleanupLimiters(expiry time.Duration) {
+	now := time.Now()
+	for ip, limiter := range limiters {
+		if now.Sub(limiter.lastSeen) > expiry {
+			delete(limiters, ip)
+		}
+	}
+}
 
 func getLimiter(ip string) *rate.Limiter {
 	mu.Lock()
 	defer mu.Unlock()
 
-	limiter, exists := limiters[ip]
+	cleanupLimiters(10 * time.Minute)
+	log.Println("Current limiters:", len(limiters))
+
+	lim, exists := limiters[ip]
 	if !exists {
-		limiter = rate.NewLimiter(1, 5)
-		limiters[ip] = limiter
+		lim = &trackedLimiter{
+			Limiter:  rate.NewLimiter(1, 5),
+			lastSeen: time.Now(),
+		}
+		limiters[ip] = lim
+	} else {
+		lim.lastSeen = time.Now()
 	}
 
-	return limiter
+	return lim.Limiter
 }
 
 func rateLimitMiddleware(next http.Handler) http.Handler {
