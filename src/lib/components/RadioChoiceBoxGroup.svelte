8e1972a8dfa0e809a33538a58b0890e42ec4cadb
JuliusNicklas <julius.bivren@gmail.com>
2025-12-01 16:53:59 +0100

    redundant imports

--- src/lib/components/RadioChoiceBoxGroup.svelte
+++ src/lib/components/RadioChoiceBoxGroup.svelte
@@ -1,8 +1,5 @@
 <script lang="ts">
   import * as RadioGroup from "$lib/components/ui/radio-group/index.js";
-  import { Label } from "$lib/components/ui/label/index.js";
-  import { cn, type WithElementRef } from "$lib/utils.js";
-  import type { HTMLAttributes } from "svelte/elements";
   import { RadioGroup as RadioGroupPrimitive } from "bits-ui";
 
   let { value = $bindable(""), ...restprops }: RadioGroupPrimitive.RootProps =
