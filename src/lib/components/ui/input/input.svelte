89e4462b9522a714ca2da0b6e0daa99a09e49f7f
Manne <74592735+ManneG@users.noreply.github.com>
2025-12-01 23:21:31 +0100

    DateTimeSelector implemented

--- src/lib/components/ui/input/input.svelte
+++ src/lib/components/ui/input/input.svelte
@@ -10,7 +10,7 @@
 
   type InputType = Exclude<HTMLInputTypeAttribute, "file">;
 
-  type Props = WithElementRef<
+  export type Props = WithElementRef<
     Omit<HTMLInputAttributes, "type"> &
       (
         | { type: "file"; files?: FileList }
@@ -34,25 +34,29 @@
   {#if childProps}<span
       aria-hidden="true"
       class={cn(
-        "absolute left-3 flex h-full w-4 items-center overflow-hidden",
+        "pointer-events-none absolute left-3 flex h-8 w-4 items-center overflow-hidden",
         restProps["aria-errormessage"] ? "text-rosa-500" : "",
       )}>{@render childProps()}</span
     >{/if}
   {#if type === "file"}
-    <input
-      bind:this={ref}
-      data-slot={dataSlot}
+    <label
       class={cn(
-        "selection:bg-primary dark:bg-input/30 selection:text-primary-foreground border-input ring-offset-background placeholder:text-muted-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 pt-1.5 text-right text-sm font-medium shadow-xs transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50",
+        "hover:bg-secondary-hover flex flex-row items-center justify-center gap-2 transition-[background-color,box-shadow]",
+        "selection:bg-primary dark:bg-input/30 selection:text-primary-foreground border-input ring-offset-background placeholder:text-muted-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 text-right text-sm font-medium shadow-xs transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50",
         "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
         " aria-invalid:border-rosa-500 dark:aria-invalid:bg-rosa-950 aria-invalid:bg-rosa-50 aria-invalid:text-rosa-500 dark:aria-invalid:text-rosa-500 aria-invalid:border-[1px]",
         className,
       )}
-      type="file"
-      bind:files
-      bind:value
-      {...restProps}
-    />
+      ><CloudUpload class="h-[16px] w-[16px]" />{m.fileupload_choose()}<input
+        bind:this={ref}
+        data-slot={dataSlot}
+        type="file"
+        class="hidden"
+        bind:files
+        bind:value
+        {...restProps}
+      /></label
+    >
   {:else}
     <input
       bind:this={ref}
@@ -68,47 +72,13 @@
       {...restProps}
     />
   {/if}
+  {#if restProps["aria-errormessage"]}
+    <p class="text-rosa-500 mt-1 text-xs font-semibold">
+      <CircleAlert class="mb-[2px] inline h-[1rem] w-[1rem]" />
+      {restProps["aria-errormessage"]}
+    </p>
+  {/if}
 </div>
-{#if type === "file"}
-  <label
-    class={cn(
-      "hover:bg-secondary-hover flex flex-row items-center justify-center gap-2 transition-[background-color,box-shadow]",
-      "selection:bg-primary dark:bg-input/30 selection:text-primary-foreground border-input ring-offset-background placeholder:text-muted-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 text-right text-sm font-medium shadow-xs transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50",
-      "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-      " aria-invalid:border-rosa-500 dark:aria-invalid:bg-rosa-950 aria-invalid:bg-rosa-50 aria-invalid:text-rosa-500 dark:aria-invalid:text-rosa-500 aria-invalid:border-[1px]",
-      className,
-    )}
-    ><CloudUpload class="h-[16px] w-[16px]" />{m.fileupload_choose()}<input
-      bind:this={ref}
-      data-slot={dataSlot}
-      type="file"
-      class="hidden"
-      bind:files
-      bind:value
-      {...restProps}
-    /></label
-  >
-{:else}
-  <input
-    bind:this={ref}
-    data-slot={dataSlot}
-    class={cn(
-      "border-input bg-background selection:bg-primary dark:bg-input/30 selection:text-primary-foreground ring-offset-background placeholder:text-muted-foreground flex h-9 w-full min-w-0 rounded-md border px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-      "focus-visible:border-muted-foreground focus-visible:ring-muted-foreground/20 focus-visible:ring-[3px]",
-      " aria-invalid:border-rosa-500 dark:aria-invalid:bg-rosa-950 aria-invalid:bg-rosa-50 aria-invalid:text-rosa-500 dark:aria-invalid:text-rosa-500 aria-invalid:border-[1px]",
-      className,
-    )}
-    {type}
-    bind:value
-    {...restProps}
-  />
-{/if}
-{#if restProps["aria-errormessage"]}
-  <p class="text-rosa-500 mt-1 text-xs font-semibold">
-    <CircleAlert class="mb-[2px] inline h-[1rem] w-[1rem]" />
-    {restProps["aria-errormessage"]}
-  </p>
-{/if}
 
 {#if childProps}
   <style>
