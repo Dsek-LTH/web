b5432f86da66873ffa44b087435491416de75bc3
JuliusNicklas <julius.bivren@gmail.com>
2025-12-01 16:46:43 +0100

    Made radiobutton

---
+++ src/lib/components/RadioChoiceBoxGroup.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import * as RadioGroup from "$lib/components/ui/radio-group/index.js";
+  import { Label } from "$lib/components/ui/label/index.js";
+  import { cn, type WithElementRef } from "$lib/utils.js";
+  import type { HTMLAttributes } from "svelte/elements";
+  import { RadioGroup as RadioGroupPrimitive } from "bits-ui";
+
+  let { value = $bindable(""), ...restprops }: RadioGroupPrimitive.RootProps =
+    $props();
+</script>
+
+<RadioGroup.Root bind:value {...restprops}></RadioGroup.Root>
