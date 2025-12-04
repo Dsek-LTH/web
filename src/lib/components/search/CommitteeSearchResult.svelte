0b6def74ddc58a4a29a327c8a8f8c1ace993d9f0
Isak <isak@kallini.se>
2025-12-04 15:20:07 +0100

    Command dialog (#1004)
    * Add command dialog
    
    * Add command dialog part 2
    
    * Ignore svelte/no-navigation-without-resolv
    
    * Fix padding
    
    * do not wrap event date
    
    * fix scroll
    
    ---------
    
    Co-authored-by: fgren <179166750+fgren@users.noreply.github.com>

---
+++ src/lib/components/search/CommitteeSearchResult.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import * as Command from "$lib/components/ui/command/index.js";
+  import * as Avatar from "$lib/components/ui/avatar/index.js";
+  import type { CommitteeSearchReturnAttributes } from "$lib/search/searchTypes";
+  import CommitteeIcon from "../images/CommitteeIcon.svelte";
+  import { languageTag } from "$paraglide/runtime";
+  const { data }: { data: CommitteeSearchReturnAttributes } = $props();
+</script>
+
+<Command.LinkItem
+  class="flex flex-row justify-between"
+  href={`/comittees/${data.shortName}`}
+  data-search-result
+>
+  <div class="flex flex-row items-center gap-2">
+    <Avatar.Root>
+      <CommitteeIcon committee={data}></CommitteeIcon>
+    </Avatar.Root>
+    <div class="flex flex-col">
+      <span class="font-medium"
+        >{languageTag() === "sv" ? data.nameSv : data.nameEn}</span
+      >
+      <span class="text-muted-foreground">
+        {languageTag() === "sv" ? data.descriptionSv : data.descriptionEn}
+      </span>
+    </div>
+  </div>
+</Command.LinkItem>
---
+++ src/lib/components/search/SongSearchResult.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import * as Command from "$lib/components/ui/command/index.js";
+  import type { SongSearchReturnAttributes } from "$lib/search/searchTypes";
+  import Music2 from "@lucide/svelte/icons/music-2";
+  const { data }: { data: SongSearchReturnAttributes } = $props();
+</script>
+
+<Command.LinkItem
+  class="flex flex-row justify-between"
+  href={`/songs/${data.slug}`}
+  data-search-result
+>
+  <div class="flex flex-row items-center gap-2">
+    <Music2 />
+    <div class="flex flex-col">
+      <span class="font-medium">{data.title}</span>
+      <span class="text-muted-foreground line-clamp-1">
+        {data.melody}
+      </span>
+    </div>
+  </div>
+</Command.LinkItem>
