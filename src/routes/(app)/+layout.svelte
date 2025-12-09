2cc4174f85a8300f9625455d6bc5178bd0b26690
fgren <179166750+fgren@users.noreply.github.com>
2025-12-09 21:43:27 +0100

    fix navbar order and icons

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -1,16 +1,5 @@
 <script>
-  import Footer from "./Footer.svelte";
-  import Navbar from "./Navbar.svelte";
-  import DarkmodeToggle from "./DarkmodeToggle.svelte";
-
   const { children } = $props();
 </script>
 
-<nav class="contents">
-  <Navbar />
-</nav>
-<div class="p-2">
-  <DarkmodeToggle />
-</div>
 {@render children?.()}
-<Footer />
