0d1d3e1477ce8f44d35491d3eed742be0b397528
fgren <179166750+fgren@users.noreply.github.com>
2025-12-09 21:44:51 +0100

    make navbar responsive

--- src/routes/(app)/Navbar.svelte
+++ src/routes/(app)/Navbar.svelte
@@ -7,14 +7,16 @@
   import Search from "@lucide/svelte/icons/search";
   import Languages from "@lucide/svelte/icons/languages";
   import Bell from "@lucide/svelte/icons/bell";
-  import { getRoutes } from "../routes";
+  import Menu from "@lucide/svelte/icons/menu";
+  import ChevronDown from "@lucide/svelte/icons/chevron-down";
+  import X from "@lucide/svelte/icons/x";
+  import { getRoutes } from "./(app)/routes";
   import { navigationMenuTriggerStyle } from "$lib/components/ui/navigation-menu/navigation-menu-trigger.svelte";
   import { page } from "$app/state";
   import { i18n } from "$lib/utils/i18n";
   import { languageTag } from "$paraglide/runtime";
-
-  let commandDialogOpen = $state(false);
   import DarkmodeToggle from "./DarkmodeToggle.svelte";
+  import * as Drawer from "$lib/components/ui/drawer";
 </script>
 
 <div class=" flex min-w-screen flex-row justify-center border-b-[1px]">
@@ -33,52 +35,7 @@
               />
             </a>
           </NavigationMenu.Item>
-          {#each getRoutes() as route (route.title)}
-            <NavigationMenu.Item>
-              {#if route.children}
-                <NavigationMenu.Trigger>{route.title}</NavigationMenu.Trigger>
-
-                <NavigationMenu.Content>
-                  <ul
-                    class="ml-0 grid list-none gap-2 p-2 md:w-[400px] lg:w-[472px] lg:grid-cols-[.75fr_1fr]"
-                  >
-                    {#if route.pictureUrl}
-                      <li class="row-span-3">
-                        <NavigationMenu.Link
-                          class={`flex h-full w-full flex-col justify-end rounded-md bg-linear-to-t bg-[linear-gradient(to_top,rgba(0,0,0,1),rgba(0,0,0,0)),url('${route.pictureUrl}')] bg-cover bg-center p-6 no-underline outline-hidden select-none focus:shadow-md`}
-                          href={route.picturePath}
-                        >
-                          <div
-                            class="mt-4 mb-2 text-xl font-semibold text-[#ffffff]"
-                          >
-                            {route.pictureTitle}
-                          </div>
-                          <p class="text-sm leading-tight text-[#ffffff]">
-                            {route.pictureDescription}
-                          </p>
-                        </NavigationMenu.Link>
-                      </li>
-                    {/if}
-                    {#each route.children as child (child.title)}
-                      <NavigationMenu.Link href={child.path}>
-                        <NavigationMenu.ContentItem
-                          title={child.title}
-                          description={child.description ?? ""}
-                        />
-                      </NavigationMenu.Link>
-                    {/each}
-                  </ul>
-                </NavigationMenu.Content>
-              {:else}
-                <NavigationMenu.Link
-                  href={route.path}
-                  class={navigationMenuTriggerStyle()}
-                >
-                  {route.title}
-                </NavigationMenu.Link>
-              {/if}
-            </NavigationMenu.Item>
-          {/each}
+          {@render desktopLinks()}
         </NavigationMenu.List>
       </NavigationMenu.Root>
     </div>
@@ -108,12 +65,121 @@
         class="p-1.5"><Bell /></Button
       >
       <DarkmodeToggle class="text-muted-foreground border-0" />
-      <Avatar.Root>
+      <Avatar.Root class="hidden md:inline">
         <Avatar.Image src="https://picsum.photos/200" alt="profile picture" />
         <Avatar.Fallback>IK</Avatar.Fallback>
       </Avatar.Root>
+      <Drawer.Root direction="right">
+        <Drawer.Trigger
+          ><Button
+            aria-label="menubar"
+            size="icon-lg"
+            variant="ghost"
+            class="block p-1.5 md:hidden"><Menu /></Button
+          ></Drawer.Trigger
+        >
+        <Drawer.Content class="max-w-xs!">
+          <Drawer.Header>
+            <Drawer.Title class="flex flex-row justify-between"
+              ><div class="flex flex-row items-center gap-2">
+                <DsekLogo
+                  variant="letter"
+                  class="hover:fill-rosa-400 fill-rosa-400 size-6"
+                />
+                <h1 class="text-[14px] leading-6">D-sektionen inom TLTH</h1>
+              </div>
+              <Drawer.Close
+                ><X
+                  class="text-muted-foreground hover:bg-secondary-hover m-1 h-6 w-6 rounded-xs p-1"
+                /></Drawer.Close
+              ></Drawer.Title
+            >
+            <Drawer.Description class="flex flex-col">
+              {@render mobileLinks()}</Drawer.Description
+            >
+          </Drawer.Header>
+        </Drawer.Content>
+      </Drawer.Root>
     </div>
   </div>
 </div>
 
-<CommandDialog bind:open={commandDialogOpen} />
+{#snippet desktopLinks()}
+  {#each getRoutes() as route (route.title)}
+    <NavigationMenu.Item class="hidden md:block">
+      {#if route.children}
+        <NavigationMenu.Trigger>{route.title}</NavigationMenu.Trigger>
+
+        <NavigationMenu.Content>
+          <ul
+            class="ml-0 grid list-none gap-2 p-2 md:w-[400px] lg:w-[472px] lg:grid-cols-[.75fr_1fr]"
+          >
+            {#if route.pictureUrl}
+              <li class="row-span-3">
+                <NavigationMenu.Link
+                  class={`flex h-full w-full flex-col justify-end rounded-md bg-linear-to-t bg-[linear-gradient(to_top,rgba(0,0,0,1),rgba(0,0,0,0)),url('${route.pictureUrl}')] bg-cover bg-center p-6 no-underline outline-hidden select-none focus:shadow-md`}
+                  href={route.picturePath}
+                >
+                  <div class="mt-4 mb-2 text-xl font-semibold text-[#ffffff]">
+                    {route.pictureTitle}
+                  </div>
+                  <p class="text-sm leading-tight text-[#ffffff]">
+                    {route.pictureDescription}
+                  </p>
+                </NavigationMenu.Link>
+              </li>
+            {/if}
+            {#each route.children as child (child.title)}
+              <NavigationMenu.Link href={child.path}>
+                <NavigationMenu.ContentItem
+                  title={child.title}
+                  description={child.description ?? ""}
+                />
+              </NavigationMenu.Link>
+            {/each}
+          </ul>
+        </NavigationMenu.Content>
+      {:else}
+        <NavigationMenu.Link
+          href={route.path}
+          class={navigationMenuTriggerStyle()}
+        >
+          {route.title}
+        </NavigationMenu.Link>
+      {/if}
+    </NavigationMenu.Item>
+  {/each}
+{/snippet}
+
+{#snippet mobileLinks()}
+  <div class="">
+    {#each getRoutes() as route (route.title)}
+      {#if route.children}
+        <h6 class="text-foreground m-1 block p-1 text-lg font-medium">
+          <ChevronDown class="mr-[2px] inline h-[16px] w-[16px]" />{route.title}
+        </h6>
+
+        <div>
+          <ul
+            class="ml-0 grid list-none pr-2 pb-2 pl-7 md:w-[400px] lg:w-[472px] lg:grid-cols-[.75fr_1fr]"
+          >
+            {#each route.children as child (child.title)}
+              <a
+                class="hover:bg-secondary-hover block rounded-sm px-1 py-1 transition-all"
+                href={child.path}
+              >
+                {child.title}
+              </a>
+            {/each}
+          </ul>
+        </div>
+      {:else}
+        <a href={route.path} class="hover:bg-secondary-hover block rounded-sm">
+          <h6 class="text-foreground m-1 block p-1 text-lg font-medium">
+            <span class="ml-[18px]">{route.title}</span>
+          </h6>
+        </a>
+      {/if}
+    {/each}
+  </div>
+{/snippet}
