77d50726d08bd800b3216c25a3792b59dd1a0d48
JuliusNicklas <julius.bivren@gmail.com>
2025-12-15 18:09:38 +0100

    Can now change props properly

--- src/lib/components/RadioChoiceBoxItem.svelte
+++ src/lib/components/RadioChoiceBoxItem.svelte
@@ -3,25 +3,25 @@
   import { Label } from "$lib/components/ui/label/index.js";
   import type { WithoutChildrenOrChild } from "$lib/utils";
   import { RadioGroup as RadioGroupPrimitive } from "bits-ui";
+  import { Label as LabelPrimitive } from "bits-ui";
 
   let {
     title,
     description = "",
+    class: ClassName,
     ...restProps
-  }: WithoutChildrenOrChild<RadioGroupPrimitive.ItemProps> & {
-    description?: string;
-  } = $props();
+  }: WithoutChildrenOrChild<RadioGroupPrimitive.ItemProps> &
+    LabelPrimitive.RootProps & {
+      description?: string;
+    } = $props();
 </script>
 
 <Label
-  class="group 
-  text-2xs 
-  hover:bg-accent/50
-  has-[[aria-checked=true]]:border-rosa-background 
-  has-[[aria-checked=true]]:bg-rosa-50 
-  dark:has-[[aria-checked=true]]:bg-rosa-950 
-   
-  flex w-full flex-row justify-between rounded-md border p-3"
+  class={[
+    " group text-2xs hover:bg-accent/50 has-[[aria-checked=true]]:border-rosa-background has-[[aria-checked=true]]:bg-rosa-50 dark:has-[[aria-checked=true]]:bg-rosa-950  flex w-full flex-row justify-between rounded-md border p-3",
+    ClassName,
+  ]}
+  {...restProps}
 >
   <div
     class=" flex flex-col justify-center gap-0.5 p-0.5 text-left font-normal"
