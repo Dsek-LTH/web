3540552b26a4f8ae18fe1e640dbcd0aaf3e3de27
Isak <isak@kallini.se>
2025-12-22 18:17:35 +0100

    New design landing page calendar (#1035)
    * Add landing page calendar
    
    * Landing page calendar done
    
    * Make code more cursed
    
    * fix lint

--- src/routes/(app)/home/+page.svelte
+++ src/routes/(app)/home/+page.svelte
@@ -1,5 +1,22 @@
-<script>
-  import NotImplemented from "$lib/components/NotImplemented.svelte";
+<script lang="ts">
+  import HomeCalendar from "$lib/components/homeCalendar/HomeCalendar.svelte";
+  const { data } = $props();
 </script>
 
-<NotImplemented />
+<div class="flex flex-col items-center justify-between pb-10 md:flex-row">
+  <div class="flex flex-row items-center justify-between">
+    <h1 class="pb-4 font-sans whitespace-nowrap">
+      Hej, {data.member?.firstName}!
+    </h1>
+  </div>
+</div>
+
+<h3>Event</h3>
+<HomeCalendar
+  events={data.events.map((e) => ({
+    startDate: e.startDatetime,
+    endDate: e.endDatetime,
+    slug: e.slug ?? "",
+    title: e.title,
+  }))}
+/>
