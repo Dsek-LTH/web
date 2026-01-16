ff7a5557e069ac12af4d6d4ad782688f5d9670b1
fgren <179166750+fgren@users.noreply.github.com>
2026-01-16 12:27:42 +0100

    fix pagination

--- src/lib/components/Pagination.svelte
+++ src/lib/components/Pagination.svelte
@@ -1,55 +1,51 @@
 <script lang="ts">
   import { page } from "$app/state";
   import * as Pagination from "$lib/components/ui/pagination";
-  import { SvelteURLSearchParams } from "svelte/reactivity";
+  import { cn } from "$lib/utils";
 
-  let { pageCount = 10 }: { pageCount?: number } = $props();
+  let { pageCount = 10, class: klass }: { pageCount?: number; class?: string } =
+    $props();
 
-  const searchParams = $derived(
-    new SvelteURLSearchParams(page.url.searchParams),
+  let thisPage = $derived(
+    Number.parseInt(page.url.searchParams?.get("page") ?? "1"),
   );
-  let thisPage = $derived(Number.parseInt(searchParams.get("page") ?? "0"));
-
-  const getPageLink = $derived((p: number) => {
-    return `?page=${p}`;
-  });
 </script>
 
-<Pagination.Root class="w-full" count={pageCount} perPage={1}>
-  {#snippet children({ pages, currentPage })}
+<Pagination.Root class={cn(klass, "w-full")} count={pageCount} perPage={1}>
+  {#snippet children({ pages })}
     <Pagination.Content>
-      <Pagination.Item>
+      <Pagination.Item data-sveltekit-preload-data="off">
         {#if thisPage > 1}
-          <a href={getPageLink(thisPage - 1)}>
+          <a data-sveltekit-preload-code="off" href={`?page=${thisPage - 1}`}>
             <Pagination.PrevButton />
           </a>
         {:else}
           <Pagination.PrevButton />
         {/if}
       </Pagination.Item>
-      {#each pages as page (page.key)}
-        {#if page.type === "ellipsis"}
+      {#each pages as pageItem (pageItem.key)}
+        {#if pageItem.type === "ellipsis"}
           <Pagination.Item>
             <Pagination.Ellipsis />
           </Pagination.Item>
         {:else}
-          <Pagination.Item>
+          <Pagination.Item data-sveltekit-preload-data="off">
             <Pagination.Link
-              {page}
-              href={getPageLink(currentPage)}
-              isActive={currentPage === page.value}
+              page={pageItem}
+              href={`?page=${pageItem.value}`}
+              isActive={thisPage == pageItem.value}
             >
               {#snippet child({ props })}
                 <a {...props}>
-                  {page.value}
+                  {pageItem.value}
                 </a>
               {/snippet}
             </Pagination.Link>
           </Pagination.Item>
         {/if}
       {/each}
-      <Pagination.Item>
-        <a href={getPageLink(thisPage + 1)}>
+      <Pagination.Item data-sveltekit-preload-data="off">
+        <a href={`?page=${thisPage + 1}`}>
           <Pagination.NextButton />
         </a>
       </Pagination.Item>
