812c315e8e03060431050d50f35cbc754adff52f
Rufus Kogg Röjder <rufus@rojder.se>
2026-02-02 19:10:51 +0100

    fix formating

--- src/lib/components/ui/menubar/menubar-menu.svelte
+++ src/lib/components/ui/menubar/menubar-menu.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
-	import { Menubar as MenubarPrimitive } from "bits-ui";
+  import { Menubar as MenubarPrimitive } from "bits-ui";
 
-	let { ...restProps }: MenubarPrimitive.MenuProps = $props();
+  let { ...restProps }: MenubarPrimitive.MenuProps = $props();
 </script>
 
 <MenubarPrimitive.Menu {...restProps} />
--- src/lib/components/ui/menubar/menubar-radio-group.svelte
+++ src/lib/components/ui/menubar/menubar-radio-group.svelte
@@ -1,11 +1,16 @@
 <script lang="ts">
-	import { Menubar as MenubarPrimitive } from "bits-ui";
+  import { Menubar as MenubarPrimitive } from "bits-ui";
 
-	let {
-		ref = $bindable(null),
-		value = $bindable(""),
-		...restProps
-	}: MenubarPrimitive.RadioGroupProps = $props();
+  let {
+    ref = $bindable(null),
+    value = $bindable(""),
+    ...restProps
+  }: MenubarPrimitive.RadioGroupProps = $props();
 </script>
 
-<MenubarPrimitive.RadioGroup bind:ref bind:value data-slot="menubar-radio-group" {...restProps} />
+<MenubarPrimitive.RadioGroup
+  bind:ref
+  bind:value
+  data-slot="menubar-radio-group"
+  {...restProps}
+/>
