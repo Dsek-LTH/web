94eeb411b34065c8fa9a9a85a0e1f973945c9937
Rufus Kogg Röjder <rufus@rojder.se>
2026-02-09 18:34:20 +0100

    Menubar (#1075)
    * Install and modify packages
    
    * Install package
    
    * fix formating

---
+++ src/lib/components/ui/menubar/menubar.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+  import { Menubar as MenubarPrimitive } from "bits-ui";
+  import { cn } from "$lib/utils.js";
+
+  let {
+    ref = $bindable(null),
+    class: className,
+    ...restProps
+  }: MenubarPrimitive.RootProps = $props();
+</script>
+
+<MenubarPrimitive.Root
+  bind:ref
+  data-slot="menubar"
+  class={cn(
+    `bg-background
+		flex
+		h-9
+		items-center
+		gap-0
+		rounded-md
+		border
+		p-0
+		shadow-xs`,
+    className,
+  )}
+  {...restProps}
+/>
