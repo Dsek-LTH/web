b185f7ca3e617ed13972a26cc37d0aaaa7e0e3cd
Simon Mechler <170467894+Fiery-132@users.noreply.github.com>
2026-02-11 14:53:59 +0100

    scheduler working - remove debug logs

--- src/lib/server/getDecryptedJWT.ts
+++ src/lib/server/getDecryptedJWT.ts
@@ -12,15 +12,9 @@ import { getToken, type JWT } from "@auth/core/jwt";
  * @returns The decrypted JWT object, or `null` if not authenticated.
  */
 export const getDecryptedJWT = async (req: Request): Promise<JWT | null> => {
-  console.log("[DEBUG] AUTH_SECRET:", env.AUTH_SECRET);
-  console.log("[DEBUG] Incoming request:", req);
-  const token = await getToken({
+  return await getToken({
     req,
     secret: env.AUTH_SECRET,
-    secureCookie: env.NODE_ENV === "production",
+    secureCookie: env["NODE_ENV"] === "production",
   });
-  console.log("[DEBUG] Decrypted JWT:", token);
-
-  return token;
-  // return await getToken({ req, secret: env.AUTH_SECRET });
 };
