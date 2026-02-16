4605d7c92d21daa8696da6494d726b4c6a0e55ae
Rufus Kogg Röjder <rufus@rojder.se>
2026-02-16 23:37:26 +0100

    Update database

---
+++ src/database/prisma/migrations/20260216222618_add_photo_albums/migration.sql
@@ -0,0 +1,46 @@
+-- CreateTable
+CREATE TABLE "albums" (
+    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
+    "title" TEXT NOT NULL,
+    "date" DATE NOT NULL,
+    "created_at" TIMESTAMPTZ(6) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(6) NOT NULL,
+    "photos_directory" TEXT NOT NULL,
+    "image_count" INTEGER NOT NULL DEFAULT 0,
+
+    CONSTRAINT "albums_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_album_photographer" (
+    "A" UUID NOT NULL,
+    "B" UUID NOT NULL,
+
+    CONSTRAINT "_album_photographer_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateTable
+CREATE TABLE "_album_editor" (
+    "A" UUID NOT NULL,
+    "B" UUID NOT NULL,
+
+    CONSTRAINT "_album_editor_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE INDEX "_album_photographer_B_index" ON "_album_photographer"("B");
+
+-- CreateIndex
+CREATE INDEX "_album_editor_B_index" ON "_album_editor"("B");
+
+-- AddForeignKey
+ALTER TABLE "_album_photographer" ADD CONSTRAINT "_album_photographer_A_fkey" FOREIGN KEY ("A") REFERENCES "albums"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_album_photographer" ADD CONSTRAINT "_album_photographer_B_fkey" FOREIGN KEY ("B") REFERENCES "members"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_album_editor" ADD CONSTRAINT "_album_editor_A_fkey" FOREIGN KEY ("A") REFERENCES "albums"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_album_editor" ADD CONSTRAINT "_album_editor_B_fkey" FOREIGN KEY ("B") REFERENCES "members"("id") ON DELETE CASCADE ON UPDATE CASCADE;
