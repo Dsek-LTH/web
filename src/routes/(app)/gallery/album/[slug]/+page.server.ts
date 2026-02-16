bbac3bbd325649f309fbd687717ff2cc838b0340
Rufus Kogg Röjder <rufus@rojder.se>
2026-02-16 23:37:43 +0100

    Implement backend

--- src/routes/(app)/gallery/album/[slug]/+page.server.ts
+++ src/routes/(app)/gallery/album/[slug]/+page.server.ts
@@ -5,13 +5,19 @@ import type { PageServerLoad } from "./$types";
 
 export const load: PageServerLoad = async ({ locals, params }) => {
   const { user } = locals;
-  const album = params.slug;
+  const album = await locals.prisma.album.findFirst({
+    where: { slug: params.slug },
+    include: {
+      photographers: true,
+      editors: true,
+    },
+  });
 
   let pictures: FileData[] = await fileHandler
     .getInBucket(
       user,
       PUBLIC_BUCKETS_ALBUMS,
-      "public/" + album.split("-")[0] + "/" + album,
+      "public/" + album?.slug + "/album",
       true,
     )
     .catch((err) => {
@@ -19,25 +25,8 @@ export const load: PageServerLoad = async ({ locals, params }) => {
       return [];
     });
 
-  type Metadata = {
-    photographer: string;
-    editor: string;
-  };
-
-  const metadataUrl = pictures.filter((e) => e.name == "album.json")[0]
-    ?.thumbnailUrl;
-  let metadata: Metadata | undefined;
-  if (metadataUrl) {
-    metadata = await fetch(metadataUrl)
-      .then((res) => res.text())
-      .then((text) => JSON.parse(text));
-  }
-
-  pictures = pictures.filter((e) => e.name != "album.json");
-
   return {
     album: album,
     pictures: pictures,
-    metadata: metadata,
   };
 };
