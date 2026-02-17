2bfb0d3c57f9f34bc9002d3ecda3f7a61faaad27
Melker Widén <melker.widen@gmail.com>
2026-02-17 02:13:28 +0100

    Fix bad stuff

--- src/lib/components/ui/kbd/kbd.svelte
+++ src/lib/components/ui/kbd/kbd.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { cn, type WithElementRef } from "$lib/utils.js";
-    import { onMount } from "svelte";
+  import { onMount } from "svelte";
   import type { HTMLAttributes } from "svelte/elements";
 
   let {
@@ -8,32 +8,36 @@
     class: className,
     symbol,
     trigger,
-    children,
     ...restProps
-  }: WithElementRef<HTMLAttributes<HTMLElement>> & { symbol: string, trigger?: string } = $props();
+  }: WithElementRef<HTMLAttributes<HTMLElement>> & {
+    symbol: string;
+    trigger?: string;
+  } = $props();
 
-    let highlighted = $state(false);
-    const key = $derived(trigger ?? symbol.toLowerCase());
+  let highlighted = $state(false);
+  const key = $derived(trigger ?? symbol.toLowerCase());
 
-    const setHighlight = (e: KeyboardEvent) => highlighted = e.key === key || highlighted;
-    const unsetHighlight = (e: KeyboardEvent) => highlighted = e.key !== key && highlighted;
+  const setHighlight = (e: KeyboardEvent) =>
+    (highlighted = e.key === key || highlighted);
+  const unsetHighlight = (e: KeyboardEvent) =>
+    (highlighted = e.key !== key && highlighted);
 
   onMount(() => {
     window.addEventListener("keydown", setHighlight);
     window.addEventListener("keyup", unsetHighlight);
-    
+
     return () => {
       window.removeEventListener("keydown", setHighlight);
       window.removeEventListener("keyup", unsetHighlight);
     };
-  })
+  });
 </script>
 
 <kbd
   bind:this={ref}
   data-slot="kbd"
   class={cn(
-    "bg-muted-background data-[highlight=true]:bg-rosa-background data-[highlight=true]:text-rosa-foreground data-[highlight=true]:border-rosa-background transition-colors border border-border text-semibold text-muted-foreground inline-flex h-4 w-4 min-h-16px min-w-16px items-center justify-center gap-1 rounded-xs px-1/2 font-sans text-xs font-medium select-none",
+    "bg-muted-background data-[highlight=true]:bg-rosa-background data-[highlight=true]:text-rosa-foreground data-[highlight=true]:border-rosa-background border-border text-semibold text-muted-foreground pointer-events-none inline-flex min-h-4 min-w-4 items-center justify-center gap-1 rounded-xs border px-1 align-middle font-sans text-xs leading-none font-medium transition-colors select-none",
     "[&_svg:not([class*='size-'])]:size-3",
     "[[data-slot=tooltip-content]_&]:bg-background/20 [[data-slot=tooltip-content]_&]:text-background dark:[[data-slot=tooltip-content]_&]:bg-background/10",
     className,
@@ -41,5 +45,5 @@
   data-highlight={highlighted}
   {...restProps}
 >
-    {symbol}
+  {symbol}
 </kbd>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,21 +1,11 @@
 <script>
   import NotImplemented from "$lib/components/NotImplemented.svelte";
   import Footer from "./(app)/Footer.svelte";
-  import * as Kbd from "$lib/components/ui/kbd/index";
   import Navbar from "./(app)/Navbar.svelte";
 </script>
 
 <nav class="contents">
   <Navbar />
 </nav>
-
-<div class="flex flex-col items-center gap-4">
-  <Kbd.Group>
-    <Kbd.Root symbol="CTRL" trigger="Control"/>
-    <span>+</span>
-    <Kbd.Root symbol="Y"/>
-  </Kbd.Group>
-</div>
-
+<NotImplemented />
 <Footer />
-
