d3e47a857ee6a11d2c0dc270091c7ae95359d188
Christofer Gärtner <crillegartner@gmail.com>
2026-03-09 10:36:08 +0100

    Add upload to button

--- src/routes/(app)/expenses/+page.svelte
+++ src/routes/(app)/expenses/+page.svelte
@@ -7,6 +7,10 @@
 </script>
 
 <h1 class="pb-5">Utlägg</h1>
-<Button variant="outline" class="border-rosa-400">Skapa utlägg</Button>
+
+<a href="/expenses/upload"
+  ><Button class="border-rosa-400">Skapa utlägg</Button></a
+>
+
 <h2 class="py-5">Mina utlägg</h2>
 <ExpenseTable {columns} data={data.myExpenses} />
