98e09e1988b016251927cbd19a745ea46a098f99
Tomas Kamsäter <112856079+Tomas-Ka@users.noreply.github.com>
2026-03-16 18:36:06 +0100

    Cafe page typing fix (#1135)
    * Fix typing (which crashed website client side)
    
    Turns out you can't import Prisma client side, so this fixes that by
    exporting a vetted and build time checked version of the TimeSlot field.
    
    This could be done programatically with a hook/generate script of some
    kind, but I don't know how those work.
    
    * fix formatting

--- src/routes/(app)/committees/cafe/types.ts
+++ src/routes/(app)/committees/cafe/types.ts
@@ -1,5 +1,16 @@
-import { Prisma, TimeSlot } from "@prisma/client";
+import { Prisma } from "@prisma/client";
+import { TimeSlot as PrismaTimeSlot } from "@prisma/client";
 import { z } from "zod";
+export type { CiabattaOfTheWeek as Ciabatta } from "@prisma/client";
+
+export const TimeSlot = {
+  DAYMANAGER: "DAYMANAGER",
+  SHIFT_1: "SHIFT_1",
+  SHIFT_2: "SHIFT_2",
+  SHIFT_3: "SHIFT_3",
+} as const satisfies Record<string, PrismaTimeSlot>;
+export type TimeSlot = (typeof TimeSlot)[keyof typeof TimeSlot];
+
 export const scheduleForm = z.object({
   date: z.date(),
   worker: z.string().optional(),
