11a25afb57d95b4e874e1cd37ff6440ecf27f89f
fgren <179166750+fgren@users.noreply.github.com>
2026-03-29 23:31:56 +0200

    redesign landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  import { Building, GraduationCap } from "@lucide/svelte";
   import * as Carousel from "$lib/components/ui/carousel/index.js";
   import { getFileUrl } from "$lib/files/client";
   import Footer from "./(app)/Footer.svelte";
@@ -7,42 +6,74 @@
   import * as m from "$paraglide/messages";
   import Button from "$lib/components/ui/button/button.svelte";
 
+  import ArrowRight from "@lucide/svelte/icons/arrow-right";
+  import Calendar from "@lucide/svelte/icons/calendar";
+  import GraduationCap from "@lucide/svelte/icons/graduation-cap";
+  import House from "@lucide/svelte/icons/house";
+  import Building from "@lucide/svelte/icons/building";
+  import Heart from "@lucide/svelte/icons/heart";
+  import Link from "@lucide/svelte/icons/link";
+
   const SECTIONS = [
     {
       title: m.landing_party_title(),
-      slogan: m.landing_party_slogan(),
       description: m.landing_party_description(),
-      images: ["party-1.jpg", "party-2.jpg", "party-3.jpg"],
+      cta: m.landing_party_cta(),
+      icon: Calendar,
+      variant: "rosa",
+      images: ["party-2.jpg", "party-1.jpg", "party-3.jpg"],
+      link: "/events",
     },
     {
       title: m.landing_activity_title(),
-      slogan: m.landing_activity_slogan(),
       description: m.landing_activity_description(),
+      cta: m.landing_activity_cta(),
+      icon: House,
+      variant: "lila",
       images: ["activity-1.jpg", "activity-2.jpg", "activity-3.jpg"],
+      link: "/about",
+    },
+    {
+      title: m.landing_study_title(),
+      description: m.landing_study_description(),
+      cta: m.landing_study_cta(),
+      icon: GraduationCap,
+      variant: "pistachio",
+      images: ["studies.jpg", "studies.jpg"],
+      link: "/committees/srd",
     },
   ] as const;
 
   const ARTICLES = [
     {
-      title: m.landing_guild_title(),
-      description: m.landing_guild_description(),
-      image: "guild.jpg",
+      title: m.landing_companies_title(),
+      description: m.landing_companies_description(),
+      image: "naru.jpg",
       imagePosition: "50% 40%",
       imageSize: "125%",
-    },
-    {
-      title: m.landing_study_title(),
-      description: m.landing_study_description(),
-      image: "studies.jpg",
-      imagePosition: "50% 0%",
-      imageSize: "150%",
+      cta: m.landing_companies_cta(),
+      icon: Building,
+      link: "/info/for-foretag",
     },
     {
       title: m.landing_dchip_title(),
       description: m.landing_dchip_description(),
       image: "dchip.jpg",
-      imagePosition: "50% 80%",
+      imagePosition: "50% 60%",
       imageSize: "250%",
+      cta: m.landing_dchip_cta(),
+      icon: Heart,
+      link: "https://dchip.se",
+    },
+    {
+      title: m.landing_contact_title(),
+      description: m.landing_contact_description(),
+      image: "studies2.jpg",
+      imagePosition: "50% 80%",
+      imageSize: "150%",
+      cta: m.landing_contact_cta(),
+      icon: Link,
+      link: "/contact",
     },
   ] as const;
 </script>
@@ -51,11 +82,11 @@
   <Navbar />
 </nav>
 <header
-  class="relative h-screen bg-linear-to-t bg-[linear-gradient(to_top,rgba(0,0,0,1),rgba(0,0,0,0.7),rgba(0,0,0,0)),url('https://files.dsek.se/files/public/photos/hero.jpg')] bg-cover bg-center"
+  class="relative h-[90vh] bg-linear-to-t bg-[linear-gradient(to_right,rgba(0,0,0,0.9),rgba(0,0,0,0.7),rgba(0,0,0,0)),url('https://files.dsek.se/files/public/photos/hero2.jpg')] bg-cover bg-center"
 >
   <div class="absolute top-1/3 px-10 lg:pl-44">
     <h1
-      class="mb-10 font-sans text-5xl font-bold text-white uppercase lg:text-9xl xl:text-[125px]"
+      class="font-sans text-5xl font-bold break-keep text-white uppercase sm:mb-10 sm:text-7xl lg:text-9xl xl:text-[125px]"
     >
       {m.dsektionen()}
     </h1>
@@ -84,16 +115,20 @@
 <main class="flex flex-col gap-2">
   {#each SECTIONS as section, i (section.title)}
     <section
-      class="flex w-full flex-col lg:flex-row"
+      class="flex flex-col items-center gap-8 border-b-[1px] px-8 py-8 sm:px-12 md:px-16 lg:h-104 lg:flex-row lg:py-0 xl:px-36"
       class:lg:flex-row-reverse={i === 1}
+      class:bg-muted-background={i === 1}
     >
-      <Carousel.Root opts={{ loop: true }} class="lg:w-3/5">
-        <Carousel.Content class="ms-0 h-80 w-full lg:h-[500px]">
+      <Carousel.Root
+        opts={{ loop: true }}
+        class="shrink-0 rounded-lg shadow-lg md:w-[468px]"
+      >
+        <Carousel.Content class="ms-0 h-60 w-full rounded-lg md:h-80">
           {#each section.images as image, i (i)}
-            <Carousel.Item class="ps-0">
+            <Carousel.Item class="rounded-lg ps-0">
               <img
                 src={getFileUrl(`minio/files/public/photos/${image}`)}
-                class="h-full w-full object-cover"
+                class="h-full w-full rounded-lg object-cover"
                 alt="party"
               />
             </Carousel.Item>
@@ -107,68 +142,55 @@
         />
       </Carousel.Root>
 
-      <div
-        class="text-md flex flex-col px-10 lg:w-2/5 lg:items-start lg:gap-7 lg:px-16 xl:px-24"
-      >
-        <h2 class="pt-6 text-xl font-bold uppercase lg:text-2xl xl:text-3xl">
+      <div class="flex flex-col gap-2">
+        <h1>
           {section.title}
-        </h2>
-        <h1
-          class="text-4xl font-bold text-balance uppercase lg:text-left lg:text-5xl xl:text-6xl"
-        >
-          {section.slogan}
         </h1>
-        <p class="mb-8 max-w-prose text-sm font-medium lg:text-lg xl:text-xl">
+        <p class="">
           {section.description}
         </p>
+        <!-- eslint-disable-next-line svelte/no-at-html-tags -->
+        <a class="mt-0" href={section.link}
+          ><Button variant={section.variant} class="w-fit"
+            ><svelte:component this={section.icon} />{section.cta}
+            <ArrowRight /></Button
+          ></a
+        >
       </div>
     </section>
   {/each}
 
-  <section class="mx-10 mt-6 flex flex-col items-center">
-    <h2 class="mb-8 text-center font-bold uppercase lg:text-3xl">
-      {m.landing_community_title()}
-    </h2>
-    <div
-      class="flex flex-col gap-x-14 lg:grid lg:w-5/6 lg:grid-flow-col lg:grid-cols-3 lg:grid-rows-2 lg:gap-y-0"
-    >
+  <section class="bg-muted-background flex flex-col items-center px-10 py-6">
+    <div class="flex flex-col gap-8 lg:grid lg:grid-cols-3 lg:gap-16">
       {#each ARTICLES as article, i (i)}
-        <article class="contents">
-          <h1
-            class="w-full self-end bg-[linear-gradient(180deg,transparent_0%,var(--muted-background)_85%),var(--url)] bg-no-repeat px-8 pt-[200px] pb-4 text-4xl font-bold uppercase"
+        <article
+          class="bg-background flex h-full w-[332px] flex-col rounded-md text-center"
+        >
+          <div
+            class="w-full self-end rounded-t-md bg-[var(--url)] bg-no-repeat px-8 pt-[200px] pb-4 text-4xl font-bold uppercase"
             style:background-size={article.imageSize}
             style:background-position={article.imagePosition}
-            style:--url="url({getFileUrl(
+            style:background-image="url({getFileUrl(
               `minio/files/public/photos/${article.image}`,
             )})"
+          ></div>
+          <div
+            class="flex h-full flex-col items-center rounded-b-md border-[1px] border-t-0 p-8 pt-4"
           >
-            {article.title}
-          </h1>
-          <p
-            class="bg-muted-background mt-0 mb-6 w-full max-w-prose px-8 pb-6 text-sm font-medium lg:pb-0 lg:text-base"
-          >
-            {article.description}
-          </p>
+            <h2>{article.title}</h2>
+            <p class="mt-2 mb-3 text-left">
+              {article.description}
+            </p>
+            <a href={article.link} class="mt-auto">
+              <Button class="mt-auto w-fit"
+                ><svelte:component this={article.icon} />
+                {article.cta}
+                <ArrowRight /></Button
+              ></a
+            >
+          </div>
         </article>
       {/each}
-
-      <!-- <article
-        class="flex flex-col items-center justify-end gap-5 bg-[linear-gradient(180deg,transparent_0%,var(--muted-background)_50%),var(--url)] bg-no-repeat px-8 py-[50px] pt-[200px]"
-        style:background-size={article.imageSize}
-        style:background-position={article.imagePosition}
-        style:--url="url({getFileUrl(
-          `minio/files/public/photos/${article.image}`,
-        )})"
-      >
-        <h1 class="text-4xl font-bold uppercase">
-          {article.title}
-        </h1>
-        <p
-          class="max-w-prose text-sm font-medium lg:max-w-[300px] lg:text-base"
-        >
-          {article.description}
-        </p>
-      </article> -->
     </div>
   </section>
 </main>
