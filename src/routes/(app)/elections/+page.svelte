fc0443bba4467bcb17b3222303eba7740edcd436
fgren <179166750+fgren@users.noreply.github.com>
2026-04-17 01:41:23 +0200

    add elections page

--- src/routes/(app)/elections/+page.svelte
+++ src/routes/(app)/elections/+page.svelte
@@ -1,5 +1,38 @@
 <script lang="ts">
-  import NotImplemented from "$lib/components/NotImplemented.svelte";
+  import type { PageData } from "./$types";
+  import * as m from "$paraglide/messages";
+  import { isAuthorized } from "$lib/utils/authorization";
+  import apiNames from "$lib/utils/apiNames";
+  import dayjs from "dayjs";
+  import utc from "dayjs/plugin/utc";
+  import timezone from "dayjs/plugin/timezone";
+  import { Button } from "$lib/components/ui/button";
+  import ElectionCard from "./ElectionCard.svelte";
+
+  let { data }: { data: PageData } = $props();
+
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
 </script>
 
-<NotImplemented />
+<div class="layout-container">
+  <div class="flex flex-row">
+    <h1>{m.openElections()}</h1>
+    {#if isAuthorized(apiNames.ELECTION.CREATE, data.user)}
+      <a href="/elections/create" class="ml-auto"><Button>+ Nytt val</Button></a
+      >
+    {/if}
+  </div>
+
+  <p class="my-4">
+    {m.elections_description()}
+  </p>
+
+  <section
+    class="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4"
+  >
+    {#each data.openElections as election, index (election.id)}
+      <ElectionCard {election} user={data.user} {index} />
+    {/each}
+  </section>
+</div>
---
+++ src/routes/(app)/elections/ElectionCard.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+  import CommitteeIcon from "$lib/components/images/CommitteeIcon.svelte";
+  import MarkdownBody from "$lib/components/MarkdownBody.svelte";
+  import { Button } from "$lib/components/ui/button";
+  import type { ExtendedPrismaModel } from "$lib/server/extendedPrisma";
+  import apiNames from "$lib/utils/apiNames";
+  import { isAuthorized } from "$lib/utils/authorization";
+  import * as m from "$paraglide/messages";
+  import Pen from "@lucide/svelte/icons/pen";
+  import type { AuthUser } from "@zenstackhq/runtime";
+  import dayjs from "dayjs";
+  import utc from "dayjs/plugin/utc";
+  import timezone from "dayjs/plugin/timezone";
+  import DOMPurify from "isomorphic-dompurify";
+
+  dayjs.extend(utc);
+  dayjs.extend(timezone);
+
+  let {
+    election,
+    user,
+    index,
+  }: {
+    election: ExtendedPrismaModel<"Election"> & {
+      committee?: Pick<
+        ExtendedPrismaModel<"Committee">,
+        | "id"
+        | "name"
+        | "nameSv"
+        | "nameEn"
+        | "darkImageUrl"
+        | "lightImageUrl"
+        | "monoImageUrl"
+      >;
+    };
+    user?: AuthUser;
+    index?: number;
+  } = $props();
+
+  const breakName = (name: string) => {
+    let output = "";
+    if (name.includes("utskottet"))
+      output = `${name.split("utskottet")[0]}&shy;utskottet`;
+    else if (name.includes("kommittén"))
+      output = `${name.split("kommittén")[0]}&shy;kommittén`;
+    else if (name.includes("mästeriet"))
+      output = `${name.split("mästeriet")[0]}&shy;mästeriet`;
+    else if (name.includes("rådet"))
+      output = `${name.split("rådet")[0]}&shy;rådet`;
+    else output = name;
+    return DOMPurify.sanitize(output);
+  };
+</script>
+
+<div
+  class="bg-muted-background animate-in fade-in fill-mode-backwards slide-in-from-bottom-[1rem] relative flex flex-col items-center gap-1 rounded-md border-[1px] p-4 duration-300"
+  style="animation-delay:{(index ?? 0) * 50}ms"
+>
+  {#if isAuthorized(apiNames.ELECTION.UPDATE, user)}
+    <Button
+      href={"/elections/" + election.id + "/edit"}
+      class="absolute top-2 right-2"
+      size="icon-sm"
+      variant="lila"
+      ><Pen />
+    </Button>
+  {/if}
+
+  <div class="w-5/12 self-center text-center">
+    <CommitteeIcon committee={election.committee ?? null} />
+  </div>
+
+  <h3 class="text-center">
+    <!-- eslint-disable-next-line svelte/no-at-html-tags Already sanitized -->
+    {@html breakName(election.committee?.name ?? "")}
+  </h3>
+
+  <MarkdownBody class="self-start" body={election.markdown} />
+
+  <div class="mt-auto flex flex-col gap-2">
+    <p>
+      {m.elections_close()}
+      {dayjs(election.expiresAt).tz(dayjs.tz.guess()).format("YYYY-MM-DD")}
+    </p>
+    <div>
+      <Button
+        href={election.link}
+        target="_blank"
+        rel="noreferrer"
+        class="w-full"
+      >
+        {m.elections_apply()}
+      </Button>
+    </div>
+  </div>
+</div>
