a0597c6d911d2a966cbc9904c6f111604633a4b3
fgren <179166750+fgren@users.noreply.github.com>
2026-04-25 12:38:39 +0200

    change tooltip delay

--- src/lib/components/ui/tooltip/tooltip-provider.svelte
+++ src/lib/components/ui/tooltip/tooltip-provider.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import { Tooltip as TooltipPrimitive } from "bits-ui";
 
-  let { delayDuration = 0, ...restProps }: TooltipPrimitive.ProviderProps =
+  let { delayDuration = 150, ...restProps }: TooltipPrimitive.ProviderProps =
     $props();
 </script>
 
