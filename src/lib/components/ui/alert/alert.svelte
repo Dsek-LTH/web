1f0d7f9f77a61995db49e276be25cdf6957455ac
Isak <isak@kallini.se>
2026-05-11 22:47:29 +0200

    New design: fix toasts (#1177)
    * fix toasts
    
    * Fix toast styling and icons
    
    ---------
    
    Co-authored-by: Manne <74592735+ManneG@users.noreply.github.com>

--- src/lib/components/ui/alert/alert.svelte
+++ src/lib/components/ui/alert/alert.svelte
@@ -2,12 +2,19 @@
   import { type VariantProps, tv } from "tailwind-variants";
 
   export const alertVariants = tv({
-    base: "relative grid w-full grid-cols-[0_1fr] items-start gap-y-0.5 rounded-lg border px-4 py-3 text-sm has-[>svg]:grid-cols-[calc(var(--spacing)*4)_1fr] has-[>svg]:gap-x-3 [&>svg]:size-4 [&>svg]:translate-y-0.5 [&>svg]:text-current",
+    base:
+      "relative grid w-full grid-cols-[0_1fr] items-start gap-y-0.5 rounded-lg border-2 px-4 py-3 text-sm has-[>svg]:grid-cols-[calc(var(--spacing)*4)_1fr] has-[>svg]:gap-x-3 [&>svg]:size-4 [&>svg]:translate-y-0.5 [&>svg]:text-current " +
+      "bg-[color(from_currentColor_srgb_calc(r+0.5)_calc(g+0.5)_calc(b+0.5))] border-current *:data-[slot=alert-description]:text-current/90 [&>svg]:text-current " +
+      "dark:bg-[color(from_currentColor_srgb_calc(r-0.6)_calc(g-0.6)_calc(b-0.6))]",
     variants: {
       variant: {
         default: "bg-card text-card-foreground",
-        destructive:
-          "text-destructive bg-card *:data-[slot=alert-description]:text-destructive/90 [&>svg]:text-current",
+        error: "text-alert-error-background",
+        success: "text-pistachio-400",
+        info: "text-alert-info-background",
+        warning: "text-alert-warning-background",
+        primary: "bg-card dark:bg-card border",
+        hidden: "hidden",
       },
     },
     defaultVariants: {
