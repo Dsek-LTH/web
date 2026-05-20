c32489fee79967a57ddea3172f4c5f8b6e29f8d0
Nelli Skogman <nelli.skogman@gmail.com>
2026-05-20 15:02:18 +0200

    Split past and upcoming events

--- src/routes/(app)/events/EventPageLoad.ts
+++ src/routes/(app)/events/EventPageLoad.ts
@@ -1,4 +1,4 @@
-import { getEvents } from "$lib/events/getEvents";
+import { getEvents, type EventSpan } from "$lib/events/getEvents";
 import { interestedGoingSchema } from "$lib/events/schema";
 import { getAllTags } from "$lib/news/tags";
 import {
@@ -6,8 +6,13 @@ import {
   getPageSizeOrThrowSvelteError,
 } from "$lib/utils/url.server";
 import type { ServerLoadEvent } from "@sveltejs/kit";
+import dayjs from "dayjs";
 import { zod4 } from "sveltekit-superforms/adapters";
 import { superValidate } from "sveltekit-superforms/server";
+import { z } from "zod";
+
+const displaySchema = z.enum(["week", "month", "upcoming", "past"]);
+export type Display = z.infer<typeof displaySchema>;
 
 const eventPageLoad =
   (adminMode = false) =>
@@ -15,12 +20,28 @@ const eventPageLoad =
     const { prisma } = locals;
     const eventCount = await prisma.event.count();
 
-    const pageSize = getPageSizeOrThrowSvelteError(url);
-    const page = getPageOrThrowSvelteError(url, {
-      fallbackValue: 1,
-      lowerBound: 1,
-      upperBound: Math.ceil(eventCount / pageSize),
-    });
+    const display = displaySchema.parse(
+      url.searchParams.get("display") ?? "week",
+    );
+
+    let spanFilter: EventSpan;
+
+    if (display === "week") {
+      spanFilter = { weekStartingAt: dayjs().startOf("week").toDate() };
+    } else if (display === "month") {
+      spanFilter = { monthStartingAt: dayjs().startOf("month").toDate() };
+    } else {
+      const pageSize = getPageSizeOrThrowSvelteError(url);
+      const page = getPageOrThrowSvelteError(url, {
+        upperBound: Math.ceil(eventCount / pageSize),
+      });
+
+      spanFilter = {
+        span: display,
+        pageSize,
+        page,
+      };
+    }
 
     const [[events, pageCount], allTags] = await Promise.all([
       getEvents(
@@ -28,8 +49,7 @@ const eventPageLoad =
         {
           tags: url.searchParams.getAll("tags"),
           search: url.searchParams.get("search") ?? undefined,
-          page,
-          pageSize,
+          ...spanFilter,
         },
         !adminMode,
       ),
@@ -40,6 +60,7 @@ const eventPageLoad =
       events,
       pageCount,
       allTags,
+      display,
       interestedGoingForm: await superValidate(zod4(interestedGoingSchema)),
     };
   };
