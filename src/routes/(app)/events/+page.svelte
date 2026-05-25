8901130e6b48c318eff2c4c45b62641ba22640d6
Nelli Skogman <nelli.skogman@gmail.com>
2026-05-26 01:02:52 +0200

    Style

--- src/routes/(app)/events/+page.svelte
+++ src/routes/(app)/events/+page.svelte
@@ -9,6 +9,7 @@
   import { page } from "$app/state";
   import { goto } from "$app/navigation";
   import type { Display } from "./EventPageLoad.js";
+  import dayjs from "dayjs";
 
   const { data } = $props();
 
@@ -24,6 +25,13 @@
 
   const kindSelectorVariant = (buttonKind: Display) =>
     display === buttonKind ? "rosa" : "outline";
+
+  let eventPairs = $derived(
+    data.events.map((event, index) => ({
+      last: index === 0 ? null : data.events[index - 1]!,
+      event,
+    })),
+  );
 </script>
 
 <div class="flex w-full flex-row justify-between">
@@ -68,9 +76,26 @@
   <!-- TODO: Implement this when the month calendar gets added. -->
   <NotImplemented />
 {:else}
-  <div class="flex flex-col gap-2 pt-2">
-    {#each data.events as event, index (event.id)}
-      <EventCard {event} {index} />
+  <div class="flex flex-col gap-4 pt-8">
+    {#each eventPairs as { last, event }, index (event.id)}
+      {@const shouldAddWeekday =
+        last?.startDatetime?.getDay() !== event.startDatetime.getDay()}
+
+      <div
+        class="flex flex-col gap-4 sm:flex-row"
+        class:pt-8={shouldAddWeekday}
+      >
+        <div class="min-w-40">
+          {#if shouldAddWeekday}
+            <h2>
+              {dayjs(event.startDatetime).format("dddd")}
+            </h2>
+            {dayjs(event.startDatetime).format("D MMMM")}
+          {/if}
+        </div>
+
+        <EventCard {event} {index} />
+      </div>
     {/each}
 
     <Pagination pageCount={data.pageCount} />
