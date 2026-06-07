d3bd2fbf280578c0c63d13268f964ac4433df23e
fgren <179166750+fgren@users.noreply.github.com>
2026-06-07 12:26:13 +0200

    fix receipts

--- src/routes/(app)/expenses/upload/+page.svelte
+++ src/routes/(app)/expenses/upload/+page.svelte
@@ -5,10 +5,17 @@
   import { CalendarDate } from "@internationalized/date";
   import { createExpense } from "./upload.remote";
   import FileUpload from "$lib/components/FileUpload.svelte";
-  import { receipt } from "$paraglide/messages";
-  import { Select } from "$lib/components/ui/select";
+  import * as m from "$paraglide/messages";
+  import * as Select from "$lib/components/ui/select";
   import createBasicReceipt from "../baseItem";
   import { COST_CENTERS } from "../config";
+  import dayjs from "dayjs";
+  import { Label } from "$lib/components/ui/label";
+  import Button from "$lib/components/ui/button/button.svelte";
+  import * as Card from "$lib/components/ui/card";
+  import PiggyBank from "@lucide/svelte/icons/piggy-bank";
+  import Coins from "@lucide/svelte/icons/coins";
+  import Pen from "@lucide/svelte/icons/pen";
 
   const { date, description, isGuildCard, receipts } = createExpense.fields;
   receipts.set([createBasicReceipt()]);
@@ -23,19 +30,15 @@
 </script>
 
 <div>
-  <form {...createExpense}>
+  <form {...createExpense} enctype="multipart/form-data">
     <div>
       Datum på kvitto: <DatePicker
         {...{
           ...date.as("date"),
-          value: new CalendarDate(
-            today.getFullYear(),
-            today.getMonth() + 1,
-            today.getDate(),
-          ),
+          value: dayjs(today).format("YYYY-MM-DD"),
         }}
         class=""
-        displayCompact
+        iso
       />
     </div>
 
@@ -49,15 +52,45 @@
       Beskrivning: <Input {...description.as("text")} />
     </div>
     {#each receipts.value() as receipt, i (receipt)}
-      <FileUpload {...receipts[i]!.image.as("file")} />
-      {#each receipts[i]!.rows.value() as row, j (row)}
-        <Select
-          {...receipts[i]!.rows[j]!.costCenter.as("select")}
-          items={costCenters}
-          type="single"
-        />
-      {/each}
+      <Card.Root>
+        <Card.Content class="flex flex-col gap-2">
+          <FileUpload allowUrl={false} {...receipts[i]!.image.as("file")} />
+          {#each receipts[i]!.rows.value() as row, j (row)}
+            {@const receipt = receipts[i]!.rows[j]!}
+            <div class="flex flex-col gap-1.5">
+              <Label>{m.expense_type()}</Label>
+              <Select.Root
+                {...receipts[i]!.rows[j]!.costCenter.as("text")}
+                items={costCenters}
+                type="single"
+                bind:value={receipt.costCenter.as("select").value
+                  ? receipt.costCenter.as("select").value.toString
+                  : () => undefined,
+                (v) => receipt.costCenter.set(v ?? "")}
+                ><Select.Trigger class="w-full"
+                  ><PiggyBank />{receipt.costCenter
+                    .as("select")
+                    .value.toString()}</Select.Trigger
+                >
+                <Select.Content>
+                  {#each costCenters as item (item.value)}
+                    <Select.Item value={item.value}>{item.label}</Select.Item>
+                  {/each}
+                </Select.Content></Select.Root
+              >
+            </div>
+            <div class="flex flex-col gap-1.5">
+              <Label>{m.expense_amount()}</Label>
+              <Input {...receipt.amount.as("number")}><Coins /></Input>
+            </div>
+            <div class="flex flex-col gap-1.5">
+              <Label>{m.receipt_comment()}</Label>
+              <Input {...receipt.comment.as("text")}><Pen /></Input>
+            </div>
+          {/each}
+        </Card.Content>
+      </Card.Root>
     {/each}
-    <button>hej</button>
+    <Button>Submit</Button>
   </form>
 </div>
