be169234fbabac264c5051474f6fb5b93a6f005b
fgren <179166750+fgren@users.noreply.github.com>
2026-07-07 18:28:15 +0200

    add info pages

--- src/routes/(app)/info/[slug]/+page.svelte
+++ src/routes/(app)/info/[slug]/+page.svelte
@@ -1,5 +1,19 @@
-<script>
-  import NotImplemented from "$lib/components/NotImplemented.svelte";
+<script lang="ts">
+  import { page } from "$app/state";
+  import MarkdownBody from "$lib/components/MarkdownBody.svelte";
+  import SetPageTitle from "$lib/components/nav/SetPageTitle.svelte";
+  import { Button } from "$lib/components/ui/button";
+  import { isAuthorized } from "$lib/utils/authorization";
+  import type { PageData } from "./$types";
+  let { data }: { data: PageData } = $props();
 </script>
 
-<NotImplemented />
+<SetPageTitle title={page.params.slug} />
+
+<div class="flex flex-col items-center p-2">
+  {#if data && isAuthorized(`markdowns:${data.slug}:update`, data.user)}
+    <Button class="btn my-4" href="{data.slug}/edit">Edit</Button>
+  {/if}
+
+  <MarkdownBody body={data.markdown?.markdown} />
+</div>
