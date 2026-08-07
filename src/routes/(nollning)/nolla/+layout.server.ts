5d5068655838b4dd84608a20499f870052e97091
David Agardh <agardh.david@gmail.com>
2026-08-07 14:30:19 +0200

    Update nollning start date comment

--- src/routes/(nollning)/nolla/+layout.server.ts
+++ src/routes/(nollning)/nolla/+layout.server.ts
@@ -1,6 +1,6 @@
 import { redirect } from "$lib/utils/redirect";
 
-const CUTOFF_DATE = Date.parse("2026-08-23T12:00:00"); // this will be in prod: 2026-08-24
+const CUTOFF_DATE = Date.parse("2026-08-23T12:00:00"); // this will be in prod: 2026-08-23
 export const load = () => {
   if (Date.now() > CUTOFF_DATE) {
     redirect(302, "/nollning");
