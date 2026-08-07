a0bf61da6c108ebc4641c217d66fd8d3adc9b720
David Agardh <agardh.david@gmail.com>
2026-08-07 15:09:34 +0200

    Nollning date fixes (#1246)
    * Update nollning messages start date
    
    * Update nollning notification start date
    
    * Update nollning start date comment
    
    * Update nollning app expiry date

--- src/routes/(nollning)/nollning/messages/+page.server.ts
+++ src/routes/(nollning)/nollning/messages/+page.server.ts
@@ -6,7 +6,7 @@ export const load = async ({ locals }) => {
     where: {
       ...BASIC_ARTICLE_FILTER(true),
       publishedAt: {
-        gte: new Date("2025-01-01"),
+        gte: new Date("2026-01-01"), // NEEDS TO BE UPDATED EVERY YEAR
       },
     },
     orderBy: {
