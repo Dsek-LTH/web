57f9c35c7aaebbeef472b92e705842304381ff6b
Casper Jensen <casper.v.jensen@gmail.com>
2026-08-13 14:57:52 +0200

    la till support för env.local till datum istället

--- src/routes/(nollning)/nolla/+layout.server.ts
+++ src/routes/(nollning)/nolla/+layout.server.ts
@@ -1,6 +1,7 @@
 import { redirect } from "$lib/utils/redirect";
+import { PRE_NOLLNING_DATE } from "$env/static/private";
 
-const CUTOFF_DATE = Date.parse("2026-08-23T12:00:00"); // this will be in prod: 2026-08-23
+const CUTOFF_DATE = Date.parse(PRE_NOLLNING_DATE); // this will be in prod: 2026-08-23
 export const load = () => {
   if (Date.now() > CUTOFF_DATE) {
     redirect(302, "/nollning");
--- src/routes/(nollning)/nollning/+layout.server.ts
+++ src/routes/(nollning)/nollning/+layout.server.ts
@@ -9,10 +9,11 @@ import { redirect } from "$lib/utils/redirect";
 import { notificationSchema } from "$lib/zod/schemas";
 import { superValidate } from "sveltekit-superforms";
 import { zod } from "sveltekit-superforms/adapters";
+import { PRE_NOLLNING_DATE } from "$env/static/private";
 
 const afterNollning = new Date("2026-10-06");
 export const load = async ({ locals, cookies }) => {
-  const CUTOFF_DATE = Date.parse("2026-08-23T12:00:00"); // this will be in prod: 2026-08-23
+  const CUTOFF_DATE = Date.parse("PRE_NOLLNING_DATE"); // this will be in prod: 2026-08-23
   if (Date.now() < CUTOFF_DATE) {
     redirect(302, "/nolla");
   }
