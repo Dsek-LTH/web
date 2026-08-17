822992fdb07b86f6ab611a8b970c97f60a34797d
CasperJensen19 <144714740+CasperJensen19@users.noreply.github.com>
2026-08-17 15:51:08 +0200

    Feature/nollning2026 (#1254)
    * Fixed maps to new color and font, also fixed color on tabs under wikia
    
    * fixed background on guild song for D
    
    * Fixed shadows on start page and header
    
    * Skrev klart ouvve biten och bytte loggan
    
    * Added image to ouvve, added english translations, fixed map images
    
    ---------
    
    Co-authored-by: David Agardh <agardh.david@gmail.com>

--- src/routes/(nollning)/nollning/wikia/dress-codes/+page.svelte
+++ src/routes/(nollning)/nollning/wikia/dress-codes/+page.svelte
@@ -4,7 +4,7 @@
   import mixed from "$lib/nollning/mixed.webp";
   import suit from "$lib/nollning/suit.webp";
   import frack from "$lib/nollning/hogtids.jpg";
-  import ouvve from "$lib/nollning/slasque.webp";
+  import ouvve from "$lib/nollning/ouvve.jpg";
 </script>
 
 <!-- eslint-disable svelte/no-at-html-tags -->
@@ -54,14 +54,37 @@
 
   <section class="ouvve">
     <h1 class="mt-8 text-center text-2xl font-semibold">
-      {@html m.nolla_packing_ouvve_titel()}
+      {@html m.nolla_ouvve_titel()}
     </h1>
     <div class="relative z-10 mb-4 flex justify-center">
       <img
         src={ouvve}
         alt="Bild på en ouvve"
-        class="relativez -10 h-64 w-64 object-cover md:h-[480px] md:w-[480px]"
+        class="h-64 w-64 rounded-lg object-cover md:h-[480px] md:w-[700px]"
       />
     </div>
+    <h2 class="mt-8 text-center text-2x1 font-semibold">
+      {@html m.nolla_ouvve_reason_titel()}
+    </h2>
+
+    <p class="mx-auto mt-4 max-w-3x1 text-center">
+      {@html m.nolla_ouvve_reason_text()}
+    </p>
+
+    <h2 class="mt-8 text-center text-2x1 font-semibold">
+      {@html m.nolla_ouvve_tradition_titel()}
+    </h2>
+
+    <p class="mx-auto mt-4 max-w-3x1 text-center">
+      {@html m.nolla_ouvve_tradition_text()}
+    </p>
+
+    <h2 class="mt-8 text-center text-2x1 font-semibold">
+      {@html m.nolla_ouvve_marken_titel()}
+    </h2>
+
+    <p class="mx-auto mt-4 max-w-3x1 text-center">
+      {@html m.nolla_ouvve_marken_text()}
+    </p>
   </section>
 </div>
