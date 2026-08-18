9b717ffc4bf482419b5ee8db6343ee41d981ddcb
David Agardh <agardh.david@gmail.com>
2026-08-18 14:36:31 +0200

    Refactor seperate internal and external minio connections

--- src/lib/files/minio.ts
+++ src/lib/files/minio.ts
@@ -1,19 +1,14 @@
 import { env } from "$env/dynamic/private";
-import {
-  PUBLIC_MINIO_ENDPOINT,
-  PUBLIC_MINIO_PORT,
-  PUBLIC_MINIO_USE_SSL,
-} from "$env/static/public";
 import { Client } from "minio";
 
 const minio = new Client({
-  endPoint: PUBLIC_MINIO_ENDPOINT || "localhost",
-  port: PUBLIC_MINIO_PORT
-    ? Number.parseInt(PUBLIC_MINIO_PORT, 10)
-    : PUBLIC_MINIO_USE_SSL === "true"
+  endPoint: env.MINIO_ENDPOINT || "localhost",
+  port: env.MINIO_PORT
+    ? Number.parseInt(env.MINIO_PORT, 10)
+    : env.MINIO_USE_SSL === "true"
       ? 443
       : 80,
-  useSSL: PUBLIC_MINIO_USE_SSL === "true",
+  useSSL: env.MINIO_USE_SSL === "true",
   accessKey: env.MINIO_ROOT_USER || "",
   secretKey: env.MINIO_ROOT_PASSWORD || "",
 });
