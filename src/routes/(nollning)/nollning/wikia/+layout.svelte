2d7681e3860a3dad9a3719ddc1c40d1ee2a7d8b9
Simon Mechler <170467894+Fiery-132@users.noreply.github.com>
2026-08-24 00:40:43 +0200

    fix lint and format errors

--- src/routes/(nollning)/nollning/wikia/+layout.svelte
+++ src/routes/(nollning)/nollning/wikia/+layout.svelte
@@ -79,7 +79,7 @@
       bind:this={elements[i]}
       id={link.link}
       href={link.link}
-      class="btn hover:bg-[#9966cc] !text-[#000000]"
+      class="btn !text-[#000000] hover:bg-[#9966cc]"
       class:btn-primary={isCurrent}
       class:btn-outline={!isCurrent}
     >
