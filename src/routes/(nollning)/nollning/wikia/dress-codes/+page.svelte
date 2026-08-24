fa9b91a8a209c25e5b09cb6824a6a2bcf48a198f
Casper Jensen <casper.v.jensen@gmail.com>
2026-08-24 10:51:17 +0200

    la till broken ouvve i klädkod

--- src/routes/(nollning)/nollning/wikia/dress-codes/+page.svelte
+++ src/routes/(nollning)/nollning/wikia/dress-codes/+page.svelte
@@ -1,26 +1,39 @@
 <script lang="ts">
   import * as m from "$paraglide/messages";
-  import slasque from "$lib/nollning/slasque.webp";
-  import mixed from "$lib/nollning/mixed.webp";
-  import suit from "$lib/nollning/suit.webp";
-  import frack from "$lib/nollning/hogtids.jpg";
-  import ouvve from "$lib/nollning/ouvve.jpg";
+  import slasque from "$lib/nollning/dressCode/slasque.webp";
+  import mixed from "$lib/nollning/dressCode/mixed.webp";
+  import suit from "$lib/nollning/dressCode/suit.webp";
+  import frack from "$lib/nollning/dressCode/hogtids.jpg";
+  import ouvve from "$lib/nollning/dressCode/ouvve.jpg";
+  import bruten from "$lib/nollning/dressCode/brutenOuvve.jpg";
 </script>
 
 <!-- eslint-disable svelte/no-at-html-tags -->
 <div>
   <section id="kladkod">
     <ul
-      class="grid grid-cols-1 gap-8 md:grid-cols-2 xl:grid-cols-4 [&>li>h6]:text-lg [&_h6]:font-semibold [&_i]:z-0 [&_i]:text-sm [&_i]:opacity-80 [&_img]:mb-2 [&_img]:rounded-box [&_img]:object-contain [&_p]:mt-2"
+      class="grid grid-cols-1 gap-8 md:grid-cols-6 [&>li>h6]:text-lg [&_h6]:font-semibold [&_i]:z-0 [&_i]:text-sm [&_i]:opacity-80 [&_img]:mb-2 [&_img]:rounded-box [&_img]:object-contain [&_p]:mt-2"
     >
-      <li>
+      <li class="md:col-span-3 md:col-start-2 xl:col-span-2 xl:col-start-2">
         <img width={1512} height={2016} src={slasque} alt="Slasque-exempel" />
         <h6>{@html m.nolla_packing_slasque()}</h6>
         <p>
           {@html m.nolla_packing_slasque_description()}
         </p>
       </li>
-      <li>
+      <li class="md:col-span-3 xl:col-span-2 xl:col-start-4">
+        <img
+          width={1512}
+          height={2016}
+          src={bruten}
+          alt="Bruten-ouvve-exempel"
+        />
+        <h6>{@html m.nolla_packing_broken()}</h6>
+        <p>
+          {@html m.nolla_packing_broken_description()}
+        </p>
+      </li>
+      <li class="md:col-span-2">
         <img
           width={1512}
           height={2016}
@@ -32,14 +45,14 @@
           {@html m.nolla_packing_mixed_suit_description()}
         </p>
       </li>
-      <li>
+      <li class="md:col-span-2">
         <img width={1512} height={2016} src={suit} alt="Mörk kostym exempel" />
         <h6>{@html m.nolla_packing_dark_suit()}</h6>
         <p>
           {@html m.nolla_packing_dark_suit_description()}
         </p>
       </li>
-      <li>
+      <li class="md:col-span-2">
         <img
           width={1512}
           height={2016}
