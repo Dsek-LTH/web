bc9a5b9514764efbdf9ecb34b77c9316a5c7464b
Casper Jensen <casper.v.jensen@gmail.com>
2026-08-24 16:07:38 +0200

    fixed text in faq

--- src/routes/(nollning)/nollning/wikia/faq/+page.svelte
+++ src/routes/(nollning)/nollning/wikia/faq/+page.svelte
@@ -9,7 +9,7 @@
     // },
     {
       title: "Hur funkar biljetter?",
-      desc: "Biljetter köps på ORBI, se till att du är medlem i D-sektionen Nollor 2025. Ditt efternamn ska vara din phaddergrupp.",
+      desc: "Biljetter köps på ORBI, se till att du är medlem i D-sektionen Nollor 2026. Fråga din phadder om du har frågor",
     },
     {
       title: "Måste jag vara med på allt under nollningen?",
@@ -39,7 +39,7 @@
     // },
     {
       title: "How do tickets work?",
-      desc: "Tickets are bought on ORBI, make sure you have a membership in D-sektionen Nollor 2025. Your last name should be your phadder group.",
+      desc: "Tickets are bought on ORBI, make sure you have a membership in D-sektionen Nollor 2026. If you have any questions ask your mentor.",
     },
     {
       title: "Do I have to participate in everything during nollningen?",
