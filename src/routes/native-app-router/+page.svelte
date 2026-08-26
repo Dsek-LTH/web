1b943e2f76af47724d1559324b2fdfe32c016ee4
Felix Ohrgren <179166750+fgren@users.noreply.github.com>
2026-08-26 12:34:01 +0200

    fix build, remove loading page

--- src/routes/native-app-router/+page.svelte
+++ src/routes/native-app-router/+page.svelte
@@ -1,49 +1,11 @@
 <script lang="ts">
-  import { REVEAL_LAUNCH_DATE } from "$lib/components/postReveal/types";
-  import { colors } from "$lib/utils/themes";
-  import { onMount } from "svelte";
   import { fade } from "svelte/transition";
-  // eslint-disable-next-line no-restricted-imports -- eh what the hell why not
-  import logo25 from "../(nollning)/nollning/(photos)/logo25.svg";
-  import type { PageData } from "./$types";
-  import { goto } from "$app/navigation";
-  import { resolve } from "$app/paths";
-
-  let { data }: { data: PageData } = $props();
-  onMount(() => {
-    goto(resolve(data.redirect), {
-      replaceState: true,
-    });
-  });
-  const revealTheme = REVEAL_LAUNCH_DATE <= new Date();
-  let background = $derived(
-    data.redirect === "/nollning" && revealTheme
-      ? colors.nollningPostReveal["base-100"]
-      : "#f280a1",
-  );
 </script>
 
-<div
-  class="relative h-dvh w-dvw"
-  style="background-color: {background}"
-  out:fade
->
-  {#if data.redirect === "/nollning" && revealTheme}
-    <!-- TODO: Replace with nollnings logo -->
-    <div
-      class="absolute top-1/2 left-1/2 size-60 max-h-[50%] max-w-[50%] -translate-x-1/2 -translate-y-1/2 md:size-80"
-    >
-      <img
-        src={logo25}
-        class="animate-scale-fade absolute inset-0"
-        alt="Nollning logo spinning"
-      />
-    </div>
-  {:else}
-    <img
-      src="/d-white.webp"
-      alt="D-sek logo"
-      class="absolute top-1/2 left-1/2 max-h-[50%] max-w-[50%] -translate-x-1/2 -translate-y-1/2"
-    />
-  {/if}
+<div class="relative h-dvh w-dvw" style="background-color: #f280a1" out:fade>
+  <img
+    src="/d-white.webp"
+    alt="D-sek logo"
+    class="absolute top-1/2 left-1/2 max-h-[50%] max-w-[50%] -translate-x-1/2 -translate-y-1/2"
+  />
 </div>
