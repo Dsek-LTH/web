51150a666b48693ee9127b44b029fce535820d29
Nelli Skogman <nelli.skogman@gmail.com>
2026-09-09 00:03:20 +0200

    Spellcheck miscellaneous files

--- src/lib/components/postReveal/types.ts
+++ src/lib/components/postReveal/types.ts
@@ -4,8 +4,8 @@ export const POST_REVEAL_PREFIX = "/nollning";
 export const REVEAL_LAUNCH_DATE = new Date("2026-08-27T14:00:00"); // NEEDS TO BE UPDATED EVERY YEAR
 export const APP_PREFERRED_PAGE_COOKIE = "APP-preferred-page";
 
-// they will check if a link starts with one of these and then redirect to nollning copy
-export const OVERRIDEN_POST_REVEAL_ROUTES = [
+// they will check if a link starts with one of these and then redirect to n0llning copy
+export const OVERRIDDEN_POST_REVEAL_ROUTES = [
   { from: "/shop/tickets", to: `${POST_REVEAL_PREFIX}/events` },
   { from: "/shop/cart" },
   { from: "/shop/inventory" },
