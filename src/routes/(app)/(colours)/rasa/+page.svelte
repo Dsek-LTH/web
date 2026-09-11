8df0b0d157ab1c5914d4b21ed33e917cafa8988e
Simon Mechler <170467894+Fiery-132@users.noreply.github.com>
2026-09-11 13:47:23 +0200

    add guild colour pages

---
+++ src/routes/(app)/(colours)/rasa/+page.svelte
@@ -0,0 +1,11 @@
+<div
+  class="flex min-h-screen items-center justify-center text-5xl font-bold text-black sm:text-8xl"
+>
+  #F280A1
+</div>
+
+<style>
+  :global(body) {
+    background-color: #f280a1;
+  }
+</style>
