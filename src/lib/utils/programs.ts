d08faf82c2772300b226fc56f4135ea81422bd31
Nelli Skogman <nelli.skogman@gmail.com>
2026-09-14 23:46:32 +0200

    Translate programmes

--- src/lib/utils/programs.ts
+++ src/lib/utils/programs.ts
@@ -1,9 +1,11 @@
+import * as messages from "$paraglide/messages";
+
 export const programs = [
-  { id: "D", name: "Datateknik" },
-  { id: "C", name: "InfoCom" },
-  { id: "VR/AR", name: "Masterprogram inom VR/AR" },
-  { id: "E", name: "Elektroteknik" },
-  { id: "BME", name: "Medicin och teknik" },
-  { id: "Dokt", name: "Doktorand" },
-  { id: "?", name: "Annat" },
+  { id: "D", name: messages.programs_computerScience },
+  { id: "C", name: messages.programs_infoCom },
+  { id: "VR/AR", name: messages.programs_vrAr },
+  { id: "E", name: messages.programs_electricalEngineering },
+  { id: "BME", name: messages.programs_BME },
+  { id: "Dokt", name: messages.programs_phD },
+  { id: "?", name: messages.programs_other },
 ] as const;
